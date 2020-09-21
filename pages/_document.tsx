7111cdc6a4a4f9d54f03113dbfcd94d5d4f121c7
Xavier Jouppe <xajouppe@gmail.com>
2020-09-21 22:49:11 +0200

    perf test without mapbox

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -265,11 +265,11 @@ class StaticDocument extends Document {
             }}
           />
 
-          <script src="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js"></script>
+          {/* <script src="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js"></script>
           <link
             href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
             rel="stylesheet"
-          />
+          /> */}
         </CustomHead>
 
         <body>
