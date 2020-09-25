10e2ec54aeb30ef68667dfcbe35a39c2c5ef6636
Xavier Jouppe <xajouppe@gmail.com>
2020-09-25 09:44:46 +0200

    :art: redirect adress to carte page

--- components/entrepriseSection/index.tsx
+++ components/entrepriseSection/index.tsx
@@ -37,8 +37,10 @@ const EntrepriseSection: React.FC<{
       )}{' '}
       {uniteLegale.etablissement_siege.geo_adresse && (
         <>
-          et domicilié au{' '}
-          <a href="#contact">{uniteLegale.etablissement_siege.geo_adresse}</a>
+          et dont le siège est domicilié au{' '}
+          <a href={`/carte?siret=${uniteLegale.etablissement_siege.siret}`}>
+            {uniteLegale.etablissement_siege.geo_adresse}
+          </a>
         </>
       )}
       .
