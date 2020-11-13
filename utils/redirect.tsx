64591474de7b8e70099fcaa99a998cf6bb66684b
Xavier Jouppe <xajouppe@gmail.com>
2020-11-13 15:17:17 +0100

    fix redirection Red Needles

--- utils/redirect.tsx
+++ utils/redirect.tsx
@@ -2,17 +2,25 @@
 import { ServerResponse } from 'http';
 import { isSirenOrSiret } from './helper';
 
+const redirectToEtablissement = (res: ServerResponse, siretOrSiren: string) => {
+  if (siretOrSiren.length === 9) {
+    redirect(res, `/entreprise/${siretOrSiren}`);
+  } else {
+    redirect(res, `/etablissement/${siretOrSiren}`);
+  }
+};
+
 export const redirectIfSiretOrSiren = (res: ServerResponse, term: string) => {
   if (!term) {
     redirect(res, '/');
   }
 
   if (isSirenOrSiret(term)) {
-    redirect(res, `/entreprise/${term}`);
+    redirectToEtablissement(res, term);
   } else {
     const noSpace = term.split(' ').join('');
     if (isSirenOrSiret(noSpace)) {
-      redirect(res, `/entreprise/${noSpace}`);
+      redirectToEtablissement(res, noSpace);
     }
   }
 };
