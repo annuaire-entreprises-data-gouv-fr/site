6b23eba99096bf57bdd3451a239aa66683cd627f
Xavier Jouppe <xajouppe@gmail.com>
2021-03-17 14:40:32 +0100

    remove client avis situation

--- clients/sirene-insee/avis-situation.ts
+++
@@ -1,9 +0,0 @@
-import { inseeClientPost } from '.';
-import routes from '../routes';
-
-export const getAvisSituationSiren = async (siren: string, nic: string) => {
-  return await inseeClientPost(
-    routes.sireneInsee.avis,
-    'form.siren=388581290&form.nic=00011'
-  );
-};
