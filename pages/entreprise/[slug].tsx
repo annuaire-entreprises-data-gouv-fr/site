71eb31f46eb226cd9939b41d9a75243bb0d5bfeb
Xavier Jouppe <xajouppe@gmail.com>
2021-06-24 00:18:51 +0200

    Uncomment redirect

--- pages/entreprise/[slug].tsx
+++ pages/entreprise/[slug].tsx
@@ -93,7 +93,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       },
     };
   } catch (e) {
-    // redirectIfIssueWithSiren(context.res, e, siren, context.req);
+    redirectIfIssueWithSiren(context.res, e, siren, context.req);
     return { props: {} };
   }
 };
