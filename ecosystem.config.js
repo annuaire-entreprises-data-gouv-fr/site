13131ea82fa8f93f82594f28d8aeddf729cfdf1e
Xavier Jouppe <xajouppe@gmail.com>
2021-07-01 12:10:01 +0200

    update ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,10 @@ module.exports = {
       error_file: '/var/log/annuaire-entreprises/err.log',
       out_file: '/var/log/annuaire-entreprises/out.log',
       time: true,
+      env: {
+        NODE_ENV: 'production',
+        app_port: 3000,
+      },
     },
   ],
 
@@ -24,10 +28,8 @@ module.exports = {
       path: '/opt/apps/annuaire-entreprises',
       'post-deploy':
         'ln -sfn ../.env .env ; npm install && npm run build && /usr/local/lib/npm/bin/pm2 startOrRestart ecosystem.config.js --env production',
-      'pre-deploy-local': "echo 'This is a local executed command'",
       env: {
         NODE_ENV: 'production',
-        app_port: 3000,
       },
     },
   },
