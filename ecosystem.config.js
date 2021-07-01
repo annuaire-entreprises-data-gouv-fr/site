58f06861535d77f80dad7abdb007ff78b812549c
Xavier Jouppe <xajouppe@gmail.com>
2021-07-01 11:12:33 +0200

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps: [
+    {
+      name: 'www',
+      cwd: '/opt/apps/annuaire-entreprises',
+      script: './node_modules/.bin/next',
+      args: 'start -p {{ app_port }}',
+      exec_mode: 'cluster',
+      instances: 'max',
+      log_file: '/var/log/annuaire-entreprises/app.log',
+      error_file: '/var/log/annuaire-entreprises/err.log',
+      out_file: '/var/log/annuaire-entreprises/out.log',
+      time: true,
+      env: {},
+    },
+  ],
+
+  deploy: {
+    staging: {
+      user: 'annuaire-entreprises',
+      // host: 'staging.annuaire-entreprises-infra.data.gouv.fr',
+      host: '149.202.167.79',
+      ref: 'origin/ansible-deployment',
+      repo: 'https://github.com/etalab/annuaire-entreprises.data.gouv.fr.git',
+      path: '/opt/apps/annuaire-entreprises',
+      'post-deploy':
+        'ln -s ../.env .env ; npm install && npm run build && /usr/local/lib/npm/bin/pm2 startOrRestart ecosystem.json --env production',
+      'pre-deploy-local': "echo 'This is a local executed command'",
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  },
+};
