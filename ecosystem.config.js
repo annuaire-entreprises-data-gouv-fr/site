c9270ae60cf09ea369f2b33f2e772c12d5efa8e3
Xavier Jouppe <xajouppe@gmail.com>
2021-07-09 17:19:00 +0200

    Remove app_port variable

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'www',
       cwd: '/opt/apps/annuaire-entreprises',
       script: './node_modules/.bin/next',
-      args: 'start -p {{ app_port }}',
+      args: 'start -p 3000',
       exec_mode: 'cluster',
       instances: 'max',
       log_file: '/var/log/annuaire-entreprises/app.log',
@@ -13,22 +13,4 @@ module.exports = {
       time: true,
     },
   ],
-
-  deploy: {
-    staging: {
-      user: 'annuaire-entreprises',
-      // host: 'staging.annuaire-entreprises-infra.data.gouv.fr',
-      host: '149.202.167.79',
-      ref: 'origin/ansible-deployment',
-      repo: 'https://github.com/etalab/annuaire-entreprises.data.gouv.fr.git',
-      path: '/opt/apps/annuaire-entreprises',
-      'post-deploy':
-        'ln -sfn ../.env .env ; npm install && npm run build && /usr/local/lib/npm/bin/pm2 startOrRestart ecosystem.config.js --env production',
-      'pre-deploy-local': "echo 'This is a local executed command'",
-      env: {
-        NODE_ENV: 'production',
-        app_port: 3000,
-      },
-    },
-  },
 };
