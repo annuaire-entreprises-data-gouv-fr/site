2d41f595d42287e00a5f51c6e53b1ea12ac01115
Xavier Jouppe <xajouppe@gmail.com>
2021-09-07 09:21:52 +0200

    Single script import and style in preact app

--- public/resources/partials/button-async/src/index.js
+++ public/resources/partials/button-async/src/index.js
@@ -1,16 +1,105 @@
-import { h, render, Fragment } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
+import { h, Fragment } from 'preact';
+import { useState, useEffect, useRef } from 'preact/hooks';
 import register from 'preact-custom-element';
+
+const DownloadIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+    <polyline points="7 10 12 15 17 10"></polyline>
+    <line x1="12" y1="15" x2="12" y2="3"></line>
+  </svg>
+);
+
+const Loader = () => (
+  <div className="loader">
+    <span></span>
+    <span></span>
+  </div>
+);
+
+const Styles = (props) => (
+  <style>
+    {`
+.button-link button {
+  cursor: ${props.isLoading} ? 'progress' : 'pointer';
+  flex-direction: 'row';
+  display: 'flex';
+}
+
+.loader {
+  margin: auto;
+  width: 25px;
+  height: 25px;
+  display: inline-block;
+  padding: 0px;
+  text-align: left;
+}
+.loader span {
+  position: absolute;
+  display: inline-block;
+  width: 25px;
+  height: 25px;
+  border-radius: 100%;
+  background: #fff;
+  -webkit-animation: loader 1s linear infinite;
+  animation: loader 1s linear infinite;
+}
+.loader span:last-child {
+  animation-delay: -0.4s;
+  -webkit-animation-delay: -0.4s;
+}
+
+.loader-container > .message {
+  font-style: italic;
+  margin: 20px auto;
+}
+@keyframes loader {
+  0% {
+    transform: scale(0, 0);
+    opacity: 0.8;
+  }
+  100% {
+    transform: scale(1, 1);
+    opacity: 0;
+  }
+}
+@-webkit-keyframes loader {
+  0% {
+    -webkit-transform: scale(0, 0);
+    opacity: 0.8;
+  }
+  100% {
+    -webkit-transform: scale(1, 1);
+    opacity: 0;
+  }
+}
+`}
+  </style>
+);
 
 const ButtonAsync = (props) => {
   const [isLoading, setIsLoading] = useState(false);
+  const instance = useRef(null);
 
   useEffect(() => {
+    if (!props.clean) {
+      return;
+    }
     const elementToClean = document.getElementById(props.clean);
     if (elementToClean) {
       elementToClean.remove();
     }
-  }, [props]);
+  }, [props.clean]);
 
   const click = () => {
     if (isLoading) {
@@ -35,42 +124,19 @@ const ButtonAsync = (props) => {
   };
 
   return (
-    <div className="button-link">
-      <button
-        onClick={click}
-        style={{
-          cursor: isLoading ? 'progress' : 'pointer',
-          flexDirection: 'row',
-          display: 'flex',
-        }}
-      >
-        {isLoading ? (
+    <div className="button-link" ref={instance}>
+      <Styles isLoading={isLoading} />
+      <button onClick={click}>
+        {isLoading || true ? (
           <Fragment>
-            <div className="loader">
-              <span></span>
-              <span></span>
-            </div>
-            <span>
+            <Loader />
+            <div>
               <i>Téléchargement en cours</i>
-            </span>
+            </div>
           </Fragment>
         ) : (
           <Fragment>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-              <polyline points="7 10 12 15 17 10"></polyline>
-              <line x1="12" y1="15" x2="12" y2="3"></line>
-            </svg>
+            <DownloadIcon />
             <span>Télécharger le justificatif d’immatriculation</span>
           </Fragment>
         )}
@@ -79,4 +145,4 @@ const ButtonAsync = (props) => {
   );
 };
 
-register(ButtonAsync, 'partial-button-async', ['href']);
+register(ButtonAsync, 'partial-button-async', ['clean', 'to']);
