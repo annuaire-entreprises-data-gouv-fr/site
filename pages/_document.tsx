abcd383daa5cad4d95462060dbb09481fc4ece41
Xavier Jouppe <xajouppe@gmail.com>
2021-10-21 08:50:26 +0200

    Fix matomo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -129,10 +129,30 @@ class StaticDocument extends Document {
             type="text/javascript"
             src="/resources/js/alpine.min.js"
           ></script>
-          {process.env.NODE_ENV === 'production' &&
-            process.env.MATOMO_SITE_ID && (
-              <script defer src="/resources/js/init-piwik.js"></script>
-            )}
+          {process.env.NODE_ENV === 'production' && process.env.MATOMO_SITE_ID && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              var _paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function () {
+                var u = 'https://stats.data.gouv.fr/';
+                _paq.push(['setTrackerUrl', u + 'piwik.php']);
+                _paq.push(['setSiteId', ${process.env.MATOMO_SITE_ID}]);
+                var d = document,
+                  g = d.createElement('script'),
+                  s = d.getElementsByTagName('script')[0];
+                g.type = 'text/javascript';
+                g.async = true;
+                g.defer = true;
+                g.src = u + 'piwik.js';
+                s.parentNode.insertBefore(g, s);
+              })();
+              `,
+              }}
+            ></script>
+          )}
         </CustomHead>
 
         <body>
