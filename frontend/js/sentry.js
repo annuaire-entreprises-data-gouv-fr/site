f2b0a4bb0d75ed113b7d2623002957e566604911
Xavier Jouppe <xajouppe@gmail.com>
2021-11-04 16:38:38 +0100

    test sentry

--- frontend/js/sentry.js
+++ frontend/js/sentry.js
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/browser';
 var dsn = import.meta.env.VITE_SENTRY_FRONT_DSN;
 
-if (Sentry && dsn) {
+console.log(import.meta.env);
+if (import.meta.env.PROD && Sentry && dsn) {
+  console.log('init');
   Sentry.init({
     dsn: dsn,
     tracesSampleRate: 1.0,
