10646f561b583a770903f2709e18f33e866af3fd
Xavier Jouppe <xajouppe@gmail.com>
2021-11-04 16:49:40 +0100

    Remove sentry tests

--- frontend/js/sentry.js
+++ frontend/js/sentry.js
@@ -1,9 +1,7 @@
 import * as Sentry from '@sentry/browser';
 var dsn = import.meta.env.VITE_SENTRY_FRONT_DSN;
 
-console.log(import.meta.env);
 if (import.meta.env.PROD && Sentry && dsn) {
-  console.log('init');
   Sentry.init({
     dsn: dsn,
     tracesSampleRate: 1.0,
