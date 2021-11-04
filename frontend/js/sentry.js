47be8431490121314a3742490ade77a97711d140
Xavier Jouppe <xajouppe@gmail.com>
2021-11-04 15:48:41 +0100

    Update sentry

--- frontend/js/sentry.js
+++ frontend/js/sentry.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/browser';
 var dsn = import.meta.env.VITE_SENTRY_FRONT_DSN;
 
-if (import.meta.env.MODE === 'production' && Sentry && dsn) {
+if (Sentry && dsn) {
   Sentry.init({
     dsn: dsn,
     tracesSampleRate: 1.0,
