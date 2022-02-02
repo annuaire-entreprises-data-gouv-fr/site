fc729b28027eb20441dda347f11dff6546dce290
Xavier Jouppe <xajouppe@gmail.com>
2022-02-02 22:12:00 +0100

    improve: low mem fingerprint for sitemap script

--- node-scripts/generate.js
+++ node-scripts/generate.js
@@ -11,6 +11,42 @@ const fs = require('fs');
  * NB 2 : main risk here is to overflow memory, always monitor max mem usage when running the script
  */
 
+/**
+ * XML Helpers
+ */
+
+const SITEMAP_START = `<?xml version="1.0" encoding="UTF-8"?>
+ <urlset
+   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+   xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+ >`;
+
+const SITEMAP_END = '</urlset>';
+
+const formatUrl = (url) => `<url>
+     <loc>${url}</loc>
+     </url>`;
+
+const saveSitemapIndex = (indices) => {
+  const index = `<?xml version="1.0" encoding="UTF-8"?>
+ <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+ ${indices
+   .map(
+     (url) => `<sitemap>
+                <loc>${url}</loc>
+                </sitemap>`
+   )
+   .join('')}
+     </sitemapindex>`;
+
+  fs.writeFileSync('./public/sitemap.xml', index);
+};
+
+/**
+ * Memory monitoring helpers
+ */
+
 const mem = () => {
   return (used = process.memoryUsage().heapUsed / 1024 / 1024);
 };
@@ -22,7 +58,9 @@ const logMem = () => {
   );
 };
 
-const workingPath = process.env.NODE_ENV === 'production' ? '.' : '.';
+/**
+ * Script itself
+ */
 
 const WEBSITE =
   process.env.SITE_URL || 'https://annuaire-entreprises.data.gouv.fr';
@@ -32,74 +70,37 @@ const getIndexUrl = (str) => `${WEBSITE}${str}`;
 const getEntrepriseUrl = (str) =>
   `${WEBSITE}/entreprise/${encodeURIComponent(str)}`;
 
-const getSitemap = (idx) => `/sitemap${idx}.xml`;
-
-const saveSitemap = (indices, idx) => {
-  const index = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-  >
-  ${indices
-    .map(
-      (url) => `
-      <url>
-      <loc>${url}</loc>
-      </url>
-      `
-    )
-    .join('')}
-      </urlset>`;
-
-  fs.writeFileSync(`${workingPath}/public${getSitemap(idx)}`, index);
-};
-
-const saveSitemapIndex = (indices) => {
-  const index = `<?xml version="1.0" encoding="UTF-8"?>
-  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${indices
-    .map(
-      (url) => `
-      <sitemap>
-      <loc>${url}</loc>
-      </sitemap>
-      `
-    )
-    .join('')}
-      </sitemapindex>`;
-
-  fs.writeFileSync(`${workingPath}/public/sitemap.xml`, index);
-};
-
 async function main() {
-  let sitemapCount = 0;
-  let currentBatch = [];
+  let writeStream = null;
+  let sitemapCount = 1;
   let urlCount = 0;
+  let totalUrlCount = 0;
   let maxMemory = 0;
+
+  const write = (line) => {
+    if (urlCount === 10000) {
+      maxMemory = Math.max(mem(), maxMemory);
+    }
+    if (urlCount === 0) {
+      const newSitemapFilePath = `./public/sitemap${sitemapCount}.xml`;
+      writeStream = fs.createWriteStream(newSitemapFilePath);
+      writeStream.write(SITEMAP_START);
+    }
+
+    writeStream.write(line);
+    urlCount++;
+    totalUrlCount++;
+
+    if (urlCount === 50000) {
+      urlCount = 0;
+      sitemapCount++;
+      writeStream.write(SITEMAP_END);
+    }
+  };
 
   console.log('*** Sitemap generation script ***');
 
   console.time('⏱ Total time to execute script');
-
-  const write = (elem) => {
-    currentBatch.push(elem);
-    urlCount++;
-
-    if (currentBatch.length === 50000) {
-      sitemapCount++;
-      saveSitemap(currentBatch, sitemapCount);
-      currentBatch = [];
-    }
-  };
-
-  [
-    '/',
-    '/donnees-extrait-kbis',
-    '/comment-ca-marche',
-    '/rechercher',
-    '/faq',
-  ].map((path) => write(WEBSITE + path));
 
   console.time('⏱ Time to download base SIREN');
   const url =
@@ -108,22 +109,52 @@ async function main() {
   const data = names.data;
   console.timeEnd('⏱ Time to download base SIREN');
 
-  data.split('\n').forEach((line, idx) => {
-    if (idx % 10000 === 0) {
-      maxMemory = Math.max(mem(), maxMemory);
+  const filePath = `/tmp/sitemap-${new Date().getTime()}`;
+
+  try {
+    await fs.promises.writeFile(filePath, data, 'utf-8');
+
+    const readStream = fs.createReadStream(filePath);
+
+    await new Promise((resolve, reject) => {
+      readStream.on('error', (err) => reject(err));
+      readStream.on('data', (chunk) => {
+        const line = chunk.toString('utf-8');
+        line.split('\n').map((line) => {
+          const url = formatUrl(getEntrepriseUrl(line));
+          write(url);
+        });
+      });
+      readStream.on('end', () => resolve());
+    });
+
+    [
+      '/',
+      '/donnees-extrait-kbis',
+      '/comment-ca-marche',
+      '/rechercher',
+      '/faq',
+    ].map((path) => {
+      write(formatUrl(WEBSITE + path));
+    });
+
+    if (urlCount !== 0) {
+      // last sitemap needs to be ended
+      write(SITEMAP_END);
     }
-    write(getEntrepriseUrl(line));
-  });
+  } finally {
+    await fs.promises.unlink(filePath);
+  }
 
   const indices = [];
   for (i = 1; i <= sitemapCount; i++) {
-    indices.push(getIndexUrl(getSitemap(i)));
+    indices.push(getIndexUrl(`/sitemap${i}.xml`));
   }
   saveSitemapIndex(indices);
 
   console.timeEnd('⏱ Total time to execute script');
   console.log(`📈 Max memory usage ${Math.round(maxMemory * 100) / 100} mo`);
-  console.log(`💾 Saved ${sitemapCount} sitemaps with ${urlCount} urls`);
+  console.log(`💾 Saved ${sitemapCount} sitemaps with ${totalUrlCount} urls`);
 }
 
 main();
