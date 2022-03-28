79e7a3e2d382f7cd7489f3f568e68a4a83612809
Xavier Jouppe <xajouppe@gmail.com>
2022-03-28 18:52:44 +0200

    fix:unused console.log

--- pages/api/account/logout/index.ts
+++ pages/api/account/logout/index.ts
@@ -4,7 +4,6 @@ import { client } from '../../../../clients/france-connect/strategy';
 
 export default nextConnect<NextApiRequest, NextApiResponse>().get(
   async (req, res, next) => {
-    console.log(client.endSessionUrl({ state, id_token_hint }));
     res.redirect(client.endSessionUrl());
   }
 );
