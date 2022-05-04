4f7d95ddf1b80121567609499d790818b42ca3a9
xavier jouppe <xajouppe@gmail.com>
2022-05-04 11:35:41 +0200

    rename function

--- pages/api/inpi-pdf-proxy/job/get/[slug].ts
+++ pages/api/inpi-pdf-proxy/job/get/[slug].ts
@@ -5,10 +5,10 @@ import routes from '../../../../../clients/routes';
  * Call with authenticated cookies to get full pdf.
  */
 
-const createPdfDownload = async (req: NextApiRequest, res: NextApiResponse) => {
+const getPdfDownload = async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query.slug as string;
 
   res.redirect(routes.rncs.proxy.document.justificatif.get + slug + '.pdf');
 };
 
-export default createPdfDownload;
+export default getPdfDownload;
