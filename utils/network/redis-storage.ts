76e5b82f8b8228e2f649a4c40cf29b9e27e21b7a
xavier jouppe <xajouppe@gmail.com>
2022-05-06 17:16:38 +0200

    fix: catch redis error

--- utils/network/redis-storage.ts
+++ utils/network/redis-storage.ts
@@ -3,6 +3,7 @@ import { buildStorage } from 'axios-cache-interceptor';
 
 const client = createClient({ url: process.env.REDIS_URL });
 
+client.on('error', (err) => console.error('Redis Client Error : ', err));
 client.connect();
 
 const redisStorage = buildStorage({
