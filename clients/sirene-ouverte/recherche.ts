a9d9f282e40592b3ae39bdd115d17e782c2e2926
xavier jouppe <xajouppe@gmail.com>
2022-05-24 10:44:39 +0200

    type: new type search results

--- clients/sirene-ouverte/recherche.ts
+++ clients/sirene-ouverte/recherche.ts
@@ -11,36 +11,36 @@ import routes from '../routes';
 
 interface ISireneOuverteUniteLegaleResultat {
   siren: string;
-  siret: string;
-  etat_administratif_unite_legale: string;
-  latitude: string;
-  longitude: string;
-  nom_complet: string;
-  page_path: string;
-  complement_adresse: string;
-  numero_voie: string;
-  indice_repetition: string;
-  type_voie: string;
-  libelle_commune: string;
-  code_postal: string;
-  libelle_voie: string;
-  // new fields
-  siret_siege: string;
-  tranche_effectif_salarie_siege: string;
-  commune: string;
-  date_debut_activite: string;
-  etat_administratif_siege: string;
-  activite_principale_siege: string;
-  date_creation_unite_legale: string;
-  tranche_effectif_salarie_unite_legale: string;
-  date_mise_a_jour: string;
+  siege: {
+    siret: string;
+    date_creation: string;
+    tranche_effectif_salarie: string;
+    date_debut_activite: string;
+    etat_adiministratif: string;
+    activite_principale: string;
+    numero_voie: string;
+    type_voie: string;
+    libelle_voie: string;
+    code_postal: string;
+    libelle_commune: string;
+    indice_repetition: string;
+    complement_adresse: string;
+    commune: string;
+    longitude: number;
+    latitude: number;
+    activite_principale_registre_metier: string;
+  };
+  date_creation: string;
   categorie_entreprise: string;
+  etat_administratif: string;
   nom_raison_sociale: string;
-  nature_juridique_unite_legale: string;
-  activite_principale_unite_legale: string;
-  nombre_etablissements_ouverts: number;
+  nature_juridique: string;
+  activite_principale: string;
+  economie_sociale_solidaire: string;
+  nom_complet: string;
   nombre_etablissements: number;
-  is_entrepreneur_individuel: string;
+  nombre_etablissements_ouverts: number;
+  is_entrepreneur_individuel: true;
 }
 
 export interface ISireneOuverteSearchResults {
@@ -91,25 +91,25 @@ const mapToDomainObjectNew = (
     results: results.map((result: ISireneOuverteUniteLegaleResultat) => {
       return {
         siren: result.siren,
-        siret: result.siret_siege,
-        estActive: result.etat_administratif_unite_legale === 'A',
+        siret: result.siege.siret,
+        estActive: result.etat_administratif === 'A',
         adresse: formatAdresse(
-          result.complement_adresse || '',
-          result.numero_voie,
-          result.indice_repetition,
-          result.type_voie,
-          result.libelle_voie,
-          result.code_postal,
-          result.libelle_commune
+          result.siege.complement_adresse || '',
+          result.siege.numero_voie,
+          result.siege.indice_repetition,
+          result.siege.type_voie,
+          result.siege.libelle_voie,
+          result.siege.code_postal,
+          result.siege.libelle_commune
         ),
-        latitude: result.latitude || '',
-        longitude: result.longitude || '',
+        latitude: result.siege.latitude || 0,
+        longitude: result.siege.longitude || 0,
         nomComplet: result.nom_complet || 'Nom inconnu',
         nombreEtablissements: result.nombre_etablissements || 1,
         nombreEtablissementsOuverts: result.nombre_etablissements_ouverts || 0,
-        chemin: result.page_path || result.siren,
+        chemin: result.siren,
         libelleActivitePrincipale: libelleFromCodeNaf(
-          result.activite_principale_unite_legale,
+          result.activite_principale,
           false
         ),
       };
