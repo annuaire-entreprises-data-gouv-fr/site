c4210faf83da807b832e15f074be6bd9a450ab98
xavier jouppe <xajouppe@gmail.com>
2022-06-14 20:17:13 +0200

    feature: use sentry/nextjs for transaction monitoring

---
+++ sentry.client.config.js
@@ -0,0 +1,11 @@
+import * as Sentry from '@sentry/nextjs';
+
+/**
+ * IGNORE
+ *
+ * This is a dummy client config file. Needed for next js build.
+ *
+ * This file is not used in production
+ *
+ * Real sentry client config can be found in /frontend/js/sentry.js
+ */
