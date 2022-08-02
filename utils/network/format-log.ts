f7ff68a8b2c461ff76634b3a00bcf6688e963675
xavier jouppe <xajouppe@gmail.com>
2022-08-02 16:33:14 +0200

    monitor: active handle and active request count in logs

--- utils/network/format-log.ts
+++ utils/network/format-log.ts
@@ -4,5 +4,11 @@ export const formatLog = (
   isFromCached = false,
   time = -1
 ) => {
-  return `status=${status} time=${time} isFromCached=${isFromCached} request=${url}`;
+  // see https://medium.com/trabe/detecting-node-js-active-handles-with-wtfnode-704e91f2b120
+
+  //@ts-ignore
+  const reqCount = process._getActiveRequests().length;
+  //@ts-ignore
+  const handleCount = process._getActiveHandles().length;
+  return `status=${status} time=${time} isFromCached=${isFromCached} handleCount=${handleCount} reqCount=${reqCount} request=${url}`;
 };
