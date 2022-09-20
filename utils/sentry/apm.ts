be3ba819bfa41f29f952491d1b7c50e79253471f
xavier jouppe <xajouppe@gmail.com>
2022-09-20 21:13:02 +0200

    Fix: group rechercher and rechercher/carte

--- utils/sentry/apm.ts
+++ utils/sentry/apm.ts
@@ -6,6 +6,11 @@ const getTransactionNameFromUrl = (url: string) => {
     if (url.indexOf('/entreprise/') === 0) {
       return '/entreprise/:slug';
     }
+    if (url.indexOf('/rechercher/carte') > -1) {
+      return '/rechercher/carte';
+    } else if (url.indexOf('/rechercher') > -1) {
+      return '/rechercher';
+    }
     return url.replace('?redirected=1', '').replace(/\d{14}|\d{9}/g, ':slug');
   } catch {
     return url;
