2c7e72675e9d868c1f55527de2d93dc20ca44bde
xavier jouppe <xajouppe@gmail.com>
2022-10-04 17:14:05 +0200

    Better load profile

--- frontend/src/load-bar.js
+++ frontend/src/load-bar.js
@@ -12,14 +12,16 @@ window.addEventListener('beforeunload', async () => {
   loader.style.left = '0';
   loader.style.background = '#000091';
   loader.style.width = '30vw';
-  loader.style.height = '4px';
+  loader.style.height = '3px';
   loader.style.zIndex = 1000;
 
   document.body.appendChild(loader);
 
+  // first two 0 = wait 200ms before triggering load bar
   for (let w of [
-    0, 3, 5, 10, 12, 15, 22, 28, 34, 35, 36, 38, 40, 45, 65, 66, 78, 80, 85, 88,
-    89, 90, 91, 92, 95,
+    0, 0, 3, 5, 10, 12, 15, 22, 28, 34, 35, 36, 38, 40, 45, 65, 66, 67, 67, 68,
+    70, 73, 75, 77, 78, 80, 82, 83, 85, 86, 88, 88, 89, 90, 91, 92, 94, 94, 95,
+    96, 96, 97,
   ]) {
     loader.style.width = `${w}vw`;
     await wait(200);
