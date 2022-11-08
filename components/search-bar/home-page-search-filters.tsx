06931299427ee9345f234a55176d455cc744dcef
xavier jouppe <xajouppe@gmail.com>
2022-11-08 17:56:29 +0100

    Fix homepage layout

--- components/search-bar/home-page-search-filters.tsx
+++ components/search-bar/home-page-search-filters.tsx
@@ -1,48 +1,50 @@
 import AdvancedSearchFilters from './advanced-search-filters';
 
 const HomePageSearchFilters = () => (
-  <div id="search-filters-container" className="layout-center">
-    <input type="checkbox" id="show-advanced-search-filter" />
-    <label className="more" htmlFor="show-advanced-search-filter">
-      Afficher les filtres de recherche
-    </label>
-    <div className="advanced-filter-container">
-      <AdvancedSearchFilters />
+  <div id="search-filters-container">
+    <div className="layout-center">
+      <input type="checkbox" id="show-advanced-search-filter" />
+      <label className="more" htmlFor="show-advanced-search-filter">
+        Afficher les filtres de recherche
+      </label>
+      <div className="advanced-filter-container">
+        <AdvancedSearchFilters />
+      </div>
+      <label className="less" htmlFor="show-advanced-search-filter">
+        Cacher les filtres de recherche
+      </label>
+      <style jsx>{`
+        div.layout-center {
+          flex-direction: column;
+        }
+
+        div.advanced-filter-container {
+          display: none;
+          flex-wrap: wrap;
+        }
+
+        label {
+          text-decoration: underline;
+          cursor: pointer;
+        }
+        label.less {
+          display: none;
+        }
+        input[type='checkbox'] {
+          display: none;
+        }
+        input[type='checkbox']:checked ~ div.advanced-filter-container {
+          display: flex;
+        }
+
+        input[type='checkbox']:checked ~ label.more {
+          display: none;
+        }
+        input[type='checkbox']:checked ~ label.less {
+          display: block;
+        }
+      `}</style>
     </div>
-    <label className="less" htmlFor="show-advanced-search-filter">
-      Cacher les filtres de recherche
-    </label>
-    <style jsx>{`
-      #search-filters-container {
-        flex-direction: column;
-      }
-
-      div.advanced-filter-container {
-        display: none;
-        flex-wrap: wrap;
-      }
-
-      label {
-        text-decoration: underline;
-        cursor: pointer;
-      }
-      label.less {
-        display: none;
-      }
-      input[type='checkbox'] {
-        display: none;
-      }
-      input[type='checkbox']:checked ~ div.advanced-filter-container {
-        display: flex;
-      }
-
-      input[type='checkbox']:checked ~ label.more {
-        display: none;
-      }
-      input[type='checkbox']:checked ~ label.less {
-        display: block;
-      }
-    `}</style>
   </div>
 );
 
