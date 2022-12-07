c49ee1ccee3fcaaf88d5e4c223649b34d19301dc
Yoan Ficadiere <yoan.ficadiere@beta.gouv.fr>
2022-12-07 09:43:12 +0100

    fix

--- components/table/simple.tsx
+++ components/table/simple.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useState } from 'react';
+import React, { PropsWithChildren } from 'react';
 import { logWarningInSentry } from '../../utils/sentry';
 
 interface ISectionProps {
