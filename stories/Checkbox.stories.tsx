61e48c64cf90b4ff62808d6a7f5e63b5ea99ffcb
Yoan Ficadiere <yoan.ficadiere@beta.gouv.fr>
2022-12-29 18:35:11 +0100

    chore: adding storybook

---
+++ stories/Checkbox.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+import { Checkbox } from '#components-ui/checkbox';
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox,
+  argTypes: {},
+} as ComponentMeta<typeof Checkbox>;
+
+const Template: ComponentStory<typeof Checkbox> = (args) => (
+  <Checkbox {...args} />
+);
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  label: 'Rechercher les établissement RGE',
+  value: false,
+};
