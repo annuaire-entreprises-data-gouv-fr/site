793b90d7f0dddabe324bcb8adeb189d111de6575
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2023-01-04 09:33:43 +0100

    Feature enable per page hydration (#191)
    * Feature enable per page hydration
    
    * Use props to activate react
    
    * feat: merge all custom documents
    
    * feat: remove partial hydration test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
 const manifest = (
@@ -6,6 +6,67 @@ const manifest = (
     ? require('../public/manifest.json')
     : {}
 ) as { [key: string]: any };
+
+const LinksAndScripts = ({ dev = false }) => (
+  <>
+    {/* Standard Meta */}
+    {/* https://gouvfr.atlassian.net/wiki/spaces/DB/pages/223019574/D+veloppeurs */}
+    <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+    <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
+    <link
+      rel="shortcut icon"
+      href="/favicons/favicon.ico"
+      type="image/x-icon"
+    />
+    <link
+      rel="manifest"
+      href="/favicons/manifest.webmanifest"
+      cross-origin="use-credentials"
+    />
+    {dev ? (
+      <>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="http://localhost:3001/frontend/style/dsfr.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="http://localhost:3001/frontend/style/globals.css"
+        />
+        <script
+          defer
+          type="module"
+          src="http://localhost:3001/@vite/client"
+        ></script>
+        <script
+          defer
+          type="module"
+          src="http://localhost:3001/frontend/src/index.js"
+        ></script>
+      </>
+    ) : (
+      <>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href={`/${manifest['style/dsfr.min.css'].file}`}
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href={`/${manifest['style/globals.css'].file}`}
+        />
+        <script
+          defer
+          type="module"
+          src={`/${manifest['src/index.js'].file}`}
+        ></script>
+      </>
+    )}
+  </>
+);
 
 class CustomHead extends Head {
   render() {
@@ -23,94 +84,24 @@ class CustomHead extends Head {
   }
 }
 
-class DevDocument extends Document {
+class CustomDocument extends Document {
   render() {
-    return (
-      <Html lang="fr">
-        <CustomHead>
-          <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
-          <link
-            rel="shortcut icon"
-            href="/favicons/favicon.ico"
-            type="image/x-icon"
-          />
-          <link
-            rel="manifest"
-            href="/favicons/manifest.webmanifest"
-            cross-origin="use-credentials"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="http://localhost:3001/frontend/style/dsfr.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="http://localhost:3001/frontend/style/globals.css"
-          />
-          <script
-            defer
-            type="module"
-            src="http://localhost:3001/@vite/client"
-          ></script>
-          <script
-            defer
-            type="module"
-            src="http://localhost:3001/frontend/src/index.js"
-          ></script>
-        </CustomHead>
-        <body>
-          <Main />
-        </body>
-      </Html>
-    );
-  }
-}
+    const isProd = process.env.NODE_ENV === 'production';
+    const useReact =
+      this.props['__NEXT_DATA__']?.props?.pageProps?.metadata?.useReact;
 
-class StaticDocument extends Document {
-  render() {
+    const HeadToUse = useReact ? Head : CustomHead;
     return (
       <Html lang="fr">
-        <CustomHead>
-          {/* Standard Meta */}
-          {/* https://gouvfr.atlassian.net/wiki/spaces/DB/pages/223019574/D+veloppeurs */}
-          <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
-          <link
-            rel="shortcut icon"
-            href="/favicons/favicon.ico"
-            type="image/x-icon"
-          />
-          <link
-            rel="manifest"
-            href="/favicons/manifest.webmanifest"
-            cross-origin="use-credentials"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href={`/${manifest['style/dsfr.min.css'].file}`}
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href={`/${manifest['style/globals.css'].file}`}
-          />
-          <script
-            defer
-            type="module"
-            src={`/${manifest['src/index.js'].file}`}
-          ></script>
-        </CustomHead>
+        <HeadToUse>
+          <LinksAndScripts dev={!isProd} />
+        </HeadToUse>
         <body>
           <Main />
-          {process.env.NODE_ENV === 'production' &&
-            process.env.MATOMO_SITE_ID && (
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
+          {isProd && process.env.MATOMO_SITE_ID && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
               var _paq = window._paq || [];
               _paq.push(['trackPageView']);
               _paq.push(['enableLinkTracking']);
@@ -128,15 +119,14 @@ class StaticDocument extends Document {
                 s.parentNode.insertBefore(g, s);
               })();
               `,
-                }}
-              ></script>
-            )}
+              }}
+            ></script>
+          )}
+          {useReact && <NextScript />}
         </body>
       </Html>
     );
   }
 }
 
-export default process.env.NODE_ENV === 'production'
-  ? StaticDocument
-  : DevDocument;
+export default CustomDocument;
