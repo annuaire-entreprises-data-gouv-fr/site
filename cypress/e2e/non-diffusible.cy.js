7fd49600f7360a4ae5c453398993a9d68df42500
xavier jouppe <xajouppe@gmail.com>
2023-01-05 09:45:40 +0100

    fix: e2e partial diff

--- cypress/e2e/non-diffusible.cy.js
+++ cypress/e2e/non-diffusible.cy.js
@@ -9,4 +9,14 @@ describe('Non-diffusible', () => {
     cy.visit('/entreprise/880878145');
     cy.contains('ne sont pas publiques').should('have.length', 0);
   });
+
+  it('Cannot find partial diffusible in search', () => {
+    cy.visit('/rechercher?terme=severnaya');
+    cy.get('.results-list').should('have.length', 0);
+  });
+
+  it('No dirigeant in partial diffusible', () => {
+    cy.visit('/dirigeants/908595879');
+    cy.contains('Données privées').should('have.length', 1);
+  });
 });
