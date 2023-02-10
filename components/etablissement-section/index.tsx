bb4d7fba87272b432a779548c9eff794b5161567
xavier jouppe <xajouppe@gmail.com>
2023-02-10 17:00:34 +0100

    feat: prenom and enseigne / nom commercial order

--- components/etablissement-section/index.tsx
+++ components/etablissement-section/index.tsx
@@ -51,11 +51,11 @@ const EtablissementSection: React.FC<IProps> = ({
           ],
         ]
       : []),
-    ...(etablissement.denomination
-      ? [['Nom de l’établissement', etablissement.denomination]]
-      : []),
     ...(etablissement.enseigne
       ? [['Enseigne de l’établissement', etablissement.enseigne]]
+      : []),
+    ...(etablissement.denomination
+      ? [['Nom de l’établissement', etablissement.denomination]]
       : []),
     [
       <FAQLink to="/faq/modifier-adresse" tooltipLabel="Adresse">
