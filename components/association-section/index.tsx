8ed5393fed6cf67dc133c1d92a9d8c8425c1a12d
xavier jouppe <xajouppe@gmail.com>
2023-02-10 16:51:06 +0100

    feat: minor updates

--- components/association-section/index.tsx
+++ components/association-section/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Warning from '#components-ui/alerts/warning';
 import { HorizontalSeparator } from '#components-ui/horizontal-separator';
 import BreakPageForPrint from '#components-ui/print-break-page';
+import { Tag } from '#components-ui/tag';
 import { Section } from '#components/section';
 import { TwoColumnTable } from '#components/table/simple';
 import { EAdministration } from '#models/administrations';
@@ -25,62 +26,66 @@ const AssociationSection: React.FC<{
       utilPublique = false,
       datePublicationJournalOfficiel = '',
       dateCreation = '',
-      dateDissolution,
-      telephone,
-      mail,
-      siteWeb,
+      dateDissolution = '',
+      telephone = '',
+      mail = '',
+      siteWeb = '',
       agrement = [],
-      eligibiliteCEC,
+      eligibiliteCEC = false,
     },
   } = uniteLegale;
 
   const data = [
-    ['N° RNA (identifiant d’association)', formatIntFr(idAssociation)],
+    ['N°RNA', formatIntFr(idAssociation)],
     ['Nom', nomComplet],
     ['Famille', libelleFamille],
     ['Objet', objet],
-    ...(regime
-      ? [
-          [
-            'Forme juridique',
-            `${regime}, ${formeJuridique} ${
-              utilPublique ? ", reconnue d'utilité publique" : ''
-            }`,
-          ],
-        ]
-      : []),
     [
-      'Date de publication au journal officiel',
+      'Forme juridique',
+      regime || formeJuridique || utilPublique || eligibiliteCEC ? (
+        <>
+          {regime ? `${regime}, ` : ''}
+          {formeJuridique}
+          {utilPublique && (
+            <Tag className="info">Reconnue d’utilité publique</Tag>
+          )}
+          {eligibiliteCEC && <Tag className="info">Éligible CEC</Tag>}
+        </>
+      ) : (
+        ''
+      ),
+    ],
+    [
+      'Agrément(s)',
+      agrement.length > 0
+        ? agrement.map((agr) => {
+            return (
+              <div key={agr.type}>
+                <b>{agr.type}&nbsp;:</b> attribué le{' '}
+                {formatDate(agr.dateAttribution)} ({agr.attributeur})
+              </div>
+            );
+          })
+        : '',
+    ],
+    ['', <br />],
+    [
+      'Date de publication au Journal Officiel',
       formatDate(datePublicationJournalOfficiel),
     ],
     ['Date de création', formatDate(dateCreation)],
     ...(dateDissolution && dateDissolution > dateCreation
       ? [['Date dissolution', formatDate(dateDissolution)]]
       : []),
+    ['', <br />],
     ['Adresse du siège', adresseSiege],
     ['Adresse de gestion', adresseGestion],
-    ['Téléphone', <a href={`tel:${telephone}`}>{telephone}</a>],
-    ['Email', <a href={`mailto:${mail}`}>{mail}</a>],
-    ['Site web', <a href={siteWeb}>{siteWeb}</a>],
     [
-      'Agrement admininstratif',
-      <ul>
-        {agrement.map((agr) => {
-          return (
-            <>
-              <li>
-                <b>{agr.type}</b>
-              </li>
-              <ul>
-                <li>Attributeur : {agr.attributeur}</li>
-                <li>Date attribution : {formatDate(agr.dateAttribution)}</li>
-              </ul>
-            </>
-          );
-        })}
-      </ul>,
+      'Téléphone',
+      telephone ? <a href={`tel:${telephone}`}>{telephone}</a> : '',
     ],
-    ['Eligible CEC', eligibiliteCEC ? 'Oui' : 'Non'],
+    ['Email', mail ? <a href={`mailto:${mail}`}>{mail}</a> : ''],
+    ['Site web', siteWeb ? <a href={siteWeb}>{siteWeb}</a> : ''],
   ];
 
   const notInRna = !nomComplet && !objet && !adresseSiege;
@@ -92,44 +97,54 @@ const AssociationSection: React.FC<{
         sources={[EAdministration.MI]}
       >
         {notInRna ? (
-          <Warning>
-            Cette structure est une association, mais aucune information n’a été
-            trouvée dans le <b>Répertoire National des Associations (RNA)</b>.
-            {!isTwoMonthOld(uniteLegale.dateCreation) && (
-              <>
-                <br />
-                Cette structure a été créée il y a moins de deux mois. Il est
-                donc possible qu’elle n’ait pas encore été publiée au RNA et
-                qu’elle le soit prochainement.
-              </>
-            )}
-          </Warning>
+          <>
+            <Warning>
+              Cette structure est une association, mais aucune information n’a
+              été trouvée dans le{' '}
+              <b>Répertoire National des Associations (RNA)</b>.
+              {!isTwoMonthOld(uniteLegale.dateCreation) && (
+                <>
+                  <br />
+                  Cette structure a été créée il y a moins de deux mois. Il est
+                  donc possible qu’elle n’ait pas encore été publiée au RNA et
+                  qu’elle le soit prochainement.
+                </>
+              )}
+            </Warning>
+            <TwoColumnTable body={[['N°RNA', formatIntFr(idAssociation)]]} />
+          </>
         ) : (
-          <p>
-            Cette structure est inscrite au{' '}
-            <b>Répertoire National des Associations (RNA)</b>, qui contient les
-            informations suivantes&nbsp;:
-          </p>
+          <>
+            <p>
+              Cette structure est inscrite au{' '}
+              <b>Répertoire National des Associations (RNA)</b>, qui contient
+              les informations suivantes&nbsp;:
+            </p>
+            <TwoColumnTable body={data} />
+          </>
         )}
-        <TwoColumnTable body={data} />
-        <br />
-        Retrouver plus d&apos;informations sur la{' '}
-        <a
-          target="_blank"
-          href={`https://www.data-asso.fr/annuaire/association/${idAssociation}?docFields=documentsDac,documentsRna`}
-          rel="noreferrer"
-        >
-          fiche de cette association
-        </a>{' '}
-        (comptes, effectifs et documents) sur{' '}
-        <a
-          target="_blank"
-          rel="noreferrer noopener"
-          href="https://www.data-asso.fr"
-        >
-          data-asso
-        </a>
-        .
+        {idAssociation && (
+          <>
+            <br />
+            Retrouvez plus d&apos;informations sur la{' '}
+            <a
+              target="_blank"
+              href={`https://www.data-asso.fr/annuaire/association/${idAssociation}?docFields=documentsDac,documentsRna`}
+              rel="noreferrer"
+            >
+              fiche de cette association
+            </a>{' '}
+            (comptes, effectifs et documents) sur{' '}
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://www.data-asso.fr"
+            >
+              data-asso
+            </a>
+            .
+          </>
+        )}
       </Section>
       <HorizontalSeparator />
       <BreakPageForPrint />
