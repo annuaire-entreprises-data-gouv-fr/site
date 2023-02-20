853370633b4a37c865413aac5ecc9dd684f74a83
Yoan Ficadiere <yoan.ficadiere@beta.gouv.fr>
2023-02-20 17:09:44 +0100

    feat: update  dedicated section for dirigeant asso

--- pages/dirigeants/[slug].tsx
+++ pages/dirigeants/[slug].tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from 'next';
 import React from 'react';
 import BreakPageForPrint from '#components-ui/print-break-page';
-import { INPI, INSEE } from '#components/administrations';
+import { INPI, INSEE, MI } from '#components/administrations';
 import BeneficiairesSection from '#components/dirigeants-section/beneficiaires';
 import DirigeantsEntrepriseIndividuelleSection from '#components/dirigeants-section/insee-dirigeant';
 import DirigeantsSection from '#components/dirigeants-section/rncs-dirigeants';
@@ -67,45 +67,7 @@ const DirigeantsPage: NextPageWithLayout<IProps> = ({
                     <BreakPageForPrint />
                   </>
                 )}
-              {uniteLegale.association.idAssociation && !uniteLegale.dirigeant && (
-                <Section
-                  id="rncs-dirigeants"
-                  title="Dirigeant(e) d’association"
-                  sources={[EAdministration.MI]}
-                >
-                  <p>
-                    Nous n’avons pas retrouvé de dirigeants enreigstrés auprès
-                    de l’
-                    <INSEE /> ou auprès de <INPI />.
-                  </p>
-                  <p>
-                    Si des dirigeants de cette strucure ont été déclarés auprès
-                    du <a href="https://www.interieur.gouv.fr">MI</a>, vous les
-                    retrouverez sur l&apos;onglet dirigeant de :{' '}
-                    <a
-                      target="_blank"
-                      href={`https://www.data-asso.fr/annuaire/association/${uniteLegale.association.idAssociation}?docFields=documentsDac,documentsRna`}
-                      rel="noreferrer"
-                    >
-                      data-asso
-                    </a>{' '}
-                  </p>
-                </Section>
-              )}
-              {isServicePublic(uniteLegale) && !uniteLegale.dirigeant && (
-                <Section
-                  id="rncs-dirigeants"
-                  title="Dirigeant(e) de service public"
-                  sources={[EAdministration.DINUM]}
-                >
-                  <p>
-                    Les administrations centrales, ministères et autres services
-                    public, n&apos;ont pas de dirigeants enregistrés dans les
-                    base de données de l’
-                    <INSEE /> ou de <INPI />.
-                  </p>
-                </Section>
-              )}
+
               <DirigeantsSection
                 immatriculationRNCS={immatriculationRNCS}
                 siren={uniteLegale.siren}
