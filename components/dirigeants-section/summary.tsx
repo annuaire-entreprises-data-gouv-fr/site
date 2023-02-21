4c813a2f57f9e87992768fabe3b2ea9f8113c9a1
xavier jouppe <xajouppe@gmail.com>
2023-02-21 11:52:21 +0100

    fix: dirigeant service public

--- components/dirigeants-section/summary.tsx
+++ components/dirigeants-section/summary.tsx
@@ -28,75 +28,70 @@ const DirigeantSummary: React.FC<IDirigeants> = ({
     `${immatriculationRNCS.beneficiaires.length} bénéficiaires inscrits à Référentiel des Bénéficiaires Effectifs`;
 
   if (hasNoDirigeant) {
-    return (
-      <>
+    if (uniteLegale.association.idAssociation) {
+      return (
+        <>
+          <p>
+            Cette association n’a pas de dirigeant(e) enregistré(e) auprès de l’
+            <INSEE /> ou auprès de l’
+            <INPI />
+          </p>
+          <p>
+            Si des dirigeants ont été déclarés auprès du <MI /> vous les
+            retrouverez sur l&apos;onglet “personnes physiques“ de :{' '}
+            <a
+              target="_blank"
+              href={`https://www.data-asso.fr/annuaire/association/${uniteLegale.association.idAssociation}?docFields=documentsDac,documentsRna`}
+              rel="noreferrer"
+            >
+              data-asso
+            </a>{' '}
+          </p>
+        </>
+      );
+    } else if (isServicePublic(uniteLegale)) {
+      return (
         <p>
-          Cette association n’a pas de dirigeant(e) enregistré(e) auprès de l’
-          <INSEE /> ou auprès de l’
-          <INPI />
+          Les administrations centrales, ministères et autres services public,
+          n&apos;ont pas de dirigeant(e) enregistré(e) dans les bases de données
+          de l’
+          <INSEE /> ou de <INPI />.
         </p>
-        <p>
-          Si des dirigeants ont été déclarés auprès du <MI /> vous les
-          retrouverez sur l&apos;onglet “personnes physiques“ de :{' '}
-          <a
-            target="_blank"
-            href={`https://www.data-asso.fr/annuaire/association/${uniteLegale.association.idAssociation}?docFields=documentsDac,documentsRna`}
-            rel="noreferrer"
-          >
-            data-asso
-          </a>{' '}
-        </p>
-      </>
-    );
-  }
-
-  if (isServicePublic(uniteLegale) && hasNoDirigeant) {
+      );
+    }
     return (
       <p>
-        Les administrations centrales, ministères et autres services public,
-        n&apos;ont pas de dirigeant(e) enregistré(e) dans les bases de données
-        de l’
-        <INSEE /> ou de <INPI />.
+        Cette structure n’a pas de dirigeant(e) enregistré(e), que ce soit
+        auprès de l’
+        <INSEE /> ou auprès de l’
+        <INPI />.
       </p>
     );
   }
 
-  return (
+  return insee || rncs || rbe ? (
     <>
-      {hasNoDirigeant ? (
-        <p>
-          Cette structure n’a pas de dirigeant(e) enregistré(e), que ce soit
-          auprès de l’
-          <INSEE /> ou auprès de l’
-          <INPI />.
-        </p>
-      ) : (
-        (insee || rncs || rbe) && (
-          <>
-            Cette structure possède :
-            <ul>
-              {insee && (
-                <li>
-                  <a href="#insee-dirigeant">{insee}</a>
-                </li>
-              )}
-              {rncs && (
-                <li>
-                  <a href="#rncs-dirigeants">{rncs}</a>
-                </li>
-              )}
-              {rbe && (
-                <li>
-                  <a href="#beneficiaires">{rbe}</a>
-                </li>
-              )}
-            </ul>
-            <br />
-          </>
-        )
-      )}
+      Cette structure possède :
+      <ul>
+        {insee && (
+          <li>
+            <a href="#insee-dirigeant">{insee}</a>
+          </li>
+        )}
+        {rncs && (
+          <li>
+            <a href="#rncs-dirigeants">{rncs}</a>
+          </li>
+        )}
+        {rbe && (
+          <li>
+            <a href="#beneficiaires">{rbe}</a>
+          </li>
+        )}
+      </ul>
+      <br />
     </>
-  );
+  ) : null;
 };
 
 export default DirigeantSummary;
