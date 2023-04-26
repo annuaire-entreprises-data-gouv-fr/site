f194049d3d083ee119b1e3db1415662b387665a4
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2023-04-26 17:22:09 +0200

    Update tranches-effectifs-options.ts

--- utils/labels/tranches-effectifs-options.ts
+++ utils/labels/tranches-effectifs-options.ts
@@ -3,23 +3,27 @@
  */
 export const tranchesEffectifsOptions = [
   {
-    label: 'Entreprise sans salarié ou nombre de salarié inconnu',
-    value: '00,NN',
+    label:
+      "Unité non employeuse (pas de salarié au cours de l'année de référence et pas d'effectif au 31/12)",
+    value: 'NN',
   },
   {
-    label: '1 - 19 salariés',
-    value: '01,02,03,11',
+    label:
+      "0 salarié (n'ayant pas d'effectif au 31/12 mais ayant employé des salariés au cours de l'année de référence)",
+    value: '00',
   },
-  {
-    label: '20 - 249 salariés',
-    value: '12,21,22,31',
-  },
-  {
-    label: '250 - 4999 salariés',
-    value: '32,41,42,51',
-  },
-  {
-    label: '5000 salariés et plus',
-    value: '52,53',
-  },
+  { label: '1 ou 2 salariés', value: '01' },
+  { label: '3 à 5 salariés', value: '02' },
+  { label: '6 à 9 salariés', value: '03' },
+  { label: '10 à 19 salariés', value: '11' },
+  { label: '20 à 49 salariés', value: '12' },
+  { label: '50 à 99 salariés', value: '21' },
+  { label: '100 à 199 salariés', value: '22' },
+  { label: '200 à 249 salariés', value: '31' },
+  { label: '250 à 499 salariés', value: '32' },
+  { label: '500 à 999 salariés', value: '41' },
+  { label: '1 000 à 1 999 salariés', value: '42' },
+  { label: '2 000 à 4 999 salariés', value: '51' },
+  { label: '5 000 à 9 999 salariés', value: '52' },
+  { label: '10 000 salariés et plus', value: '53' },
 ];
