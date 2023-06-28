40e25642cf6f199d31c1fe4a4efba4cedefb7dc7
Yoan Ficadiere <yoan.ficadiere@beta.gouv.fr>
2023-06-29 00:31:30 +0200

    feat: create match all polyfill to support safari 12

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,12 @@
+function matchAllPolyfill(string, regex) {
+  const matches = [];
+  string.replace(regex, (...args) => {
+    const matchArray = args.slice(0, -2);
+    matches.push(matchArray);
+    return args[0];
+  });
+  return matches;
+}
 /**
  * These are duplicates from /helpers as helpers uses typescript and we would rather use js to directly use these in browser
  * @param {*} url
@@ -10,7 +19,7 @@ const extractSirenOrSiretSlugFromUrl = (url) => {
   }
   // match a string that ends with either 9 digit or 14 like a siren or a siret
   // we dont use a $ end match as there might be " or %22 at the end
-  const matches = url.matchAll(/\d{14}|\d{9}/g);
+  const matches = matchAllPolyfill(url, /\d{14}|\d{9}/g);
   const m = Array.from(matches, (m) => m[0]);
   if (m && m.length > 0) {
     return m[m.length - 1]; // last occurence of match
