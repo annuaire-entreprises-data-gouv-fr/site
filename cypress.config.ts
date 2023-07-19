8b7aed15cb6bdd902dab8dea2ca8729844db386c
xavier jouppe <xajouppe@gmail.com>
2023-07-19 15:16:14 +0200

    chore: add timeouts

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   video: false,
   e2e: {
     baseUrl: 'http://localhost:3000',
+    video: false,
   },
   userAgent:
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',
