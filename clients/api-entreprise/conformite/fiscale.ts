aea0e480b55dfa53714ae9ab27091d41502d45eb
Thomas COMES <thomas.comes.tech@gmail.com>
2023-09-13 11:53:45 +0200

    Update api-entreprise timeout duration

--- clients/api-entreprise/conformite/fiscale.ts
+++ clients/api-entreprise/conformite/fiscale.ts
@@ -32,7 +32,7 @@ export const clientApiEntrepriseConformiteFiscale = async (siren: Siren) => {
       headers: {
         Authorization: `Bearer ${process.env.API_ENTREPRISE_TOKEN}`,
       },
-      timeout: constants.timeout.M,
+      timeout: constants.timeout.XL,
       params: {
         object: 'espace-agent-public',
         context: 'annuaire-entreprises',
