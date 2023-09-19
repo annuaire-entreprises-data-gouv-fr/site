9e6adb99e959f5500c7d687f1757bcecf661759d
Thomas COMES <thomas.comes.tech@gmail.com>
2023-09-19 11:54:45 +0200

    Cleaned Conformite Component

--- components/espace-agent-components/conformite.tsx
+++ components/espace-agent-components/conformite.tsx
@@ -2,7 +2,35 @@ import { Icon } from "#components-ui/icon/wrapper";
 import { Loader } from "#components-ui/loader";
 import { IAPINotRespondingError, isAPINotResponding } from "#models/api-not-responding";
 import { IConformite } from "#models/espace-agent/donnees-restreintes-entreprise";
+import React from "react";
 import AdministrationInformation from "./administration-information";
+
+const APINotRespongingElement: React.FC<{
+  data: IAPINotRespondingError;
+  administration?: string;
+}> = ({ data, administration }) => {
+  return (
+    <Icon slug="closed">
+      {(data?.errorType === 404 && (
+        <AdministrationInformation
+          str="document non trouvé"
+          administration={administration}
+        />
+      )) ||
+        (data?.errorType === 408 && (
+          <AdministrationInformation
+            str="la récupération du document a pris trop de temps"
+            administration={administration}
+          />
+        )) || (
+          <AdministrationInformation
+            str={`erreur ${data?.errorType}`}
+            administration={administration}
+          />
+        )}
+    </Icon>
+  )
+}
 
 const Conformite: React.FC<{
   data: IConformite | IAPINotRespondingError | undefined;
@@ -13,51 +41,27 @@ const Conformite: React.FC<{
   }
 
   if (isAPINotResponding(data)) {
-    return (
-      <Icon slug="closed">
-        {(data?.errorType === 404 && (
-          <AdministrationInformation
-            str="document non trouvé"
-            administration={administration}
-          />
-        )) ||
-          (data?.errorType === 408 && (
-            <AdministrationInformation
-              str="la récupération du document a pris trop de temps"
-              administration={administration}
-            />
-          )) || (
-            <AdministrationInformation
-              str={`erreur ${data?.errorType}`}
-              administration={administration}
-            />
-          )}
-      </Icon>
-    );
+    return <APINotRespongingElement data={data} administration={administration} />;
   }
 
   return (
     <div className="layout-space-between">
-      {typeof data.isValid === 'boolean' ? (
-        data.isValid ? (
-          <Icon slug="open">
-            <AdministrationInformation
-              str="conforme"
-              administration={administration}
-            />
-          </Icon>
-        ) : (
-          <Icon slug="closed">
-            <AdministrationInformation
-              str="non conforme"
-              administration={administration}
-            />
-          </Icon>
-        )
+      { data.isValid === true ? (
+        <Icon slug="open">
+          <AdministrationInformation
+            str="conforme"
+            administration={administration}
+          />
+        </Icon>
       ) : (
-        <span />
+        <Icon slug="closed">
+          <AdministrationInformation
+            str="non conforme"
+            administration={administration}
+          />
+        </Icon>
       )}
-      {data.url && (
+      { data.url && (
         <a href={data.url}>
           <Icon slug="download">{data.label || 'télécharger'}</Icon>
         </a>
