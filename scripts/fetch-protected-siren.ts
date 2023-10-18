60cfceafdbf047d57855c57b47941307ea592c52
Johan Girod <dev@johangirod.com>
2023-10-18 17:29:39 +0200

    build: improve script fetching siren
    - add a fallback to the currently deployed protected siren list if the
      grist table is not available
    - add a check that the grist table contains at least as many siren as
      the currently deployed protected siren list
    - add a check that the siren fetched from the grist table are valid
    - add various logs message to see what is happening
    - fallback to an empty protected siren list in dev mode if both grist
      and currently deployed protected siren list are not available
    - fail in production if the env variable GRIST_API_KEY is not set
    - run prepare in the load-dependencies action

--- scripts/fetch-protected-siren.ts
+++ scripts/fetch-protected-siren.ts
@@ -1,43 +1,123 @@
+/* eslint-disable no-console */
 import 'dotenv/config';
 import fs from 'fs';
 import path from 'path';
+import { isSiren } from '#utils/helpers';
 
 // Script that fetches the protected siren from the grist table
 // and saves them under /public/protected-siren.txt
-const DOC_ID = 'hp8PLhMGY9sNWuzGDGe6yi';
-const TABLE_ID = 'Protected_siren';
 
-if (!process.env.GRIST_API_KEY) {
-  console.error(
-    `[fetch-protected_siren]: Please set the GRIST_API_KEY environment variable`
+const GRIST_DOC_ID = 'hp8PLhMGY9sNWuzGDGe6yi';
+const GRIST_TABLE_ID = 'Protected_siren';
+
+fetchAndSaveProtectedSiren();
+async function fetchAndSaveProtectedSiren() {
+  if (!process.env.GRIST_API_KEY) {
+    console.error(
+      `[fetch-protected_siren]: GRIST_API_KEY environment variable is not set`
+    );
+    if (process.env.NODE_ENV === 'production') {
+      process.exit(1);
+    }
+  }
+
+  let protectedSiren: string[] | undefined;
+
+  if (process.env.GRIST_API_KEY) {
+    try {
+      console.log(
+        `[fetch-protected_siren]: Fetching protected siren from Grist table...`
+      );
+      protectedSiren = await fetchFromGrist();
+      `[fetch-protected_siren]: Grist table successfully fetched`;
+    } catch (err) {
+      console.error(
+        `[fetch-protected_siren]: Error fetching from grist table (${err})`
+      );
+    }
+  }
+
+  try {
+    let currentlyDeployedProtectedSiren = await fetchFromCurrentlyDeployed();
+
+    // If the protected siren fetched from Grist is shorter than the one
+    if (
+      protectedSiren &&
+      protectedSiren.length < currentlyDeployedProtectedSiren.length
+    ) {
+      console.warn(
+        `[fetch-protected_siren]: Table in Grist contains less protected siren than the currently deployed one.`
+      );
+      console.log(
+        `[fetch-protected_siren]: Please check that no protected siren has been inadvertedly removed.`
+      );
+      protectedSiren = undefined;
+    }
+    if (!protectedSiren) {
+      console.log(
+        `[fetch-protected_siren]: Fallback to currently deployed protected siren`
+      );
+      protectedSiren = currentlyDeployedProtectedSiren;
+    }
+  } catch (err) {
+    console.error(
+      `[fetch-protected_siren]: Error fetching from currently deployed protected siren (${err})`
+    );
+  }
+
+  if (!protectedSiren) {
+    if (process.env.NODE_ENV === 'production') {
+      process.exit(1);
+    } else {
+      console.log(
+        `[fetch-protected_siren]: Fallback to empty protected siren list (dev mode only)`
+      );
+      protectedSiren = [];
+    }
+  }
+
+  fs.writeFileSync(
+    path.join(__dirname, '../public/protected-siren.txt'),
+    protectedSiren.join('\n')
   );
-  process.exit(process.env.NODE_ENV !== 'production' ? 1 : 0);
+
+  console.log(`[fetch-protected_siren]: Done!`);
 }
 
-console.log(
-  `[fetch-protected_siren]: Fetching protected siren from Grist table...`
-);
-fetch(
-  `https://grist.incubateur.net/api/docs/${DOC_ID}/tables/${TABLE_ID}/data`,
-  {
-    headers: {
-      Authorization: 'Bearer ' + process.env.GRIST_API_KEY,
-    },
+async function fetchFromGrist(): Promise<string[]> {
+  const gristData = await (
+    await fetch(
+      `https://grist.incubateur.net/api/docs/${GRIST_DOC_ID}/tables/${GRIST_TABLE_ID}/records`,
+      {
+        headers: {
+          Authorization: 'Bearer ' + process.env.GRIST_API_KEY,
+        },
+      }
+    )
+  ).json();
+
+  if ('error' in gristData) {
+    throw new Error(gristData.error);
   }
-)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(
-      `[fetch-protected_siren]: Saving protected siren to /public/protected-siren.txt...`
-    );
-    const sirenString = data.siren.join('\n');
-    fs.writeFileSync(
-      path.join(__dirname, '../public/protected-siren.txt'),
-      sirenString
-    );
-    console.log(`[fetch-protected_siren]: Done!`);
-  })
-  .catch((err) => {
-    console.error(`[fetch-protected_siren]: ${err}`);
-    process.exit(process.env.NODE_ENV !== 'production' ? 1 : 0);
-  });
+  const gristSiren = gristData.records
+    .map((record: any) => record.fields.siren)
+    .filter(Boolean)
+    .map((number: number) => number.toString());
+
+  if (!gristSiren.every(isSiren)) {
+    throw new Error('some siren are not valid');
+  }
+  return gristSiren;
+}
+
+function fetchFromCurrentlyDeployed(): Promise<string[]> {
+  return fetch(`https://annuaire-entreprises.data.gouv.fr/protected-siren.txt`)
+    .then((res) => res.text())
+    .then((text) => {
+      const sirens = text.trim().split('\n');
+      if (!sirens.every(isSiren)) {
+        throw new Error('invalid response from annuaire-entreprises');
+      }
+      return sirens;
+    });
+}
