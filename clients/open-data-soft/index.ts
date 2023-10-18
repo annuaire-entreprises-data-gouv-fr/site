6101211f62eaed4d890fb4a02b8e2fc64b479249
xavier jouppe <xajouppe@gmail.com>
2023-10-18 15:53:59 +0200

    fix: types in ODS client

--- clients/open-data-soft/index.ts
+++ clients/open-data-soft/index.ts
@@ -6,9 +6,9 @@ import { IODSMetadata, IODSResponse } from './types';
  * Request ODS - Only from backend
  */
 const odsClient = async (
-  search: { url: string; config?: AxiosRequestConfig<unknown> },
+  search: { url: string; config?: AxiosRequestConfig },
   metaDataUrl: string
-): Promise<unknown> => {
+): Promise<any> => {
   const [response, responseMetaData] = await Promise.all([
     httpGet<IODSResponse>(search.url, search.config),
     httpGet<IODSMetadata>(metaDataUrl),
