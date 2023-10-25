76c2caf0b609d963135f869062e9ecca752f3db2
Johan Girod <dev@johangirod.com>
2023-10-25 17:15:52 +0200

    refactor: remove sentry edge config

--- sentry.edge.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-import { isNextJSSentryActivated } from '#utils/sentry';
-if (isNextJSSentryActivated) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    tracesSampleRate: 0.1,
-    maxBreadcrumbs: 0, // dont log breadcrumb
-  });
-}
