1f80e1e44c49a096ea880bd4ebd8e8e6cf089863
Johan Girod <dev@johangirod.com>
2023-11-06 10:40:56 +0100

    feat: add a message when data loading takes long
    fix #722

--- components/section/data-section-loader.tsx
+++ components/section/data-section-loader.tsx
@@ -1,33 +1,48 @@
+import Warning from '#components-ui/alerts/warning';
+import FadeIn from '#components-ui/animation/fade-in';
 import { Loader } from '#components-ui/loader';
 import { IAdministrationMetaData } from '#models/administrations/types';
+import { useTimeout } from 'hooks/use-timeout';
 
 export default function DataSectionLoader({
   dataSources,
 }: {
   dataSources: IAdministrationMetaData[];
 }) {
+  const after5s = useTimeout(5000);
+
   return (
-    <div className="layout-center">
-      {!dataSources ? (
-        <span>
-          Nous récupérons les informations <Loader />
-        </span>
-      ) : dataSources.length === 1 ? (
-        <span>
-          Nous récupérons les informations dans la base de données «{' '}
-          {dataSources[0].long} » <Loader />
-        </span>
-      ) : (
-        <>
-          <Loader /> Nous récupérons les informations dans les bases de données
-          :
-          <ul>
-            {dataSources.map((d) => (
-              <li key={d.slug}>{d.long}</li>
-            ))}
-          </ul>
-        </>
+    <>
+      <div className="layout-center">
+        {!dataSources ? (
+          <span>
+            Nous récupérons les informations <Loader />
+          </span>
+        ) : dataSources.length === 1 ? (
+          <span>
+            Nous récupérons les informations dans la base de données «{' '}
+            {dataSources[0].long} » <Loader />
+          </span>
+        ) : (
+          <>
+            <Loader /> Nous récupérons les informations dans les bases de
+            données :
+            <ul>
+              {dataSources.map((d) => (
+                <li key={d.slug}>{d.long}</li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+      {after5s && (
+        <FadeIn>
+          <Warning>
+            Le service semble particulièrement occupé en ce moment, cela peut
+            prendre encore un peu de temps (10s à 20s).
+          </Warning>
+        </FadeIn>
       )}
-    </div>
+    </>
   );
 }
