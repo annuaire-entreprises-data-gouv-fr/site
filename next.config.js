ef32fa49415f239f25d84c13d0522188ed1324b2
Johan Girod <johan.girod@beta.gouv.fr>
2023-11-06 15:52:06 +0100

    fix: disable sourcemap upload when sentry is down (#757)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 const redirects = require('./redirects.json');
 
+const WITH_SENTRY = process.env.NEXT_PUBLIC_SENTRY_DSN && process.env.NODE_ENV === "production";
+const DISABLE_SOURCEMAP_UPLOAD = process.env.SENTRY_DISABLE_SOURCEMAP_UPLOAD === "true";
+
+if (DISABLE_SOURCEMAP_UPLOAD) {
+  console.warn(`
+  
+--------------------------------------------------------
+WARNING: Building without uploading sourcemap to Sentry
+(probably because https://errors.data.gouv.fr is down)
+--------------------------------------------------------
+
+`)
+}
 const nextjsConfig = {
   webpack: function (config) {
     config.module.rules.push({
@@ -12,13 +25,19 @@ const nextjsConfig = {
   async redirects() {
     return redirects;
   },
-  sentry: {
-    widenClientFileUpload: true,
-  },
+  ...(WITH_SENTRY ? {
+    sentry: {
+      widenClientFileUpload: true,
+      ...(DISABLE_SOURCEMAP_UPLOAD ? {
+        disableServerWebpackPlugin: true,
+        disableClientWebpackPlugin: true,
+      } : {}),
+    }
+  } : {}),
 }
 
 
-module.exports = process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_SENTRY_DSN
+module.exports = WITH_SENTRY
   ? withSentryConfig(nextjsConfig, { silent: true, hideSourceMaps: false, ignore: [] })
   : nextjsConfig;
 
