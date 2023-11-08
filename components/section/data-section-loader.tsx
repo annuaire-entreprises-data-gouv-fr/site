4ae75d70d81bef666555dfc7eb5b7795f4c745ab
Johan Girod <johan.girod@beta.gouv.fr>
2023-11-08 11:58:42 +0100

    chore: move warning message up

--- components/section/data-section-loader.tsx
+++ components/section/data-section-loader.tsx
@@ -1,5 +1,6 @@
 import Warning from '#components-ui/alerts/warning';
 import FadeIn from '#components-ui/animation/fade-in';
+import { HeightTransition } from '#components-ui/animation/height-transition';
 import { Loader } from '#components-ui/loader';
 import { IAdministrationMetaData } from '#models/administrations/types';
 import { useTimeout } from 'hooks/use-timeout';
@@ -10,18 +11,27 @@ export default function DataSectionLoader({
   dataSources: IAdministrationMetaData[];
 }) {
   const after5s = useTimeout(5000);
-
   return (
     <>
+      {after5s && (
+        <HeightTransition animateAppear>
+          <FadeIn>
+            <Warning>
+              Le service semble particulièrement occupé en ce moment, cela peut
+              prendre encore un peu de temps (10s à 20s).
+            </Warning>
+          </FadeIn>
+        </HeightTransition>
+      )}
       {!dataSources ? (
-        <span>
+        <p>
           Nous récupérons les informations <Loader />
-        </span>
+        </p>
       ) : dataSources.length === 1 ? (
-        <span>
+        <p>
           Nous récupérons les informations dans la base de données «{' '}
           {dataSources[0].long} » <Loader />
-        </span>
+        </p>
       ) : (
         <>
           <Loader /> Nous récupérons les informations dans les bases de données
@@ -32,14 +42,6 @@ export default function DataSectionLoader({
             ))}
           </ul>
         </>
-      )}
-      {after5s && (
-        <FadeIn>
-          <Warning>
-            Le service semble particulièrement occupé en ce moment, cela peut
-            prendre encore un peu de temps (10s à 20s).
-          </Warning>
-        </FadeIn>
       )}
     </>
   );
