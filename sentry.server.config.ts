f67c5d691a9fc73d52c0403d118d0966bc480892
Johan Girod <johan.girod@beta.gouv.fr>
2023-11-13 17:11:42 +0100

    chore: improve sentry (#778)

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,9 +1,30 @@
 import * as Sentry from '@sentry/nextjs';
 import { isNextJSSentryActivated } from '#utils/sentry';
+import isUserAgentABot from '#utils/user-agent';
 if (isNextJSSentryActivated) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     tracesSampleRate: 0.005,
     maxBreadcrumbs: 0, // dont log breadcrumb
+    beforeSend(event, hint) {
+      if (!event.request) {
+        return event;
+      }
+
+      const headers = Object.entries(event.request.headers ?? {}).filter(
+        // Remove sensitive headers
+        ([key]) => !key.startsWith('sec-') && !key.startsWith('x-')
+      );
+      event.request.headers = Object.fromEntries(headers);
+
+      if (!event.tags) {
+        event.tags = {};
+      }
+      event.tags['is_bot'] = isUserAgentABot(
+        event.request.headers.userAgent ?? ''
+      );
+
+      return event;
+    },
   });
 }
