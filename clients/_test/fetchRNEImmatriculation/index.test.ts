b2bf1febef7d0047ae39231871b5fb396a514c06
Johan Girod <dev@johangirod.com>
2023-11-13 17:17:00 +0100

    test: disable snapshot test when RNA API is down

--- clients/_test/fetchRNEImmatriculation/index.test.ts
+++ clients/_test/fetchRNEImmatriculation/index.test.ts
@@ -1,32 +1,58 @@
 import { fetchRNEImmatriculation } from '#clients/api-proxy/rne';
+import { IImmatriculationRNE } from '#models/immatriculation';
 import { Siren } from '#utils/helpers';
 import { expectClientToMatchSnapshot } from '../expect-client-to-match-snapshot';
 
+const TIMEOUT_RNE = 60000;
+
+function postProcessResult(result: IImmatriculationRNE) {
+  if (result.metadata.isFallback) {
+    throw new Error(
+      'RNE : official API is down, skipping the test of the fallback version'
+    );
+  }
+}
+
 describe('fetchRNEImmatriculation', () => {
-  it('Should match snapshot for protected uniteLegale with the characteristics (PROTECTED)', async () => {
-    await expectClientToMatchSnapshot({
-      client: fetchRNEImmatriculation,
-      args: ['908595879' as Siren],
-      __dirname,
-      snaphotFile: 'protected.json',
-    });
-  });
+  it(
+    'Should match snapshot for protected uniteLegale with the characteristics (PROTECTED)',
+    async () => {
+      await expectClientToMatchSnapshot({
+        client: fetchRNEImmatriculation,
+        args: ['908595879' as Siren],
+        __dirname,
+        postProcessResult,
+        snaphotFile: 'protected.json',
+      });
+    },
+    TIMEOUT_RNE
+  );
 
-  it('Should match snapshot for protected uniteLegale with the characteristics (RGE)', async () => {
-    await expectClientToMatchSnapshot({
-      client: fetchRNEImmatriculation,
-      args: ['528163777' as Siren],
-      __dirname,
-      snaphotFile: 'rge.json',
-    });
-  });
+  it(
+    'Should match snapshot for protected uniteLegale with the characteristics (RGE)',
+    async () => {
+      await expectClientToMatchSnapshot({
+        client: fetchRNEImmatriculation,
+        args: ['528163777' as Siren],
+        __dirname,
+        postProcessResult,
+        snaphotFile: 'rge.json',
+      });
+    },
+    TIMEOUT_RNE
+  );
 
-  it('Should match snapshot for 356000000 siret', async () => {
-    await expectClientToMatchSnapshot({
-      client: fetchRNEImmatriculation,
-      args: ['356000000' as Siren],
-      __dirname,
-      snaphotFile: 'siret-356000000.json',
-    });
-  }, 30000);
+  it(
+    'Should match snapshot for 356000000 siret',
+    async () => {
+      await expectClientToMatchSnapshot({
+        client: fetchRNEImmatriculation,
+        args: ['356000000' as Siren],
+        __dirname,
+        postProcessResult,
+        snaphotFile: 'siret-356000000.json',
+      });
+    },
+    TIMEOUT_RNE
+  );
 });
