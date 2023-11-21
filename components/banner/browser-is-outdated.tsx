02a5a01e77950fd069d081d789073d9f6d234b48
xavier jouppe <xajouppe@gmail.com>
2023-11-21 19:08:57 +0100

    fix: use innerHTML instead of just changing display

--- components/banner/browser-is-outdated.tsx
+++ components/banner/browser-is-outdated.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { PrintNever } from '#components-ui/print-visibility';
-import constants from '#models/constants';
 
 export const BrowserIsOutdatedBanner: React.FC<{}> = () => (
   <PrintNever>
@@ -8,30 +7,7 @@ export const BrowserIsOutdatedBanner: React.FC<{}> = () => (
       id="browser-is-outdated"
       role="dialog"
       aria-label="Votre navigateur est obsolète"
-    >
-      <div className="fr-container">
-        <h1>⚠️ Votre navigateur est obsolète </h1>
-        <p>
-          La plupart des fonctionalités de ce site <b>ne fonctionneront pas</b>.
-          Vous pourrez essentiellement :
-        </p>
-        <ul>
-          <li>Rechercher une entreprise par son nom ou son SIREN</li>
-          <li>Consulter la fiche résumé de l’entreprise</li>
-        </ul>
-        <p>
-          Avoir un navigateur à jour est{' '}
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href="https://www.ssi.gouv.fr/entreprise/precautions-elementaires/bonnes-pratiques-de-navigation-sur-linternet/"
-          >
-            fortement recommandé par l’ANSSI
-          </a>{' '}
-          pour naviguer sur internet en sécurité.
-        </p>
-      </div>
-    </div>
+    />
 
     <script
       //  Show warning for browsers without Optional chaining
@@ -44,8 +20,8 @@ export const BrowserIsOutdatedBanner: React.FC<{}> = () => (
             eval('var foo = {}; foo?.bar');
           } catch (e) {
             window.IS_OUTDATED_BROWSER = true;
-            document.getElementById('browser-is-outdated').style.display = 'block';
-         
+            document.getElementById('browser-is-outdated').innerHTML = '<div class="fr-container"> <style>#browser-is-outdated{padding-top: 15px; padding-bottom: 15px; font-size: 0.9rem; width: 100%; background-color: #b50800; color: #fff; font-family: "Marianne", sans-serif; border-bottom: 2px solid #000091;}#browser-is-outdated h1{color: #fff; margin-top: 0;}</style> <h1>⚠️ Votre navigateur est obsolète</h1> <p> La plupart des fonctionalités de ce site <b>ne fonctionneront pas</b>. Vous pourrez essentiellement : </p><ul> <li> Rechercher une entreprise par son nom ou son SIREN </li><li>Consulter la fiche résumé de l’entreprise</li></ul> <p> Avoir un navigateur à jour est <a target="_blank" rel="noreferrer noopener" href="https://www.ssi.gouv.fr/entreprise/precautions-elementaires/bonnes-pratiques-de-navigation-sur-linternet/" >fortement recommandé par l’ANSSI</a> pour naviguer sur internet en sécurité. </p></div>';
+
             window._paq = window._paq || [];
             window._paq.push([
                 'trackEvent',
@@ -58,22 +34,5 @@ export const BrowserIsOutdatedBanner: React.FC<{}> = () => (
         `,
       }}
     ></script>
-    <style jsx>{`
-      #browser-is-outdated {
-        padding-top: 15px;
-        padding-bottom: 15px;
-        font-size: 0.9rem;
-        width: 100%;
-        display: none;
-        background-color: #b50800;
-        color: #fff;
-        font-family: 'Marianne', sans-serif;
-        border-bottom: 2px solid ${constants.colors.frBlue};
-      }
-      h1 {
-        color: #fff;
-        margin-top: 0;
-      }
-    `}</style>
   </PrintNever>
 );
