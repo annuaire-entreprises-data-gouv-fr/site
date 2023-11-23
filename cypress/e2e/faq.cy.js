005d92d58c1d78b52012e3b984946304d6fafa32
xavier jouppe <xajouppe@gmail.com>
2023-11-23 16:53:40 +0100

    fix: test e2e

--- cypress/e2e/faq.cy.js
+++ cypress/e2e/faq.cy.js
@@ -5,14 +5,6 @@ describe(`FAQ contextual links`, () => {
       cy.url().should('include', '/faq/modifier-adresse');
     });
   });
-  it('TVA link', () => {
-    cy.visit(`/entreprise/880878145`).then((resp) => {
-      cy.contains('Que signifie “inconnu ou non-assujettie à la TVA” ?').click({
-        force: true,
-      });
-      cy.url().should('include', '/definitions/tva-intracommunautaire');
-    });
-  });
   it('Source de données', () => {
     cy.visit(`/entreprise/880878145`).then((resp) => {
       cy.contains('Sources : Insee').click();
