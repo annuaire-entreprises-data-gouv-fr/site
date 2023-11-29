8ad2ef6008ff485f6772cae6acbf741739012200
xavier jouppe <xajouppe@gmail.com>
2023-11-29 12:32:27 +0100

    chore: update tva snaps

--- clients/_test/clientTVA/index.test.ts
+++ clients/_test/clientTVA/index.test.ts
@@ -4,19 +4,19 @@ import { expectClientToMatchSnapshot } from '../expect-client-to-match-snapshot'
 describe('clientTVA', () => {
   it('Should match snapshot', async () => {
     const testValues = [
-      ['11198100125', 'tva-11198100125.json'],
-      ['29528163777', 'tva-29528163777.json'],
-      ['27552032534', 'tva-27552032534.json'],
-      ['39356000000', 'tva-39356000000.json'],
-      ['43842019051', 'tva-43842019051.json'],
-      ['45300025764', 'tva-45300025764.json'],
+      '11198100125',
+      '29528163777',
+      '27552032534',
+      '39356000000',
+      '43842019051',
+      '45300025764',
     ];
-    for (let [arg, file] of testValues) {
+    for (let arg of testValues) {
       await expectClientToMatchSnapshot({
         client: clientTVA,
         args: [arg],
         __dirname,
-        snaphotFile: file,
+        snaphotFile: `${arg}.json`,
       });
     }
   });
