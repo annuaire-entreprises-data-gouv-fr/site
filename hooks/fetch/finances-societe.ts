104ebf37e3d33036ec005b7269fc33d11cf66656
xavier jouppe <xajouppe@gmail.com>
2023-12-11 19:25:59 +0100

    chore: remove info on 404 for bilan financier

--- hooks/fetch/finances-societe.ts
+++ hooks/fetch/finances-societe.ts
@@ -2,7 +2,7 @@ import { clientBilansFinanciers } from '#clients/open-data-soft/clients/bilans-f
 import { EAdministration } from '#models/administrations';
 import { FetchRessourceException } from '#models/exceptions';
 import { IUniteLegale } from '#models/index';
-import logErrorInSentry, { logInfoInSentry } from '#utils/sentry';
+import logErrorInSentry from '#utils/sentry';
 import { useFetchData } from './use-fetch-data';
 
 export function useFetchFinancesSociete(uniteLegale: IUniteLegale) {
@@ -12,19 +12,17 @@ export function useFetchFinancesSociete(uniteLegale: IUniteLegale) {
       fetchData: () => clientBilansFinanciers(siren),
       administration: EAdministration.MEF,
       logError: (e: any) => {
-        const exception = new FetchRessourceException({
-          ressource: 'BilansFinanciers',
-          administration: EAdministration.MEF,
-          cause: e,
-          context: {
-            siren: uniteLegale.siren,
-          },
-        });
-        if (e.status === 404) {
-          logInfoInSentry(exception);
-        } else {
-          logErrorInSentry(exception);
-        }
+        if (e.status !== 404)
+          logErrorInSentry(
+            new FetchRessourceException({
+              ressource: 'BilansFinanciers',
+              administration: EAdministration.MEF,
+              cause: e,
+              context: {
+                siren: uniteLegale.siren,
+              },
+            })
+          );
       },
     },
     [uniteLegale.siren]
