37b4c332ef57078ed87c715fd8a77b05db548e09
xavier jouppe <xajouppe@gmail.com>
2023-12-15 13:16:55 +0100

    fix: CC unknwon is breaking CC view

--- clients/recherche-entreprise/idcc-metadata.ts
+++ clients/recherche-entreprise/idcc-metadata.ts
@@ -1,18 +1,18 @@
-import { HttpNotFound, HttpServerError } from '#clients/exceptions';
+import { HttpServerError } from '#clients/exceptions';
 import routes from '#clients/routes';
 import { ICCWithMetadata } from '#models/conventions-collectives-list';
 import { httpGet } from '#utils/network';
 
 type IIdccMetadata = {
   [idcc: string]: {
-    'titre de la convention': string; //Convention collective na… des cabinets d'avocats"
-    id_kali: string; //KALICONT000005635185"
-    cc_ti: string; //IDCC"
-    nature: string; //CONVENTION COLLECTIVE NATIONALE"
-    etat: string; //VIGUEUR_ETEN"
-    debut: string; //1979-03-01 00:00:00"
+    'titre de la convention': string; //Convention collective na… des cabinets d'avocats
+    id_kali: string; //KALICONT000005635185
+    cc_ti: string; //IDCC
+    nature: string; //CONVENTION COLLECTIVE NATIONALE
+    etat: string; //VIGUEUR_ETEN
+    debut: string; //1979-03-01 00:00:00
     fin: string | null;
-    url: string; //https://www.legifrance.g…/id/KALICONT000005635185"
+    url: string; //https://www.legifrance.g…/id/KALICONT000005635185
   };
 };
 
@@ -37,7 +37,7 @@ class IdCCMetadataClient {
     if (idcc in this._idccMetadata) {
       return this._idccMetadata[idcc] || {};
     } else {
-      throw new HttpNotFound('Convention collective not found');
+      return null;
     }
   };
 
