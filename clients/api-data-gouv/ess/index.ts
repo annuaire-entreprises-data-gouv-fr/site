3ec5931507ee6538acd9ae9dbf0b20b262fb8d2f
xavier jouppe <xajouppe@gmail.com>
2023-12-19 16:23:29 +0100

    fix: build and use diacritics

--- clients/api-data-gouv/ess/index.ts
+++ clients/api-data-gouv/ess/index.ts
@@ -10,10 +10,10 @@ type IESSItem = {
   "Nom ou raison sociale de l'entreprise": string; //"MAM P'TITE ETOILE";
   "Famille juridique de l'entreprise": string; //'Associations';
   'Code postal': string; //'35390';
-  "Libell\u00e9 de la commune de l'\u00e9tablissement": string; //'GRAND-FOUGERAY';
-  "Code du d\u00e9partement de l'\u00e9tablissement": string; //'35';
-  "D\u00e9partement de l'\u00e9tablissement": string; //'ILLE ET VILAINE';
-  "R\u00e9gion de l'\u00e9tablissement": string; //'BRETAGNE';
+  "Libellé de la commune de l'établissement": string; //'GRAND-FOUGERAY';
+  "Code du département de l'établissement": string; //'35';
+  "Département de l'établissement": string; //'ILLE ET VILAINE';
+  "Région de l'établissement": string; //'BRETAGNE';
 };
 type IESSDatagouvResponse = {
   data: IESSItem[];
@@ -43,6 +43,6 @@ const mapToDomainObject = (response: IESSItem) => {
   return {
     familleJuridique: response["Famille juridique de l'entreprise"],
     nom: response["Nom ou raison sociale de l'entreprise"],
-    region: response["R\u00e9gion de l'\u00e9tablissement"],
+    region: response["Région de l'établissement"],
   };
 };
