8961bf52758be88ef690a3161cb9789ba60e62bf
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2024-01-22 17:39:21 +0100

    Revert "feat: create agent connect oidc client" (#866)

--- pages/api/auth/mon-compte-pro/login.ts
+++ pages/api/auth/mon-compte-pro/login.ts
@@ -1,16 +1,15 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { agentConnectAuthorizeUrl } from '#clients/auth/agent-connect/strategy';
+import { monCompteProAuthorizeUrl } from '#clients/auth/mon-compte-pro/strategy';
 import { logFatalErrorInSentry } from '#utils/sentry';
-import { sessionOptions, setSirenFrom } from '#utils/session';
+import { sessionOptions } from '#utils/session';
 import { AgentConnectionFailedException } from './callback';
 
 export default withIronSessionApiRoute(loginRoute, sessionOptions);
 
-async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
+async function loginRoute(_req: NextApiRequest, res: NextApiResponse) {
   try {
-    await setSirenFrom(req.session, (req?.query?.sirenFrom || '') as string);
-    const url = await agentConnectAuthorizeUrl(req);
+    const url = await monCompteProAuthorizeUrl();
     res.redirect(url);
   } catch (e: any) {
     logFatalErrorInSentry(new AgentConnectionFailedException({ cause: e }));
