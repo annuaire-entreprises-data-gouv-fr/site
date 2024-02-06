9cd4b113d2895793fc29e9413be8cba9c79f03b9
Johan Girod <dev@johangirod.com>
2024-02-06 14:58:20 +0100

    feat: ask for NPS every 6 month
    fix #873

--- components/banner/nps.tsx
+++ components/banner/nps.tsx
@@ -2,45 +2,47 @@ import React from 'react';
 import { PrintNever } from '#components-ui/print-visibility';
 import constants from '#models/constants';
 
-export const NPSBanner: React.FC<{}> = () => (
-  <PrintNever>
-    <div id="nps-modal" role="dialog" aria-label="Donnez-nous votre avis">
-      <div className="fr-container">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
+export const NPSBanner: React.FC<{}> = () => {
+  return (
+    <PrintNever>
+      <div id="nps-modal" role="dialog" aria-label="Donnez-nous votre avis">
+        <div className="fr-container">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `
               <a onclick="window.closeModal('nps-modal')"  href="/formulaire/nps" target="_blank">
                 👍👎 Quel est votre avis sur ce site ?
               </a>`,
-          }}
-        />
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
+            }}
+          />
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `
               <button onclick="window.closeModal('nps-modal')" style="box-shadow:none;font-family: 'Marianne', sans-serif;padding: 0;outline: none;border: none;background-color: transparent;">
                 <strong>Ne plus afficher ce message ✕</strong>
               </button>
         `,
-          }}
-        ></div>
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
-    <style jsx>{`
-      #nps-modal {
-        display: none;
-        padding-top: 15px;
-        padding-bottom: 15px;
-        font-size: 0.9rem;
-        width: 100%;
-        background-color: #fffde6;
-        font-family: 'Marianne', sans-serif;
-        border-bottom: 2px solid ${constants.colors.frBlue};
-      }
-      #nps-modal .fr-container {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-      }
-    `}</style>
-  </PrintNever>
-);
+      <style jsx>{`
+        #nps-modal {
+          display: none;
+          padding-top: 15px;
+          padding-bottom: 15px;
+          font-size: 0.9rem;
+          width: 100%;
+          background-color: #fffde6;
+          font-family: 'Marianne', sans-serif;
+          border-bottom: 2px solid ${constants.colors.frBlue};
+        }
+        #nps-modal .fr-container {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+      `}</style>
+    </PrintNever>
+  );
+};
--- frontend/src/helpers-feedbacks.js
+++ frontend/src/helpers-feedbacks.js
@@ -16,22 +16,47 @@ function init() {
       modal.style.display = 'none';
     }
     try {
-      window.localStorage.setItem(modalId, true);
+      window.localStorage.setItem(modalId, new Date().toISOString());
     } catch (e) {}
   };
 }
 
-function triggerModal(modalId, triggerCount = 1, startsWithString = '/') {
+function triggerModal(
+  modalId,
+  triggerCount = 1,
+  startsWithString = '/',
+  periodicity = undefined
+) {
   var path = window.location.pathname;
 
   var hasAlreadyBeenTriggered;
   try {
-    hasAlreadyBeenTriggered = window.localStorage.getItem(modalId) || false;
+    hasAlreadyBeenTriggered = window.localStorage.getItem(modalId);
   } catch (e) {
-    hasAlreadyBeenTriggered = true;
+    hasAlreadyBeenTriggered = 'true';
   }
-  if (hasAlreadyBeenTriggered) {
+  if (hasAlreadyBeenTriggered === 'true') {
+    if (periodicity) {
+      try {
+        window.localStorage.setItem(modalId, new Date().toISOString());
+      } catch (e) {}
+    }
     return;
+  }
+
+  if (periodicity) {
+    try {
+      var lastTime = window.localStorage.getItem(modalId);
+      if (lastTime) {
+        var lastTimeDate = new Date(lastTime);
+        var currentTime = new Date();
+        var diffInMonths =
+          (currentTime - lastTimeDate) / (1000 * 60 * 60 * 24 * 30);
+        if (diffInMonths < periodicity) {
+          return;
+        }
+      }
+    } catch (e) {}
   }
 
   if (path.indexOf(startsWithString) === -1) {
@@ -51,7 +76,7 @@ function triggerModal(modalId, triggerCount = 1, startsWithString = '/') {
 
 if (typeof window !== 'undefined') {
   init();
-  triggerModal('nps-modal', 2, '/');
+  triggerModal('nps-modal', 2, '/', 6);
 }
 
 // triggerModal('we-need-you-modal', 0, '/');
