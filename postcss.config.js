8fa2bd22842bf0b93702822c01b34d6b9f0fac44
xavier jouppe <xajouppe@gmail.com>
2024-03-06 15:18:07 +0100

    fix: CSS not being purged

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,19 @@
 module.exports = {
-  // plugins.push(autoprefixer());
-  // plugins.push(require('cssnano'));
-  plugins:
-    process.NODE_ENV !== 'production'
-      ? []
-      : [
-          'postcss-flexbugs-fixes',
-          [
-            'postcss-preset-env',
-            {},
-            // {
-            //   autoprefixer: {
-            //     flexbox: 'no-2009',
-            //   },
-            //   stage: 3,
-            //   features: {
-            //     'custom-properties': false,
-            //   },
-            // },
-          ],
-          [
-            '@fullhuman/postcss-purgecss',
-            {
-              css: ['frontend/style/dsfr.min.css'],
-              content: [
-                './pages/**/*.{js,jsx,ts,tsx}',
-                './app/**/*.{js,jsx,ts,tsx}',
-                './components/**/*.{js,jsx,ts,tsx}',
-                './components-ui/**/*.{js,jsx,ts,tsx}',
-              ],
-              safelist: ['html', 'body', 'anchor-link'],
-            },
-          ],
+  plugins: [
+    'postcss-flexbugs-fixes',
+    ['postcss-preset-env', {}],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        css: ['frontend/style/dsfr.min.css'],
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './app/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}',
+          './components-ui/**/*.{js,jsx,ts,tsx}',
         ],
+        safelist: ['html', 'body', 'anchor-link'],
+      },
+    ],
+  ],
 };
