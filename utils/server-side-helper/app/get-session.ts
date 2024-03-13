80f5d7541646811afc9fc30b156d9071a26507ad
Johan Girod <johan.girod@beta.gouv.fr>
2024-03-13 11:44:46 +0100

    refactor: use server action for hide personal data form (#933)
    should fix #926

--- utils/server-side-helper/app/get-session.ts
+++ utils/server-side-helper/app/get-session.ts
@@ -2,6 +2,6 @@ import { getIronSession } from 'iron-session';
 import { cookies } from 'next/headers';
 import { ISession, sessionOptions } from '#utils/session';
 
-export default async function useSessionServer(): Promise<ISession> {
+export default async function getSession(): Promise<ISession> {
   return await getIronSession<ISession>(cookies(), sessionOptions);
 }
