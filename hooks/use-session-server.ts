0d93e62294d6edc9c64c5742bba8948327e4785a
Xavier Jp <xajouppe@gmail.com>
2024-03-13 12:04:54 +0100

    Revert "Revert "refactor: use server action for hide personal data form" (#942)"
    This reverts commit 66b010861d552eb0907434ad957886e865ab9d49.

--- hooks/use-session-server.ts
+++
@@ -1,7 +0,0 @@
-import { getIronSession } from 'iron-session';
-import { cookies } from 'next/headers';
-import { ISession, sessionOptions } from '#utils/session';
-
-export default async function useSessionServer(): Promise<ISession> {
-  return await getIronSession<ISession>(cookies(), sessionOptions);
-}
