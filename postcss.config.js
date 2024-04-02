90f87225ee87f75778e0e952049000df4a71d6a5
xavier jouppe <xajouppe@gmail.com>
2024-04-02 16:42:43 +0200

    chore: deactivate purge css in dev

--- postcss.config.js
+++ postcss.config.js
@@ -1,32 +1,39 @@
-module.exports = {
-  plugins: [
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        css: ['frontend/style/dsfr.min.css'],
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './app/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-          './components-ui/**/*.{js,jsx,ts,tsx}',
-        ],
-        safelist: ['html', 'body', 'anchor-link'],
-        fontFace: true,
-      },
-    ],
-    'postcss-flexbugs-fixes',
-    ['postcss-preset-env', {}],
-    [
-      'cssnano',
-      {
-        preset: [
-          'default',
+const plugins =
+  process.env.NODE_ENV === 'production'
+    ? [
+        [
+          '@fullhuman/postcss-purgecss',
           {
-            discardComments: { removeAll: true },
-            calc: false,
+            css: ['frontend/style/dsfr.min.css'],
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './app/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+              './components-ui/**/*.{js,jsx,ts,tsx}',
+            ],
+            safelist: ['html', 'body', 'anchor-link'],
+            fontFace: true,
           },
         ],
-      },
-    ],
-  ],
+        'postcss-flexbugs-fixes',
+        ['postcss-preset-env', {}],
+        [
+          'cssnano',
+          {
+            preset: [
+              'default',
+              {
+                discardComments: { removeAll: true },
+                calc: false,
+              },
+            ],
+          },
+        ],
+      ]
+    : [['postcss-preset-env', {}]];
+
+console.log(plugins);
+
+module.exports = {
+  plugins,
 };
