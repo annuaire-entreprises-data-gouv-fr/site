6f101f7bbb28dc55428255060b11642c306b37c6
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2024-04-11 13:50:34 +0200

    fix: copy paste in search event (#988)

--- middleware.ts
+++ middleware.ts
@@ -26,11 +26,29 @@ export async function middleware(request: NextRequest) {
     );
   }
 
-  // https://github.com/vercel/next.js/issues/43704#issuecomment-1411186664
+  /**
+   * pathname for /app router RSC
+   */
 
+  // https://github.com/vercel/next.js/issues/43704#issuecomment-1411186664
   // Store current request url in a custom header, which you can read later
   const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('x-pathname', request.nextUrl.pathname);
+  requestHeaders.set('x-pathname', pathname);
+
+  /**
+   * siren redirection logging
+   */
+  const nextUrl = request.nextUrl;
+  const paramIsPresent = nextUrl.search.indexOf('redirected=1') > -1;
+  if (paramIsPresent) {
+    // store redirection status in custom header as referrer seems missing from headers in RSC
+    // isRedirected = params is present + previous page is coming from site
+    const referer = requestHeaders.get('referer') || '';
+    const isFromSite = referer.indexOf('https://annuaire-entreprises') === 0;
+
+    const isRedirected = paramIsPresent && isFromSite ? '1' : '0';
+    requestHeaders.set('x-redirected', isRedirected);
+  }
 
   const response = NextResponse.next({
     request: {
@@ -44,6 +62,7 @@ export async function middleware(request: NextRequest) {
     sessionOptions
   );
   await setVisitTimestamp(session);
+
   return response;
 }
 
