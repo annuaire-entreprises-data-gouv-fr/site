eabf512f6d29601a56ea7d1fd2c9e0fe3bb89226
xavier jouppe <xajouppe@gmail.com>
2024-04-23 11:54:20 +0200

    fix: redirect and try catch dont mix

--- utils/server-side-helper/app/cached-methods.ts
+++ utils/server-side-helper/app/cached-methods.ts
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 import { cache } from 'react';
 import { HttpNotFound } from '#clients/exceptions';
-import { Exception, InternalError } from '#models/exceptions';
+import { Exception } from '#models/exceptions';
 import { extractSirenOrSiretSlugFromUrl } from '#utils/helpers';
 import { logFatalErrorInSentry, logWarningInSentry } from '#utils/sentry';
 import { getEtablissementWithUniteLegaleFromSlug } from '../../../models/core/etablissement';
@@ -15,54 +15,42 @@ import {
 import { getUniteLegaleFromSlug } from '../../../models/core/unite-legale';
 
 const handleException = (e: any, slug: string) => {
-  try {
-    if (
-      e instanceof NotASirenError ||
-      e instanceof NotASiretError ||
-      e instanceof HttpNotFound
-    ) {
-      logWarningInSentry(
-        new Exception({
-          name: 'PageNotFoundException',
-          cause: e,
-          context: { slug },
-        })
-      );
-      redirect('/404');
-    } else if (
-      e instanceof SirenNotFoundError ||
-      e instanceof SiretNotFoundError
-    ) {
-      logWarningInSentry(
-        new Exception({
-          name: 'SirenNotFoundOrInvalid',
-          cause: e,
-          context: { slug },
-        })
-      );
-      redirect('/erreur/introuvable/' + slug);
-    } else if (e instanceof FetchRechercheEntrepriseException) {
-      logFatalErrorInSentry(e);
-      throw e;
-    } else {
-      logFatalErrorInSentry(
-        new Exception({
-          name: 'ServerErrorPageException',
-          cause: e,
-          context: { slug },
-        })
-      );
-    }
-  } catch (e2) {
-    console.error('=== Error-handler failed to handle exception ===');
-    console.error(e2);
-    console.error(e);
-    e.cause = e2;
-    const internalError = new InternalError({
-      message: 'Error-handler failed to handle exception',
-      cause: e,
-    });
-    logFatalErrorInSentry(internalError);
+  if (
+    e instanceof NotASirenError ||
+    e instanceof NotASiretError ||
+    e instanceof HttpNotFound
+  ) {
+    logWarningInSentry(
+      new Exception({
+        name: 'PageNotFoundException',
+        cause: e,
+        context: { slug },
+      })
+    );
+    redirect('/404');
+  } else if (
+    e instanceof SirenNotFoundError ||
+    e instanceof SiretNotFoundError
+  ) {
+    logWarningInSentry(
+      new Exception({
+        name: 'SirenNotFoundOrInvalid',
+        cause: e,
+        context: { slug },
+      })
+    );
+    redirect('/erreur/introuvable/' + slug);
+  } else if (e instanceof FetchRechercheEntrepriseException) {
+    logFatalErrorInSentry(e);
+    throw e;
+  } else {
+    logFatalErrorInSentry(
+      new Exception({
+        name: 'ServerErrorPageException',
+        cause: e,
+        context: { slug },
+      })
+    );
   }
 };
 
