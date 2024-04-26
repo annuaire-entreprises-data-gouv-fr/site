d6ae6539c1cd5377ebd2bf6829711d686ca05070
xavier jouppe <xajouppe@gmail.com>
2024-04-26 10:15:29 +0200

    style: smaller info banner

--- components/banner/info-banner.tsx
+++ components/banner/info-banner.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from '#components-ui/icon/wrapper';
 import { PrintNever } from '#components-ui/print-visibility';
 import { INSEE } from '#components/administrations';
 import constants from '#models/constants';
@@ -10,21 +11,24 @@ export const InfoBanner: React.FC<{}> = () => (
       role="dialog"
       aria-label="Instabilité des services de nos partenaires"
       style={{
-        paddingTop: '15px',
-        paddingBottom: '15px',
+        paddingTop: '10px',
+        paddingBottom: '5px',
         width: '100%',
         backgroundColor: '#e5f3ff',
         borderBottom: `2px solid ${constants.colors.frBlue}`,
+        fontSize: '0.9rem',
       }}
     >
       <div className="fr-container">
-        ℹ️ Suite à la mise à jour{' '}
-        <a href="https://www.data.gouv.fr/fr/datasets/base-sirene-des-entreprises-et-de-leurs-etablissements-siren-siret/">
-          de la base Sirene
-        </a>{' '}
-        de l’
-        <INSEE />, les données des sociétés à mission sont temporairement
-        manquantes.
+        <Icon slug="information" color={constants.colors.frBlue} size={14}>
+          Suite à la mise à jour{' '}
+          <a href="https://www.data.gouv.fr/fr/datasets/base-sirene-des-entreprises-et-de-leurs-etablissements-siren-siret/">
+            de la base Sirene
+          </a>{' '}
+          de l’
+          <INSEE />, les données des sociétés à mission sont temporairement
+          manquantes.
+        </Icon>
       </div>
     </div>
   </PrintNever>
