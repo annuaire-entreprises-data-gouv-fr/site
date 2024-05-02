044ecedc52f0345cc2634106f7fe8a9ea8aede5e
xavier jouppe <xajouppe@gmail.com>
2024-05-02 10:15:46 +0200

    content: CGU

---
+++ app/(header-default)/cgu/page.tsx
@@ -0,0 +1,163 @@
+import { Metadata } from 'next';
+import TextWrapper from '#components-ui/text-wrapper';
+import { NextPageWithLayout } from '../../../pages/_app';
+
+export const metadata: Metadata = {
+  title: 'Conditions générales d’utilisation',
+  robots: 'noindex, nofollow',
+  alternates: {
+    canonical: 'https://annuaire-entreprises.data.gouv.fr/cgu',
+  },
+};
+
+const CGU: NextPageWithLayout = () => (
+  <TextWrapper>
+    <h1>
+      Conditions générales d’utilisation - Annuaire des Entreprises - Agents
+      publics - 29/04/2024
+    </h1>
+    <p>
+      Les présentes conditions générales d’utilisation (dites “CGU”) fixent le
+      cadre juridique de la plateforme numérique “Annuaire des Entreprises” et
+      définissent les conditions d’accès et d’utilisation du Service proposé à
+      l’Utilisateur.
+    </p>
+    <h2>Article 1 - Champ d’application</h2>
+    <p>
+      Le présent document a pour objet d’encadrer l’utilisation du Service qui
+      est d’accès libre et gratuit à tout Utilisateur. Toute utilisation du
+      Service est subordonnée au respect intégral des présentes conditions
+      générales d’utilisation.
+    </p>
+    <h2>Article 2 - Définitions</h2>
+    <p>
+      <strong>“L’Éditeur”</strong> de l’Annuaire des Entreprises est l’équipe
+      chargée de concevoir et de proposer le Service pour le compte la Direction
+      Interministérielle du Numérique (DINUM).
+    </p>
+    <p>
+      <strong>“L’Utilisateur”</strong> désigne tout agent public contractuel ou
+      fonctionnaire qui exerce au sein de la fonction publique d’Etat et des
+      opérateurs de l’Etat disposant d’un compte sur le Service.
+    </p>
+    <p>
+      <strong>“Le Service”</strong> désigne toutes les fonctionnalités offertes
+      par le Service depuis l’espace agent public.
+    </p>
+    <h2>Article 3 - Objet</h2>
+    <p>
+      Le Service a pour objectif de permettre aux Utilisateurs d’accéder à des
+      informations administratives et économiques des entreprises, des
+      associations et des services publics immatriculés dans un registre
+      national.
+    </p>
+    <h2>Article 4 - Fonctionnalités</h2>
+    <h3>4.1 Accès et connexion à l’espace agent public</h3>
+    <p>
+      L’Utilisateur s’authentifie et se connecte à l’espace agent public via le
+      fournisseur d’identité AgentConnect.
+    </p>
+    <p>
+      Le Service ne propose aucun autre mode d’accès à cet espace agent public.
+    </p>
+    <p>
+      L’accès au Service est restreint au seul agent public contractuel ou
+      fonctionnaire qui exerce au sein de la fonction publique d’Etat et des
+      opérateurs de l’Etat
+    </p>
+    <h3>
+      4.2 Accès aux informations légales des sociétés, associations et services
+      publics
+    </h3>
+    <p>
+      L’Utilisateur peut consulter et extraire les informations dont celles
+      dites “non-diffusibles” de l’ensemble des structures disposant d’un numéro
+      de SIRET.
+    </p>
+    <h2>Article 5 - Responsabilités</h2>
+    <h3>5.1 L’Éditeur de le Service</h3>
+    <p>
+      Les sources des informations diffusées sur le Service sont réputées
+      fiables mais l’Editeur ne garantit pas qu’elle soit exempte de défauts,
+      d’erreurs ou d’omissions.
+    </p>
+    <p>
+      L’Éditeur s’autorise à suspendre ou révoquer n’importe quel compte et
+      toutes les actions réalisées par ce biais, s’il estime que l’usage réalisé
+      du Service porte préjudice à son image ou ne correspond pas aux exigences
+      de sécurité.
+    </p>
+    <p>
+      L’Éditeur s’engage à la sécurisation du Service, notamment en prenant
+      toutes les mesures nécessaires permettant de garantir la sécurité et la
+      confidentialité des informations fournies.
+    </p>
+    <p>
+      L’Éditeur fournit les moyens nécessaires et raisonnables pour assurer un
+      accès continu, sans contrepartie financière, au Service.
+    </p>
+    <p>
+      L’Éditeur ne peut être tenu responsable des pertes et/ou préjudices, de
+      quelque nature qu’ils soient, qui pourraient être causés à la suite d’un
+      dysfonctionnement ou d’une indisponibilité du Service. De telles
+      situations n’ouvriront droit à aucune compensation ou indemnité d’aucune
+      nature dont financière.
+    </p>
+    <p>
+      L’Éditeur enrichit au fur et à mesure les sources de données de l’Annuaire
+      des Entreprises. La responsabilité de l’Éditeur ne peut en aucun cas être
+      recherchée en cas de suppression ou d’indisponibilité d’une base de
+      données, d’une API ou d’une quelconque source de données.
+    </p>
+    <h3>5.2 L’Utilisateur</h3>
+    <p>
+      L’Utilisateur s’assure de garder son mot de passe secret. Toute
+      divulgation du mot de passe, quelle que soit sa forme, est interdite. Il
+      assume les risques liés à l’utilisation de son identifiant et mot de
+      passe.
+    </p>
+    <p>
+      Il est rappelé que les Utilisateurs doivent observer une obligation de
+      secret professionnel notamment prévu par l’article L. 121-6 du code
+      général de la fonction publique et l’article 1-1 du décret n° 86-83 du 17
+      janvier 1986.
+    </p>
+    <p>
+      L’Utilisateur veille à ne pas commercialiser, diffuser publiquement ou à
+      communiquer à des tiers qui n’ont pas à en connaître les informations et
+      données du Service qui sont couvertes par un secret protégé par la loi (ex
+      : secret des affaires). Il s’engage à stocker les éventuelles extractions
+      de données et informations du Service dans des conditions qui permettent
+      la protection de leur intégrité, sécurité et confidentialité. Cette
+      obligation d’assurer l’intégrité, la confidentialité et la sécurité
+      perdure aussi longtemps que les informations ou données sont protégées par
+      un secret.
+    </p>
+    <p>
+      L’Utilisateur s’engage à ne pas mettre en ligne de contenus ou
+      informations contraires aux dispositions légales et réglementaires en
+      vigueur.
+    </p>
+    <p>
+      Il est rappelé que toute personne procédant à une fausse déclaration pour
+      elle-même ou pour autrui s’expose, notamment, aux sanctions prévues à
+      l’article 441-1 du code pénal, prévoyant des peines pouvant aller jusqu’à
+      trois ans d’emprisonnement et 45 000 euros d’amende.
+    </p>
+    <p>
+      L’Éditeur se réserve le droit de suspendre l’accès temporairement ou
+      définitivement à un Utilisateur qui ne respecterait pas les présentes
+      conditions générales d’utilisation. Et le cas échéant, à informer
+      l’employeur de l’Utilisateur des désordres et / ou comportements observés.
+    </p>
+    <h2>Article 6 - Mise à jour des conditions générales d’utilisation</h2>
+    <p>
+      Les termes des présentes conditions générales d’utilisation peuvent être
+      amendés à tout moment, sans préavis, en fonction des modifications
+      apportées à le Service, de l’évolution de la législation ou pour tout
+      autre motif jugé nécessaire.
+    </p>
+  </TextWrapper>
+);
+
+export default CGU;
