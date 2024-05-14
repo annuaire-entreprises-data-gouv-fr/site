c523739b35be44f7aad613cdfc79d25cf2e0b21d
xavier jouppe <xajouppe@gmail.com>
2024-05-14 08:55:08 +0200

    style: visual agent distinction for protected badges

--- components/badges-section/labels-and-certificates/protected-certificats.tsx
+++ components/badges-section/labels-and-certificates/protected-certificats.tsx
@@ -1,4 +1,6 @@
+import { Icon } from '#components-ui/icon/wrapper';
 import { isAPINotResponding } from '#models/api-not-responding';
+import constants from '#models/constants';
 import { IProtectedCertificatsEntreprise } from '#models/espace-agent/certificats';
 import { IUniteLegale } from '../../../models/core/types';
 import { LabelWithLinkToSection } from './label-with-link-to-section';
@@ -13,31 +15,41 @@ export const ProtectedCertificatesBadgesSection: React.FC<{
   const { opqibi, qualibat, qualifelec } = protectedCertificats;
 
   return (
-    <>
-      {!isAPINotResponding(opqibi) && (
-        <LabelWithLinkToSection
-          informationTooltipLabel="Cette structure possède une certification délivrée par l'association OPQIBI, attestant de ses différentes qualifications d'ingénierie"
-          label="OPQIBI - Ingénierie"
-          sectionId="opqibi"
-          siren={uniteLegale.siren}
-        />
-      )}
-      {!isAPINotResponding(qualibat) && (
-        <LabelWithLinkToSection
-          informationTooltipLabel="Cette structure a obtenue un label de fiabilité QUALIBAT, garantissant sa qualification dans le bâtiment"
-          label="QUALIBAT - Bâtiment"
-          sectionId="qualibat"
-          siren={uniteLegale.siren}
-        />
-      )}
-      {!isAPINotResponding(qualifelec) && (
-        <LabelWithLinkToSection
-          informationTooltipLabel="Cette structure est certifiée par QUALIFELEC, attestant de ses qualifications dans le domaine du génie électrique et énergétique"
-          label="QUALIFELEC - Génie électrique"
-          sectionId="qualifelec"
-          siren={uniteLegale.siren}
-        />
-      )}
-    </>
+    <div
+      style={{
+        borderRadius: '30px',
+        border: `2px solid ${constants.colors.espaceAgentPastel}`,
+        padding: '3px',
+        paddingLeft: '8px',
+        display: 'inline-block',
+      }}
+    >
+      <Icon slug="lockFill" color={constants.colors.espaceAgent}>
+        {!isAPINotResponding(opqibi) && (
+          <LabelWithLinkToSection
+            informationTooltipLabel="Cette structure possède une certification délivrée par l'association OPQIBI, attestant de ses différentes qualifications d'ingénierie"
+            label="OPQIBI - Ingénierie"
+            sectionId="opqibi"
+            siren={uniteLegale.siren}
+          />
+        )}
+        {!isAPINotResponding(qualibat) && (
+          <LabelWithLinkToSection
+            informationTooltipLabel="Cette structure a obtenue un label de fiabilité QUALIBAT, garantissant sa qualification dans le bâtiment"
+            label="QUALIBAT - Bâtiment"
+            sectionId="qualibat"
+            siren={uniteLegale.siren}
+          />
+        )}
+        {!isAPINotResponding(qualifelec) && (
+          <LabelWithLinkToSection
+            informationTooltipLabel="Cette structure est certifiée par QUALIFELEC, attestant de ses qualifications dans le domaine du génie électrique et énergétique"
+            label="QUALIFELEC - Génie électrique"
+            sectionId="qualifelec"
+            siren={uniteLegale.siren}
+          />
+        )}
+      </Icon>
+    </div>
   );
 };
