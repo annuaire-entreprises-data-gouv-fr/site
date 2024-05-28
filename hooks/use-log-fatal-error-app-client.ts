f4d89934f602445455f3113755f6c006153af96c
Johan Girod <dev@johangirod.com>
2024-05-28 12:37:45 +0200

    chore: log separately error that result in a 500 error page displayed

--- hooks/use-log-fatal-error-app-client.ts
+++ hooks/use-log-fatal-error-app-client.ts
@@ -2,12 +2,29 @@ import { useEffect } from 'react';
 import { Exception } from '#models/exceptions';
 import { logFatalErrorInSentry } from '#utils/sentry';
 export type NextAppError = Error & { digest?: string };
+
 export function useLogFatalErrorAppClient(error: Error & { digest?: string }) {
   useEffect(() => {
     // Log the error to Sentry
     logFatalErrorInSentry(
       new Exception({
         name: 'ClientErrorPageDisplayed',
+        cause: error,
+        context: {
+          digest: error.digest,
+          page: window.location.pathname,
+        },
+      })
+    );
+  }, [error]);
+}
+
+export function useLog500ErrorAppClient(error: Error & { digest?: string }) {
+  useEffect(() => {
+    // Log the error to Sentry
+    logFatalErrorInSentry(
+      new Exception({
+        name: 'Client500PageDisplayed',
         cause: error,
         context: {
           digest: error.digest,
