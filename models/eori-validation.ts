d0d61464838e53ea8cb999d788e4620f88237626
xavier jouppe <xajouppe@gmail.com>
2024-06-27 15:46:55 +0200

    feat: improve model error for EORI and TVA

--- models/eori-validation.ts
+++ models/eori-validation.ts
@@ -1,10 +1,12 @@
 import { clientEORI } from '#clients/api-proxy/eori';
 import { verifySiret } from '#utils/helpers';
+import logErrorInSentry from '#utils/sentry';
 import { EAdministration } from './administrations/EAdministration';
 import {
   APINotRespondingFactory,
   IAPINotRespondingError,
 } from './api-not-responding';
+import { FetchRessourceException } from './exceptions';
 
 export type IEORIValidation = {
   eori: string;
@@ -18,11 +20,17 @@ export const getEORIValidation = async (
     const siret = verifySiret(eori);
     const data = await clientEORI(siret);
     if (!data) {
-      return APINotRespondingFactory(EAdministration.DOUANES, 500);
+      throw new Error('EOS response is empty');
     }
     return data;
   } catch (e: any) {
-    console.error(e);
+    logErrorInSentry(
+      new FetchRessourceException({
+        ressource: 'EORIValidation',
+        cause: e,
+        context: { slug: eori },
+      })
+    );
     return APINotRespondingFactory(EAdministration.DOUANES, e.status || 500);
   }
 };
--- models/tva/verify.ts
+++ models/tva/verify.ts
@@ -1,11 +1,13 @@
-import { TVAUserException, clientTVA } from '#clients/api-proxy/tva';
+import { clientTVA } from '#clients/api-proxy/tva';
 import { HttpNotFound } from '#clients/exceptions';
 import { EAdministration } from '#models/administrations/EAdministration';
 import {
   APINotRespondingFactory,
   IAPINotRespondingError,
 } from '#models/api-not-responding';
+import { FetchRessourceException } from '#models/exceptions';
 import { verifySiren, verifyTVANumber } from '#utils/helpers';
+import logErrorInSentry from '#utils/sentry';
 import { tvaNumber } from './utils';
 
 export const buildAndVerifyTVA = async (
@@ -17,14 +19,16 @@ export const buildAndVerifyTVA = async (
   try {
     return { tva: await clientTVA(tvaNumberFromSiren) };
   } catch (e: any) {
-    if (e instanceof TVAUserException) {
-      throw e;
-    }
-
-    // no need to log an error as API-Proxy already logged it
+    logErrorInSentry(
+      new FetchRessourceException({
+        ressource: 'TVAValidation',
+        cause: e,
+        context: { slug },
+      })
+    );
     if (e instanceof HttpNotFound) {
       return APINotRespondingFactory(EAdministration.VIES, 404);
     }
-    return APINotRespondingFactory(EAdministration.VIES, 500);
+    return APINotRespondingFactory(EAdministration.VIES, e?.status || 500);
   }
 };
