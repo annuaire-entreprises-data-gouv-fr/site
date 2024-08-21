0f64511525255bdd254c38168e992ac686056454
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2024-08-21 16:27:18 +0200

    refactor: move fallback rne call to site rather than proxy (#1194)
    * refactor: move fallback rne call to site rather than proxy
    
    * test: e2e snaps
    
    * fix: route formatting

--- clients/_test/fetchRNEImmatriculation/index.test.ts
+++ clients/_test/fetchRNEImmatriculation/index.test.ts
@@ -1,17 +1,9 @@
 import { fetchRNEImmatriculation } from '#clients/api-proxy/rne';
-import { IImmatriculationRNE } from '#models/immatriculation';
+import { fetchRNEImmatriculationFallback } from '#clients/api-proxy/rne/fallback';
 import { Siren } from '#utils/helpers';
 import { expectClientToMatchSnapshot } from '../expect-client-to-match-snapshot';
 
 const TIMEOUT_RNE = 90000;
-
-function postProcessResult(result: IImmatriculationRNE) {
-  if (result.metadata.isFallback) {
-    throw new Error(
-      'RNE : official API is down, skipping the test of the fallback version'
-    );
-  }
-}
 
 describe('fetchRNEImmatriculation', () => {
   it(
@@ -21,7 +13,6 @@ describe('fetchRNEImmatriculation', () => {
         client: fetchRNEImmatriculation,
         args: ['908595879' as Siren],
         __dirname,
-        postProcessResult,
         snaphotFile: 'protected.json',
       });
     },
@@ -35,7 +26,6 @@ describe('fetchRNEImmatriculation', () => {
         client: fetchRNEImmatriculation,
         args: ['487444697' as Siren],
         __dirname,
-        postProcessResult,
         snaphotFile: 'rge.json',
       });
     },
@@ -49,8 +39,22 @@ describe('fetchRNEImmatriculation', () => {
         client: fetchRNEImmatriculation,
         args: ['356000000' as Siren],
         __dirname,
-        postProcessResult,
         snaphotFile: 'siret-356000000.json',
+      });
+    },
+    TIMEOUT_RNE
+  );
+});
+
+describe('fetchRNEImmatriculationFallback', () => {
+  it(
+    'Should match snapshot for protected uniteLegale with the characteristics (RGE)',
+    async () => {
+      await expectClientToMatchSnapshot({
+        client: fetchRNEImmatriculationFallback,
+        args: ['487444697' as Siren],
+        __dirname,
+        snaphotFile: 'rge.json',
       });
     },
     TIMEOUT_RNE
--- models/immatriculation/rne.ts
+++ models/immatriculation/rne.ts
@@ -1,5 +1,6 @@
 import { fetchRNEImmatriculation } from '#clients/api-proxy/rne';
 import { clientDocuments } from '#clients/api-proxy/rne/documents';
+import { fetchRNEImmatriculationFallback } from '#clients/api-proxy/rne/fallback';
 import { HttpNotFound } from '#clients/exceptions';
 import routes from '#clients/routes';
 import { EAdministration } from '#models/administrations/EAdministration';
@@ -19,34 +20,35 @@ export const getImmatriculationRNE = async (
 ): Promise<IAPINotRespondingError | IImmatriculationRNE> => {
   const siren = verifySiren(maybeSiren);
 
-  try {
-    // fetch RNE and use cache
-    const {
-      identite,
-      metadata,
-      dirigeants,
-      beneficiaires,
-      observations = [],
-    } = await fetchRNEImmatriculation(siren);
+  const sirenAndLinks = {
+    siren,
+    downloadLink: `${routes.rne.portail.pdf}?format=pdf&ids=[%22${siren}%22]`,
+    siteLink: `${routes.rne.portail.entreprise}${siren}`,
+  };
 
+  try {
     return {
-      siren,
-      downloadLink: `${routes.rne.portail.pdf}?format=pdf&ids=[%22${siren}%22]`,
-      siteLink: `${routes.rne.portail.entreprise}${siren}`,
-      identite,
-      dirigeants,
-      beneficiaires,
-      observations,
-      metadata,
+      ...(await fetchRNEImmatriculation(siren)),
+      ...sirenAndLinks,
     };
-  } catch (e: any) {
-    if (e instanceof HttpNotFound) {
+  } catch (eDefaultTry: any) {
+    if (eDefaultTry instanceof HttpNotFound) {
       return APINotRespondingFactory(EAdministration.INPI, 404);
     }
 
-    // no need to log an error as API-Proxy already logged it
+    try {
+      return {
+        ...(await fetchRNEImmatriculationFallback(siren)),
+        ...sirenAndLinks,
+      };
+    } catch (eFallback: any) {
+      if (eFallback instanceof HttpNotFound) {
+        return APINotRespondingFactory(EAdministration.INPI, 404);
+      }
 
-    return APINotRespondingFactory(EAdministration.INPI, 500);
+      // no need to log an error as API-Proxy already logged it
+      return APINotRespondingFactory(EAdministration.INPI, 500);
+    }
   }
 };
 
@@ -64,7 +66,6 @@ export async function getDocumentsRNEProtected(maybeSiren: string) {
     }
 
     // no need to log an error as API-Proxy already logged it
-
     return APINotRespondingFactory(EAdministration.INPI, 500);
   }
 }
