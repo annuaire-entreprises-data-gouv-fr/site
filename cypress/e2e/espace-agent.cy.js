66f05201d4ab882f10d0c81dd00a37c6b8b5cde3
Johan Girod <dev@johangirod.com>
2024-09-10 09:39:27 +0200

    chore: reactive E2E test for agent
    fix #1123
    
    # Conflicts:
    #	cypress/e2e/certifications.cy.js

--- cypress/e2e/espace-agent.cy.js
+++ cypress/e2e/espace-agent.cy.js
@@ -4,15 +4,10 @@ describe(
     defaultCommandTimeout: 10000, // 10 sec
     pageLoadTimeout: 180000, // 3min because AgentConnect testing env is veeeery slow
     testIsolation: false, // We need to share session cookies between tests
-    retries: {
-      runMode: 2,
-    },
   },
   () => {
-    before(() => {
-      cy.clearCookies();
-    });
     it("Page d'accueil", () => {
+      cy.clearCookies();
       cy.visit(`/`);
       cy.contains('Espace agent')
         // The element is present twice (mobile and desktop menu).
@@ -28,39 +23,40 @@ describe(
       cy.contains('button', 'ProConnect');
     });
 
-    // it('Connexion', () => {
-    //   cy.visit(`/lp/agent-public`);
-    //   cy.contains('button', 'AgentConnect').click();
-    //   cy.origin('fca.integ01.dev-agentconnect.fr', () => {
-    //     cy.get('input[type="email"]').clear().type('user@yopmail.com');
-    //     cy.contains('button', 'Se connecter').click();
-    //   });
+    it('Connexion', () => {
+      cy.clearCookies();
+      cy.visit(`/lp/agent-public`);
+      cy.contains('button', 'AgentConnect').click();
+      cy.origin('fca.integ01.dev-agentconnect.fr', () => {
+        cy.get('input[type="email"]').clear().type('user@yopmail.com');
+        cy.contains('button', 'Se connecter').click();
+      });
 
-    //   cy.origin('app-sandbox.moncomptepro.beta.gouv.fr', () => {
-    //     cy.contains('Renseignez votre mot de passe');
-    //     cy.get('input[type="password"]').type('user@yopmail.com');
-    //     cy.contains('button', 'S’identifier').click();
-    //     cy.contains('DINUM').click();
-    //   });
+      cy.origin('app-sandbox.moncomptepro.beta.gouv.fr', () => {
+        cy.contains('Renseignez votre mot de passe');
+        cy.get('input[type="password"]').type('user@yopmail.com');
+        cy.contains('button', 'S’identifier').click();
+        cy.contains('DINUM').click();
+      });
 
-    //   cy.location().should((loc) => {
-    //     expect(loc.pathname).to.eq('/');
-    //   });
-    //   cy.contains('user@yopmail.com');
-    // });
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/');
+      });
+      cy.contains('user@yopmail.com');
+    });
 
-    // it('Actes et statuts accessibles', () => {
-    //   cy.visit('/documents/487444697');
-    //   cy.contains('Actes et statuts');
-    //   cy.contains(/Cette entreprise possède [\d]+ document\(s\) au RNE/);
-    // });
+    it('Actes et statuts accessibles', () => {
+      cy.visit('/documents/487444697');
+      cy.contains('Actes et statuts');
+      cy.contains(/Cette entreprise possède [\d]+ document\(s\) au RNE/);
+    });
 
-    // it('Bilans accessibles', () => {
-    //   cy.visit('/donnees-financieres/487444697');
-    //   cy.contains('Bilans');
-    //   cy.contains(
-    //     /Cette entreprise possède [\d]+ bilan\(s\) déposé\(s\) au RNE/
-    //   );
-    // });
+    it('Bilans accessibles', () => {
+      cy.visit('/donnees-financieres/487444697');
+      cy.contains('Bilans');
+      cy.contains(
+        /Cette entreprise possède [\d]+ bilan\(s\) déposé\(s\) au RNE/
+      );
+    });
   }
 );
