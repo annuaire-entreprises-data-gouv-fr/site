b5d08cb9c6dcd6e0cabdbefca473c765823b9da2
xavier jouppe <xajouppe@gmail.com>
2024-09-18 18:32:03 +0200

    feat: update budget

--- app/(header-default)/a-propos/budget/page.tsx
+++ app/(header-default)/a-propos/budget/page.tsx
@@ -32,7 +32,8 @@ export default function Budget() {
             <a href="https://numerique.gouv.fr/" target="_blank" rel="noopener">
               Direction Interministérielle du Numérique (DINUM)
             </a>{' '}
-            au titre de ses missions d’ouverture et de circulation de la donnée.
+            au titre de ses missions d’innovation, d’ouverture et de circulation
+            de la donnée.
           </li>
           <li>
             le projet a bénéficié en 2022 du programme{' '}
@@ -67,7 +68,7 @@ export default function Budget() {
             ['Déploiement', '5 000 €', '98 000 €', '157 000 €', '255 000 €'],
             ['Design', '25 000 €', '25 000 €', '43 000 €', '60 000 €'],
             ['Logiciels', '-', '12 000 €', '12 000 €', '12 000'],
-            ['Sécurité', '-', '-', '-', '15 000 €'],
+            ['Expertise sécurité', '-', '-', '-', '15 000 €'],
             ['Hébergement', '1000 €', '3000 €', '3000 €', '25 000 €'],
             [
               <strong>Total TTC</strong>,
@@ -78,40 +79,71 @@ export default function Budget() {
             ],
           ]}
         ></FullTable>
+        <p>
+          <strong>NB :</strong> contrairement aux entreprises du secteur privé,
+          les administrations ne peuvent pas récupérer la TVA supportée sur
+          leurs achats dans le cadre de leur activité. Le montant TTC inclut la
+          TVA au taux de 20%. La TVA est collectée et reversée à l’État et
+          diminue donc le montant du budget utilisable sur le projet.
+        </p>
         <h2>Impact</h2>
         <p>
-          Mise en regard de nos dépenses et de notre impact (les statistiques
-          2024 sont des estimations):
+          Mise en regard des dépenses au vu des mesures d’impact du projet (les
+          statistiques 2024 sont des estimations) :
         </p>
         <FullTable
           head={[
-            'Mesure d’impact',
+            'Mesure d’impact pour le grand public',
             '2021',
             '2022',
             '2023',
             '2024 (prévisionnel)',
           ]}
           body={[
-            ['Pages “entreprise” consultées', '768k', '3M', '13M', '40M'],
-            [' ‣ € / page', '0,25 €', '0,11 €', '0,04 €', '0,02 €'],
-            ['Visiteurs uniques', '156k', '781k', '3,7M', '12M'],
-            [' ‣ € / visiteur unique', '1,2 €', '0,45 €', '0,15 €', '0,06 €'],
-            ['Agents publics', '-', '-', '-', '40k'],
-            [' ‣ € / agent public / an', '-', '-', '-', '20 €'],
+            [
+              'Fiche “entreprise” consultées',
+              '768 000',
+              '2 788 000',
+              '12 934 000',
+              '40 000 000',
+            ],
+            ['・€ / page', '0,25 €', '0,11 €', '0,04 €', '0,02 €'],
+            [
+              'Visiteurs uniques',
+              '156 000',
+              '781 000',
+              '3 775 000',
+              '12 000 000',
+            ],
+            ['・€ / visiteur unique', '1,2 €', '0,45 €', '0,15 €', '0,06 €'],
+          ]}
+        ></FullTable>
+        <br />
+        <p>
+          En 2024, la DINUM a créé un{' '}
+          <a href="/lp/agent-public">compte pour les agents public</a>. Ce
+          compte permet aux agents publics d’accèder à des données
+          complémentaires sur les entreprises. Cela{' '}
+          <strong>facilite leur travail</strong> et leur{' '}
+          <strong>
+            évite de redemander ces données aux entreprises elles-mêmes
+          </strong>
+          .
+        </p>
+        <FullTable
+          head={[
+            'Mesure d’impact pour les agents publics',
+            '2024 (prévisionnel)',
+          ]}
+          body={[
+            ['Fiches “entreprise” consultées par des agents', '480 000'],
+            ['Agents publics uniques', '45 000'],
+            ['・€ / agent public / mois', '1,6 €'],
           ]}
         ></FullTable>
         <p>
           Le détail de nos mesures d’impact est disponible sur notre{' '}
           <a href="/a-propos/stats">page de statistiques</a>.
-        </p>
-        <h2>À propos de la TVA</h2>
-        <p>
-          Contrairement aux entreprises du secteur privé, les administrations ne
-          peuvent pas récupérer la TVA supportée sur leurs achats dans le cadre
-          de leur activité. Le montant TTC inclut la TVA au taux de 20%.
-          <br />
-          La TVA est collectée et reversée à l’État et diminue donc le montant
-          du budget utilisable sur le projet.
         </p>
       </TextWrapper>
     </div>
