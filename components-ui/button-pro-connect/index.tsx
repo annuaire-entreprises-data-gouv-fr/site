e80e462f521429f8f9f25edfa61907e879125955
Robin Monnier <monnier.robin@gmail.com>
2024-10-02 19:25:25 +0200

    feat: add link Qu'est-ce que ProConnect

--- components-ui/button-pro-connect/index.tsx
+++ components-ui/button-pro-connect/index.tsx
@@ -38,6 +38,16 @@ const ButtonProConnect: React.FC<IProps> = ({
           <span className="fr-connect__login">S’identifier avec</span>
           <span className="fr-connect__brand">ProConnect</span>
         </button>
+        <p>
+          <a
+            href="https://www.proconnect.gouv.fr/"
+            target="_blank"
+            rel="noopener"
+            title="Qu'est-ce que ProConnect ? - nouvelle fenêtre"
+          >
+            Qu’est-ce que ProConnect ?
+          </a>
+        </p>
       </div>
     </form>
   );
