055e778659b79e7a51cacbdeaa4c6bada1b89d62
Robin Monnier <monnier.robin@gmail.com>
2024-10-04 11:33:12 +0200

    fix: good redirect after agent public login (#1239)

--- hooks/use-path-server.ts
+++
@@ -1,7 +0,0 @@
-import { headers } from 'next/headers';
-
-export default function usePathServer(): string {
-  const headersList = headers();
-  // read the custom x-url header
-  return headersList.get('x-pathname') || '';
-}
