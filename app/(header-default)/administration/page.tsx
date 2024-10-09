f44cf6aee78d614d8790ab3cf1ba1a467d5df3a6
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2024-10-09 12:48:38 +0200

    fix: incorrect 500 in static pages (#1257)
    * fix: incorrect 500 in static pages
    
    * fix: build
    
    * Update app/(header-default)/administration/[slug]/page.tsx
    
    Co-authored-by: Robin Monnier <monnier.robin@gmail.com>
    Signed-off-by: Xavier Jp <xajouppe@gmail.com>
    
    ---------
    
    Signed-off-by: Xavier Jp <xajouppe@gmail.com>
    Co-authored-by: Robin Monnier <monnier.robin@gmail.com>

---
+++ app/(header-default)/administration/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from 'next';
+import React from 'react';
+import TextWrapper from '#components-ui/text-wrapper';
+import AdministrationDescription from '#components/administrations/administration-description';
+import { administrationsMetaData } from '#models/administrations';
+
+export const metadata: Metadata = {
+  title: 'Liste des administrations partenaires de l’Annuaire des Entreprises',
+  alternates: {
+    canonical: 'https://annuaire-entreprises.data.gouv.fr/administration',
+  },
+};
+
+const AllAdmnistrationsPage = () => (
+  <TextWrapper>
+    <h1>Administrations partenaires</h1>
+    <p>
+      L’Annuaire des Entreprises est conçu en partenariat avec{' '}
+      {Object.values(administrationsMetaData).length} administrations
+      différentes, qui nous transmettent les données qu’elles possèdent sur les
+      entreprises, les associations ou les services publics&nbsp;:
+    </p>
+    {Object.values(administrationsMetaData).map(({ slug }) => (
+      <React.Fragment key={slug}>
+        <AdministrationDescription slug={slug} />
+      </React.Fragment>
+    ))}
+  </TextWrapper>
+);
+
+export default AllAdmnistrationsPage;
