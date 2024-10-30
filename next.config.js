72b17af1904403274827cb43a43f3827fb874494
Robin Monnier <monnier.robin@gmail.com>
2024-10-30 12:08:12 +0100

    fix: scalingo predeploy

--- next.config.js
+++ next.config.js
@@ -27,7 +27,9 @@ const nextjsConfig = {
     return config;
   },
   // https://github.com/nexdrew/next-build-id
-  generateBuildId: () => nextBuildId({ dir: __dirname }),
+  // if Scalingo is deploying, SOURCE_VERSION is set to be the lastest git commit hash
+  generateBuildId: () =>
+    process.env.SOURCE_VERSION || nextBuildId({ dir: __dirname }),
   async redirects() {
     return redirects;
   },
