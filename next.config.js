f114fa57ff9ed5fd3b4f6153ba4374b22e6b92cc
Robin Monnier <monnier.robin@gmail.com>
2024-11-08 11:18:37 +0100

    fix: improve sentry config 2

--- next.config.js
+++ next.config.js
@@ -59,11 +59,15 @@ const nextjsConfig = {
 
 module.exports = WITH_SENTRY
   ? withSentryConfig(nextjsConfig, {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      sentryUrl: process.env.SENTRY_URL,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+
       // Only print logs for uploading source maps in CI
       silent: !process.env.CI,
-
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
       /**
        * Options related to react component name annotations.
