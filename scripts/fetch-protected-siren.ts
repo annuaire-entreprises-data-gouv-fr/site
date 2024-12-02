ce5c7c849f7ef454aac6caf8d296d08379585a8d
xavier jouppe <xajouppe@gmail.com>
2024-12-02 14:43:16 +0100

    fix: protected-siren should not fallback by default

--- scripts/fetch-protected-siren.ts
+++ scripts/fetch-protected-siren.ts
@@ -12,70 +12,48 @@ const GRIST_DOC_ID = 'hp8PLhMGY9sNWuzGDGe6yi';
 const GRIST_TABLE_ID = 'Protected_siren';
 
 fetchAndSaveProtectedSiren();
+
+const failProcess = () => {
+  if (process.env.NODE_ENV === 'production') {
+    process.exit(1);
+  } else {
+    console.log(
+      '[fetch-protected_siren]: dev-mode, ignoring error, empty list'
+    );
+  }
+};
+
 async function fetchAndSaveProtectedSiren() {
+  let protectedSiren = [] as string[];
+  console.log(`[fetch-protected_siren]: let's fetch protected siren`);
   if (!process.env.GRIST_API_KEY) {
-    console.error(
-      `[fetch-protected_siren]: GRIST_API_KEY environment variable is not set`
-    );
-    if (process.env.NODE_ENV === 'production') {
-      process.exit(1);
-    }
-  }
-
-  let protectedSiren: string[] | undefined;
-
-  if (process.env.GRIST_API_KEY) {
+    console.error(`[fetch-protected_siren]: GRIST_API_KEY missing`);
+    failProcess();
+  } else {
     try {
-      console.log(
-        `[fetch-protected_siren]: Fetching protected siren from Grist table...`
-      );
-
       protectedSiren = await fetchFromGrist();
+      console.log(`[fetch-protected_siren]: Grist fetching success`);
 
-      console.log(`[fetch-protected_siren]: Grist table successfully fetched`);
-    } catch (err) {
-      console.error(
-        `[fetch-protected_siren]: Error fetching from grist table (${err})`
-      );
-    }
-  }
+      let oldProtectedSiren = await fetchCurrentProtectedList();
+      const newSirenToOldRatio =
+        protectedSiren.length / oldProtectedSiren.length;
 
-  try {
-    let currentlyDeployedProtectedSiren = await fetchFromCurrentlyDeployed();
+      if (newSirenToOldRatio < 0.75) {
+        throw new ProtectedSirenTooManySuppressionsError(
+          `New list is only ${newSirenToOldRatio}% of previous list. Should not be less than 75%`
+        );
+      }
+    } catch (err: any) {
+      console.error(`[fetch-protected_siren]: Grist fetching failure (${err})`);
 
-    // If the protected siren fetched from Grist is shorter than the one
-    if (
-      protectedSiren &&
-      protectedSiren.length < currentlyDeployedProtectedSiren.length
-    ) {
-      console.warn(
-        `[fetch-protected_siren]: Table in Grist contains less protected siren than the currently deployed one.`
-      );
-      console.log(
-        `[fetch-protected_siren]: Please check that no protected siren has been inadvertedly removed.`
-      );
-      protectedSiren = undefined;
-    }
-    if (!protectedSiren) {
-      console.log(
-        `[fetch-protected_siren]: Fallback to currently deployed protected siren`
-      );
-      protectedSiren = currentlyDeployedProtectedSiren;
-    }
-  } catch (err) {
-    console.error(
-      `[fetch-protected_siren]: Error fetching from currently deployed protected siren (${err})`
-    );
-  }
-
-  if (!protectedSiren) {
-    if (process.env.NODE_ENV === 'production') {
-      process.exit(1);
-    } else {
-      console.log(
-        `[fetch-protected_siren]: Fallback to empty protected siren list (dev mode only)`
-      );
-      protectedSiren = [];
+      // if grist is down one can manually activate fallback on current prod list to force deployment
+      if (err instanceof ProtectedSirenGristFetchFailedError) {
+      }
+      if (err instanceof ProtectedSirenTooManySuppressionsError) {
+      }
+      if (err instanceof ProtectedSirenInvalidSirenError) {
+      }
+      failProcess();
     }
   }
 
@@ -87,42 +65,84 @@ async function fetchAndSaveProtectedSiren() {
   console.log(`[fetch-protected_siren]: Done!`);
 }
 
+/**
+ * Fetchers functions
+ */
+
 async function fetchFromGrist(): Promise<string[]> {
-  const gristData = await (
-    await fetch(
-      `${routes.tooling.grist}${GRIST_DOC_ID}/tables/${GRIST_TABLE_ID}/records`,
-      {
-        headers: {
-          Authorization: 'Bearer ' + process.env.GRIST_API_KEY,
-        },
-      }
-    )
-  ).json();
+  let gristSiren = [] as string[];
+  try {
+    const gristData = await (
+      await fetch(
+        `${routes.tooling.grist}${GRIST_DOC_ID}/tables/${GRIST_TABLE_ID}/records`,
+        {
+          headers: {
+            Authorization: 'Bearer ' + process.env.GRIST_API_KEY,
+          },
+        }
+      )
+    ).json();
 
-  if ('error' in gristData) {
-    throw new Error(gristData.error);
-  }
-  const gristSiren = gristData.records
-    .map((record: any) => record.fields.siren)
-    .filter(Boolean)
-    .map((number: number) => number.toString());
+    if ('error' in gristData) {
+      throw new Error(gristData.error);
+    }
 
-  if (!gristSiren.every(isSiren)) {
-    throw new Error('some siren are not valid');
+    gristSiren = gristData.records
+      .map((record: any) => record.fields.siren)
+      .filter(Boolean)
+      .map((number: number) => number.toString());
+  } catch (e: any) {
+    throw new ProtectedSirenGristFetchFailedError(e);
   }
+
+  const invalidSiren = gristSiren.filter((s) => !isSiren(s));
+  if (invalidSiren.length > 0) {
+    throw new ProtectedSirenInvalidSirenError(invalidSiren.join(','));
+  }
+
   return gristSiren;
 }
 
-function fetchFromCurrentlyDeployed(): Promise<string[]> {
-  return fetch(`https://annuaire-entreprises.data.gouv.fr/protected-siren.txt`)
+/**
+ * If Grist failed, fallback on production - do not use in production
+ * @returns
+ */
+async function fetchCurrentProtectedList(): Promise<string[]> {
+  return await fetch(
+    `https://annuaire-entreprises.data.gouv.fr/protected-siren.txt`
+  )
     .then((res) => res.text())
     .then((text) => {
       const sirens = text.trim().split('\n');
-      if (!sirens.every(isSiren)) {
-        throw new Error(
-          'One or more slug from current protected-siren.txt is invalid'
-        );
+      const invalidSiren = sirens.filter((s) => !isSiren(s));
+      if (invalidSiren.length > 0) {
+        throw new ProtectedSirenGristFetchFailedError(invalidSiren.join(','));
       }
       return sirens;
     });
+}
+
+/**
+ * Errors and exceptions
+ */
+
+class ProtectedSirenInvalidSirenError extends Error {
+  constructor(public message: string) {
+    super(message);
+    this.name = 'At least one protected siren is invalid';
+  }
+}
+
+class ProtectedSirenGristFetchFailedError extends Error {
+  constructor(public message: string) {
+    super(message);
+    this.name = 'Grist fetch failed';
+  }
+}
+
+class ProtectedSirenTooManySuppressionsError extends Error {
+  constructor(public message: string) {
+    super(message);
+    this.name = 'Too many siren deletion compared to current list';
+  }
 }
