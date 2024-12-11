b94788b97d60f63a40e74266b61ea5280aa95100
Robin Monnier <monnier.robin@gmail.com>
2024-12-11 10:03:57 +0100

    chore: remove quotes

--- cypress/mocks/routes.ts
+++ cypress/mocks/routes.ts
@@ -13,18 +13,18 @@ export const routesHandlers = [
   http.get(routes.proxy.tva('*'), tvaHandler),
   http.get(routes.proxy.eori('*'), eoriHandler),
   http.get(
-    `${routes.rechercheEntreprise.rechercheUniteLegale}`,
+    routes.rechercheEntreprise.rechercheUniteLegale,
     rechercheEntrepriseHandler
   ),
-  http.get(`${routes.certifications.rge.api}`, rgeHandler),
-  http.get(`${routes.certifications.bio.api}`, apiBioHandler),
+  http.get(routes.certifications.rge.api, rgeHandler),
+  http.get(routes.certifications.bio.api, apiBioHandler),
   http.get(
     routes.certifications.entrepriseInclusive.api.siren,
     apiInclusionHandler
   ),
   http.get(
-    `${routes.certifications.entrepreneurSpectacles.ods.search}`,
+    routes.certifications.entrepreneurSpectacles.ods.search,
     entrepreneurSpectaclesHandler
   ),
-  http.get(`${routes.datagouv.ess}`, apiDataGouvEssHandler),
+  http.get(routes.datagouv.ess, apiDataGouvEssHandler),
 ];
