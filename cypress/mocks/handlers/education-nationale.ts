9441e947bc0ecbb65058768acfeb0e51868804aa
Robin Monnier <monnier.robin@gmail.com>
2024-12-13 12:30:51 +0100

    tests: remove stub for education nationale

---
+++ cypress/mocks/handlers/education-nationale.ts
@@ -0,0 +1,8 @@
+import { HttpResponse, HttpResponseResolver } from 'msw';
+import educationNationale from '../../fixtures/education-nationale.json';
+
+export const educationNationaleHandler: HttpResponseResolver = ({
+  request,
+}) => {
+  return HttpResponse.json(educationNationale);
+};
