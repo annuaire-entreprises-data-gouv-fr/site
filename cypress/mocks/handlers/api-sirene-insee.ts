9caea33c6830c78a938af7334662f122c6fb0603
Robin Monnier <monnier.robin@gmail.com>
2024-12-13 16:40:14 +0100

    tests: remove stub for insee sirene

---
+++ cypress/mocks/handlers/api-sirene-insee.ts
@@ -0,0 +1,21 @@
+import { HttpResponse, HttpResponseResolver } from 'msw';
+import apiSireneInseeAuth from '../../fixtures/api-sirene-insee-auth.json';
+import apiSireneInseeSiren from '../../fixtures/api-sirene-insee-siren.json';
+
+export const apiSireneInseeSirenHandler: HttpResponseResolver = ({
+  request,
+}) => {
+  return HttpResponse.json(apiSireneInseeSiren);
+};
+
+export const apiSireneInseeSiretHandler: HttpResponseResolver = ({
+  request,
+}) => {
+  return HttpResponse.json(apiSireneInseeSiren);
+};
+
+export const apiSireneInseeAuthHandler: HttpResponseResolver = ({
+  request,
+}) => {
+  return HttpResponse.json(apiSireneInseeAuth);
+};
