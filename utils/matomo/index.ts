0933fbab5148b25e593adb7840f1ed9a020ec414
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2024-12-19 15:10:20 +0100

    feat: matomo event on FAQ parcours (#1475)

--- utils/matomo/index.ts
+++ utils/matomo/index.ts
@@ -4,7 +4,11 @@
  * @param category
  * @param label
  */
-const logMatomoEvent = (category: string, action: string, label: string) => {
+export const logMatomoEvent = (
+  category: string,
+  action: string,
+  label: string
+) => {
   var _paq = window._paq || [];
   _paq.push(['trackEvent', category, action, label]);
 };
