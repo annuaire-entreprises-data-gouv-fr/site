90bc11b18b6a2ac12c19d8352fafc187e120bbbc
Robin Monnier <monnier.robin@gmail.com>
2024-12-27 11:13:34 +0100

    fix: e2e for idcc (#1488)

---
+++ cypress/mocks/handlers/recherche-entreprises-idcc.ts
@@ -0,0 +1,8 @@
+import { HttpResponse, HttpResponseResolver } from 'msw';
+import rechercheEntrepriseIdcc from '../../fixtures/recherche-entreprise-idcc.json';
+
+export const rechercheEntrepriseIdccHandler: HttpResponseResolver = async ({
+  request,
+}) => {
+  return HttpResponse.json(rechercheEntrepriseIdcc);
+};
