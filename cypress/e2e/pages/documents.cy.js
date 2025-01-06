a2eb12cfb2a29fa060e534036c61bc7d6fda9a55
Robin Monnier <monnier.robin@gmail.com>
2025-01-06 13:19:17 +0100

    refacto: create folders pages, fonctionnalites, recherche (#1498)
    refacto: e2e tests organization

---
+++ cypress/e2e/pages/documents.cy.js
@@ -0,0 +1,22 @@
+describe('Documents ESSOR ENERGIES (SOLARSUD)', () => {
+  it('Bouton agent connect sur les données protégées', () => {
+    cy.visit('/documents/487444697');
+    cy.contains('Réservé aux agents publics').should('be.visible');
+    cy.contains('button', 'ProConnect').should('be.visible');
+  });
+
+  it('[LOGGED] Should display documents', () => {
+    cy.login();
+    cy.visit('/documents/487444697');
+    // Conformité
+    cy.contains('Conformité').should('be.visible');
+    cy.contains('DGFiP : conforme').should('be.visible');
+    cy.contains('URSSAF : conforme').should('be.visible');
+    cy.contains('MSA : conforme').should('be.visible');
+    // Carte professionnelle travaux publics
+    cy.contains('Carte professionnelle travaux publics').should('be.visible');
+    cy.contains(
+      'Cette entreprise possède une carte professionnelle d’entrepreneur de travaux publics,'
+    ).should('be.visible');
+  });
+});
