91dfe0265e4c5b352798904f1fab8906d0ea80b4
xavier jouppe <xajouppe@gmail.com>
2025-01-08 15:15:39 +0100

    feat: remove twiiter and facebook links

--- components-ui/icon/index.tsx
+++ components-ui/icon/index.tsx
@@ -243,22 +243,6 @@ const information = (
   </svg>
 );
 
-const facebook = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="100%"
-    height="100%"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    stroke="none"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-  </svg>
-);
-
 const linkedin = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -274,22 +258,6 @@ const linkedin = (
     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
     <rect x="2" y="9" width="4" height="12"></rect>
     <circle cx="4" cy="4" r="2"></circle>
-  </svg>
-);
-
-const twitter = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="100%"
-    height="100%"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    stroke="none"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
   </svg>
 );
 
@@ -466,7 +434,6 @@ export const icons = {
   discussion,
   download,
   errorFill,
-  facebook,
   file,
   information,
   lightbulbFill,
@@ -482,6 +449,5 @@ export const icons = {
   qrCode,
   questionFill,
   successFill,
-  twitter,
   user,
 };
