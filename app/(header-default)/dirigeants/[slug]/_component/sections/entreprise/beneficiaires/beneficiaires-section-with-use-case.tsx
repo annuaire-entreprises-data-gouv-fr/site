ae636d9fb093cf11b5f10bb18831936872586f68
Robin Monnier <monnier.robin@gmail.com>
2025-01-09 12:14:31 +0100

    refacto: create generic SectionWithUseCase component

--- app/(header-default)/dirigeants/[slug]/_component/sections/entreprise/beneficiaires/beneficiaires-section-with-use-case.tsx
+++ app/(header-default)/dirigeants/[slug]/_component/sections/entreprise/beneficiaires/beneficiaires-section-with-use-case.tsx
@@ -7,7 +7,7 @@ import { FullTable } from '#components/table/full';
 import { UniteLegalePageLink } from '#components/unite-legale-page-link';
 import { EAdministration } from '#models/administrations/EAdministration';
 import { IUniteLegale } from '#models/core/types';
-import { IBeneficairesEffectif } from '#models/espace-agent/beneficiaires';
+import { IBeneficiairesEffectif } from '#models/espace-agent/beneficiaires';
 import { UseCase } from '#models/user/agent';
 import { ISession } from '#models/user/session';
 import { formatDatePartial } from '#utils/helpers';
@@ -20,11 +20,23 @@ import { useMemo } from 'react';
  * @param param0
  * @returns
  */
-const ProtectedBeneficiairesSection: React.FC<{
+export default function BeneficiairesSectionWithUseCase({
+  uniteLegale,
+  session,
+  useCase,
+  title,
+  id,
+  sources,
+  isProtected,
+}: {
   uniteLegale: IUniteLegale;
   session: ISession | null;
   useCase: UseCase;
-}> = ({ uniteLegale, session, useCase }) => {
+  title: string;
+  id: string;
+  sources: EAdministration[];
+  isProtected: boolean;
+}) {
   const params = useMemo(
     () => ({
       params: { useCase },
@@ -41,9 +53,10 @@ const ProtectedBeneficiairesSection: React.FC<{
   return (
     <>
       <AsyncDataSectionClient
-        id="beneficiaires"
-        title="Bénéficiaire(s) effectif(s)"
-        sources={[EAdministration.INPI]}
+        title={title}
+        id={id}
+        isProtected={isProtected}
+        sources={sources}
         notFoundInfo={
           <>
             Cette structure n’est pas enregistrée au{' '}
@@ -51,7 +64,6 @@ const ProtectedBeneficiairesSection: React.FC<{
           </>
         }
         data={beneficiaires}
-        isProtected
       >
         {(beneficiaires) => (
           <>
@@ -64,16 +76,16 @@ const ProtectedBeneficiairesSection: React.FC<{
       </AsyncDataSectionClient>
     </>
   );
-};
+}
 
 function BénéficiairesContent({
   beneficiaires,
   uniteLegale,
 }: {
-  beneficiaires: IBeneficairesEffectif[];
+  beneficiaires: IBeneficiairesEffectif[];
   uniteLegale: IUniteLegale;
 }) {
-  const formatInfos = (beneficiaire: IBeneficairesEffectif) => [
+  const formatInfos = (beneficiaire: IBeneficiairesEffectif) => [
     <>
       {beneficiaire.prenoms}
       {beneficiaire.prenoms && ' '}
@@ -326,5 +338,3 @@ function PartCapitalModalite({
     </>
   );
 }
-
-export default ProtectedBeneficiairesSection;
---
+++ app/(header-default)/dirigeants/[slug]/_component/sections/entreprise/beneficiaires/info-agent-rbe.tsx
@@ -0,0 +1,21 @@
+export const InfoAgentRBE = () => (
+  <>
+    <p>
+      Depuis le 31 juillet 2024, les{' '}
+      <a href="/faq/registre-des-beneficiaires-effectifs">
+        bénéficiaires effectifs ne sont plus librement accessibles
+      </a>
+      .
+    </p>
+    <p>
+      Les agents publics peuvent y accéder uniquement dans les cas d’usages
+      justifiant d’un intérêt légitime. En déclarant le cadre juridique dans
+      lequel vous accédez à ces données, vous vous engagez{' '}
+      <a href="/cgu">à respecter nos conditions générales d’utilisation</a>.
+    </p>
+    <p>
+      Toute demande d’accès aux données est tracée et envoyée à la commission
+      européeene.
+    </p>
+  </>
+);
