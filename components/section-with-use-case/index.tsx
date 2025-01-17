bf8e78603f76bdd9699a6ddf3ad8e1542e4975df
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2025-01-17 19:13:39 +0100

    Update components/section-with-use-case/index.tsx
    Co-authored-by: Kevin Masseix <kevin.masseix@beta.gouv.fr>
    Signed-off-by: Xavier Jp <xajouppe@gmail.com>

--- components/section-with-use-case/index.tsx
+++ components/section-with-use-case/index.tsx
@@ -21,7 +21,7 @@ type WrappedSectionProps = {
 
 const DefaultIntroContent = () => (
   <p>
-    Les agents publics peuvent demander accéder à cette donnée uniquement dans
+    Les agents publics peuvent demander à accéder à cette donnée uniquement dans
     les cas d’usages justifiant d’un intérêt légitime. En déclarant le cadre
     juridique dans lequel vous accédez à ces données, vous vous engagez{' '}
     <a href="/cgu">à respecter nos conditions générales d’utilisation</a>.
