46c09dc7ab23d0d6b5743e572ee42cc034865f77
Xavier Jp <xavier.jouppe@beta.gouv.fr>
2025-01-21 10:00:26 +0100

    Update utils/integrations/s3.ts
    Co-authored-by: Robin Monnier <monnier.robin@gmail.com>
    Signed-off-by: Xavier Jp <xajouppe@gmail.com>

--- utils/integrations/s3.ts
+++ utils/integrations/s3.ts
@@ -45,21 +45,18 @@ export async function readFromS3(
 
         // Store all of data chunks returned from the response data stream
         // into an array then use Array#join() to use the returned contents as a String
-        //@ts-ignore
-        let responseDataChunks = [];
+        const responseDataChunks: Buffer[] = [];
+        const stream = response.Body as Readable;
 
         // Handle an error while streaming the response body
-        //@ts-ignore
-        response.Body.once('error', (err) => reject(err));
+        stream.once('error', (err) => reject(err));
 
         // Attach a 'data' listener to add the chunks of data to our array
         // Each chunk is a Buffer instance
-        //@ts-ignore
-        response.Body.on('data', (chunk) => responseDataChunks.push(chunk));
+        stream.on('data', (chunk) => responseDataChunks.push(chunk));
 
         // Once the stream has no more data, join the chunks into a string and return the string
-        //@ts-ignore
-        response.Body.once('end', () => resolve(responseDataChunks.join('')));
+        stream.once('end', () => resolve(responseDataChunks.join('')));
       } catch (err: any) {
         const statusCode = err['$metadata']?.httpStatusCode;
         if (statusCode === 404) {
