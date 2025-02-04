3a553eb557674ad66ebbc70dc25a9485993a0b62
xavier jouppe <xajouppe@gmail.com>
2025-02-04 17:05:18 +0100

    chore: unecessary changes

--- components/layouts/layout-default.tsx
+++ components/layouts/layout-default.tsx
@@ -1,9 +1,10 @@
+import { PropsWithChildren } from 'react';
 import { NPSBanner } from '#components/banner/nps';
 import Footer from '#components/footer';
 import { HeaderPageRouter } from '#components/header/header-page-router';
+import { WeNeedYouModal } from '#components/modal/we-need-you';
 import { Question } from '#components/question';
 import SocialNetworks from '#components/social-network';
-import { PropsWithChildren } from 'react';
 
 type IProps = {
   searchBar?: boolean;
@@ -15,6 +16,7 @@ export const LayoutDefault = ({
 }: PropsWithChildren<IProps>) => {
   return (
     <div id="page-layout">
+      <WeNeedYouModal />
       <NPSBanner />
 
       <HeaderPageRouter
