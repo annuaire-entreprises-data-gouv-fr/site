95801e3a681ef6708ab29baa289fbaf5410900a3
xavier jouppe <xajouppe@gmail.com>
2025-02-10 10:53:32 +0100

    chore: increase protected siren ttl

--- models/protected-siren/index.ts
+++ models/protected-siren/index.ts
@@ -15,7 +15,7 @@ import {
 class ProtectedSirenList {
   public _list: DataStore<boolean>;
   // time before protected siren list update
-  private TTL = 3600000; //1h
+  private TTL = 21600000; //1h
 
   constructor() {
     this._list = new DataStore<boolean>(
