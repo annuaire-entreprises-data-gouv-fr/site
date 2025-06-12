b08bc289c0c5c5cd59e2f5b3d4efb323e0f03278
Robin Monnier <monnier.robin@gmail.com>
2025-06-12 10:13:33 +0200

    feat: improve error handling and add 200k limitation

--- app/api/export-csv/route.ts
+++ app/api/export-csv/route.ts
@@ -8,33 +8,42 @@ import { NextRequest } from 'next/server';
 import z from 'zod';
 import { exportCsvSchema } from './input-validation';
 
+class APIResponseError extends Error {
+  constructor(message: string, public data: any) {
+    super(message);
+    this.name = 'APIResponseError';
+  }
+}
+
 async function exportCsv(request: NextRequest): Promise<Response> {
   try {
     const body = await request.json();
     const validatedData = exportCsvSchema.parse(body);
 
-    if (validatedData.count) {
-      const response = await getEtablissementListeCount(validatedData);
+    const response = await getEtablissementListeCount(validatedData);
 
-      if (typeof response === 'number') {
-        return Response.json({ count: response });
-      } else {
-        return Response.json(response);
-      }
+    if (validatedData.count && typeof response === 'number') {
+      return Response.json({ count: response });
     } else {
+      if (typeof response !== 'number') {
+        throw new APIResponseError('Count export CSV request failed', response);
+      } else if (response > 200000) {
+        throw new Error('Response is not a number or is greater than 200000');
+      }
+
       const csvData = await getEtablissementListe(validatedData);
 
-      if (typeof csvData === 'string') {
-        return new Response(csvData, {
-          headers: {
-            'Content-Type': 'text/csv; charset=utf-8',
-            'Content-Disposition':
-              'attachment; filename="annuaire-etablissements.csv"',
-          },
-        });
-      } else {
-        return Response.json(csvData);
+      if (typeof csvData !== 'string') {
+        throw new APIResponseError('Export CSV request failed', response);
       }
+
+      return new Response(csvData, {
+        headers: {
+          'Content-Type': 'text/csv; charset=utf-8',
+          'Content-Disposition':
+            'attachment; filename="annuaire-etablissements.csv"',
+        },
+      });
     }
   } catch (e) {
     if (e instanceof z.ZodError) {
@@ -48,6 +57,10 @@ async function exportCsv(request: NextRequest): Promise<Response> {
         },
         { status: 400 }
       );
+    }
+    if (e instanceof APIResponseError) {
+      logErrorInSentry(new Exception({ name: e.name, cause: e }));
+      return Response.json(e.data, { status: 500 });
     }
 
     logErrorInSentry(new Exception({ name: 'Export CSV', cause: e }));
