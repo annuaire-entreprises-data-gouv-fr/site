e5f16dab2dbc9afe0042c8e1eb0936290fc092b2
Robin Monnier <monnier.robin@gmail.com>
2025-06-13 09:29:34 +0200

    fix: title

--- app/(header-default)/export-csv/page.tsx
+++ app/(header-default)/export-csv/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import ExportCsv from './ExportCsv';
 
 export const metadata: Metadata = {
-  title: 'Budget de l’Annuaire des Entreprises',
+  title: 'Générer une liste CSV d‘entreprises | L’Annuaire des Entreprises',
   alternates: {
     canonical: 'https://annuaire-entreprises.data.gouv.fr/export-csv',
   },
