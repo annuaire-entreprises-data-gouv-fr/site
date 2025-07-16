a03786b4e589dd5b8e067024a9832debf9ed8ae3
Robin Monnier <monnier.robin@gmail.com>
2025-07-16 11:30:22 +0200

    chore: update to new d-roles api

--- models/group/index.ts
+++ models/group/index.ts
@@ -21,9 +21,9 @@ export class Group {
   /**
    * Check if a user is an admin of this group
    */
-  async isUserAdmin(email: string): Promise<boolean> {
+  async isUserAdmin(userEmail: string, userSub: string): Promise<boolean> {
     try {
-      const groups = await Groups.find(email);
+      const groups = await Groups.find(userEmail, userSub);
       const groupData =
         groups.find((group) => group.id === this.groupId) || null;
 
@@ -31,7 +31,7 @@ export class Group {
         return false;
       }
 
-      const user = groupData.users.find((user) => user.email === email);
+      const user = groupData.users.find((user) => user.email === userEmail);
       return user?.is_admin ?? false;
     } catch (error) {
       logFatalErrorInSentry(
@@ -47,20 +47,44 @@ export class Group {
   /**
    * Add a user to this group (requires admin permissions)
    */
+  async updateName(groupName: string, actingUserSub: string): Promise<boolean> {
+    try {
+      await droleClient.updateName(this.groupId, groupName, actingUserSub);
+      return true;
+    } catch (error) {
+      logFatalErrorInSentry(
+        new FetchRessourceException({
+          ressource: 'D-Roles Update Name of a Group',
+          cause: error,
+        })
+      );
+      throw error;
+    }
+  }
+
+  /**
+   * Add a user to this group (requires admin permissions)
+   */
   async addUser(
     adminEmail: string,
+    adminSub: string,
     userEmail: string,
     roleId: number
   ): Promise<boolean> {
     try {
-      const isAdmin = await this.isUserAdmin(adminEmail);
+      const isAdmin = await this.isUserAdmin(adminEmail, adminSub);
       if (!isAdmin) {
         throw new HttpUnauthorizedError(
           'User does not have admin permissions for this group'
         );
       }
 
-      await droleClient.addUserToGroup(this.groupId, userEmail, roleId);
+      await droleClient.addUserToGroup(
+        this.groupId,
+        userEmail,
+        roleId,
+        adminSub
+      );
       return true;
     } catch (error) {
       logFatalErrorInSentry(
@@ -74,14 +98,50 @@ export class Group {
   }
 
   /**
+   * Update a user from a group (requires admin permissions)
+   */
+  async updateUser(
+    adminEmail: string,
+    adminSub: string,
+    userEmail: string,
+    roleId: number
+  ): Promise<boolean> {
+    try {
+      const isAdmin = await this.isUserAdmin(adminEmail, adminSub);
+      if (!isAdmin) {
+        throw new HttpUnauthorizedError(
+          'User does not have admin permissions for this group'
+        );
+      }
+
+      await droleClient.updateUserFromGroup(
+        this.groupId,
+        userEmail,
+        roleId,
+        adminSub
+      );
+      return true;
+    } catch (error) {
+      logFatalErrorInSentry(
+        new FetchRessourceException({
+          ressource: 'D-Roles Update User From Group',
+          cause: error,
+        })
+      );
+      throw error;
+    }
+  }
+
+  /**
    * Remove a user from this group (requires admin permissions)
    */
   async removeUserFromGroup(
     adminEmail: string,
+    adminSub: string,
     userEmail: string
   ): Promise<boolean> {
     try {
-      const isAdmin = await this.isUserAdmin(adminEmail);
+      const isAdmin = await this.isUserAdmin(adminEmail, adminSub);
       if (!isAdmin) {
         throw new HttpUnauthorizedError(
           'User does not have admin permissions for this group'
@@ -90,7 +150,7 @@ export class Group {
 
       // Get the user ID to remove
       const user = await droleClient.getUserByEmail(userEmail);
-      await droleClient.removeUserFromGroup(this.groupId, user.id);
+      await droleClient.removeUserFromGroup(this.groupId, user.id, adminSub);
       return true;
     } catch (error) {
       logFatalErrorInSentry(
