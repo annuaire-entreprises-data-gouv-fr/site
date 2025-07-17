e4b42a9397ad2d07bd645604f9907756f83b5499
Robin Monnier <monnier.robin@gmail.com>
2025-07-17 12:29:26 +0200

    feat: create group entity component

---
+++ components/espace-agent-components/group-management.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { IDRolesUser } from '#clients/api-d-roles/interface';
+import { Groups, IDRolesGroup } from '#models/groups';
+import { useEffect, useState } from 'react';
+import { GroupEntity } from './group-entity';
+
+export function GroupManagement({
+  currentUserEmail,
+  currentUserSub,
+}: {
+  currentUserEmail: string;
+  currentUserSub: string;
+}) {
+  const [groups, setGroups] = useState<IDRolesGroup[]>([]);
+  const [roles, setRoles] = useState<IDRolesUser[]>([]);
+  const [rolesLoading, setRolesLoading] = useState(true);
+  const [groupsLoading, setGroupsLoading] = useState(true);
+
+  const adminRoleName = roles.find((r) => r.is_admin)?.role_name;
+
+  useEffect(() => {
+    const fetchRoles = async () => {
+      try {
+        const response = await fetch('/api/teams/roles');
+        if (response.ok) {
+          const rolesData = await response.json();
+          setRoles(rolesData);
+        }
+      } catch (error) {
+        console.error('Error fetching roles:', error);
+      } finally {
+        setRolesLoading(false);
+      }
+    };
+
+    const fetchGroups = async () => {
+      try {
+        const groups = await Groups.find(currentUserEmail, currentUserSub);
+        setGroups(groups);
+      } catch (error) {
+        console.error('Error fetching groups:', error);
+      } finally {
+        setGroupsLoading(false);
+      }
+    };
+
+    fetchRoles();
+    fetchGroups();
+  }, [currentUserEmail, currentUserSub]);
+
+  if (rolesLoading || groupsLoading) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <>
+      {groups.map((group) => {
+        const currentUserRole = group.users.find(
+          (user) => user.email === currentUserEmail
+        );
+        const isAdmin = Boolean(
+          adminRoleName && currentUserRole?.role_name === adminRoleName
+        );
+
+        return (
+          <GroupEntity
+            key={group.id}
+            group={group}
+            setGroup={(group: IDRolesGroup) =>
+              setGroups(groups.map((g) => (g.id === group.id ? group : g)))
+            }
+            isAdmin={isAdmin}
+            roles={roles}
+          />
+        );
+      })}
+    </>
+  );
+}
