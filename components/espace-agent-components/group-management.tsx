a9c9e619f5b01cb6efdc5510e7d169422ede5e9b
Robin Monnier <monnier.robin@gmail.com>
2025-07-17 15:07:54 +0200

    chore: use server components for first rendering

--- components/espace-agent-components/group-management.tsx
+++ components/espace-agent-components/group-management.tsx
@@ -2,59 +2,21 @@
 
 import { IDRolesUser } from '#clients/api-d-roles/interface';
 import { IDRolesGroup } from '#models/groups';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { GroupEntity } from './group-entity';
 
 export function GroupManagement({
   currentUserEmail,
-  currentUserSub,
+  roles,
+  initialGroups,
 }: {
   currentUserEmail: string;
-  currentUserSub: string;
+  roles: IDRolesUser[];
+  initialGroups: IDRolesGroup[];
 }) {
-  const [groups, setGroups] = useState<IDRolesGroup[]>([]);
-  const [roles, setRoles] = useState<IDRolesUser[]>([]);
-  const [rolesLoading, setRolesLoading] = useState(true);
-  const [groupsLoading, setGroupsLoading] = useState(true);
+  const [groups, setGroups] = useState<IDRolesGroup[]>(initialGroups);
 
   const adminRoleName = roles.find((r) => r.is_admin)?.role_name;
-
-  useEffect(() => {
-    const fetchRoles = async () => {
-      try {
-        const response = await fetch('/api/groups/roles');
-        if (response.ok) {
-          const rolesData = await response.json();
-          setRoles(rolesData);
-        }
-      } catch (error) {
-        console.error('Error fetching roles:', error);
-      } finally {
-        setRolesLoading(false);
-      }
-    };
-
-    const fetchGroups = async () => {
-      try {
-        const response = await fetch('/api/groups');
-        if (response.ok) {
-          const groupsData = await response.json();
-          setGroups(groupsData);
-        }
-      } catch (error) {
-        console.error('Error fetching groups:', error);
-      } finally {
-        setGroupsLoading(false);
-      }
-    };
-
-    fetchRoles();
-    fetchGroups();
-  }, [currentUserEmail, currentUserSub]);
-
-  if (rolesLoading || groupsLoading) {
-    return <div>Chargement...</div>;
-  }
 
   return (
     <>
