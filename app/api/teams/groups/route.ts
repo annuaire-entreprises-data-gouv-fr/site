c592db68da84e316b5ffaceb483620a763dceb06
Robin Monnier <monnier.robin@gmail.com>
2025-07-17 14:00:39 +0200

    fix: groups data fetching

---
+++ app/api/teams/groups/route.ts
@@ -0,0 +1,14 @@
+import { Groups } from '#models/groups';
+import getSession from '#utils/server-side-helper/app/get-session';
+import { NextResponse } from 'next/server';
+import { withAgentAuth, withErrorHandling } from '../route-wrappers';
+
+async function getGroupsHandler() {
+  const session = await getSession();
+
+  const groups = await Groups.find(session!.user!.email, session!.user!.userId);
+
+  return NextResponse.json(groups);
+}
+
+export const GET = withAgentAuth(withErrorHandling(getGroupsHandler));
