9b6ac470b39cafa56c68677653340ae69c6a7fb6
Robin Monnier <monnier.robin@gmail.com>
2025-07-18 15:24:47 +0200

    feat: metadata

--- app/(header-compte)/(compte)/mes-equipes/page.tsx
+++ app/(header-compte)/(compte)/mes-equipes/page.tsx
@@ -6,7 +6,16 @@ import {
 } from '#models/authentication/user/rights';
 import { Groups } from '#models/groups';
 import getSession from '#utils/server-side-helper/app/get-session';
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'Vos équipes de l’Annuaire des Entreprises',
+  alternates: {
+    canonical: 'https://annuaire-entreprises.data.gouv.fr/mes-equipes',
+  },
+  robots: 'noindex, nofollow',
+};
 
 const MesEquipesPage = async () => {
   const session = await getSession();
