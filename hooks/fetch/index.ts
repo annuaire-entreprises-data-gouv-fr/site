7a1c3aa8d66d55c29101468ce67f4436ef865f3d
12private12 <120792mon@gmail.com>
2025-08-08 10:06:35 +0800

    Delete hooks directory
    Signed-off-by: 12private12 <120792mon@gmail.com>

--- hooks/fetch/index.ts
+++
@@ -1,4 +0,0 @@
-export { useFetchBODACC } from './bodacc';
-export { useFetchComptesAssociation } from './comptes-association';
-export { useFetchFinancesSociete } from './indicateurs-financiers-societe';
-export { useFetchJOAFE } from './joafe';
