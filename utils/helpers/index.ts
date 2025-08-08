e7fd9fd44f6b385d428832c551b21962349352e7
12private12 <120792mon@gmail.com>
2025-08-08 10:07:38 +0800

    Delete utils directory
    Signed-off-by: 12private12 <120792mon@gmail.com>

--- utils/helpers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './checks';
-export * from './formatting/formatting';
-export * from './formatting/unite-legale-label';
-export * from './id-rna';
-export * from './random-id';
-export * from './siren-and-siret';
