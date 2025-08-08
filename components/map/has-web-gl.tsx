12e0bd79f58338755f97c72654929ff4ed36bd80
12private12 <120792mon@gmail.com>
2025-08-08 10:17:39 +0800

    Delete components directory
    Signed-off-by: 12private12 <120792mon@gmail.com>

--- components/map/has-web-gl.tsx
+++
@@ -1,9 +0,0 @@
-export const hasWebGLSupport = () => {
-  if (typeof window === 'undefined') {
-    return false;
-  }
-  const canvas = document.createElement('canvas');
-  const gl =
-    canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-  return gl !== null && gl !== undefined;
-};
