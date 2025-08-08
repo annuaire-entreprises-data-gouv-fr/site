8a12d213663061bcbcad1a0096ff3d64fe6fe2b2
12private12 <120792mon@gmail.com>
2025-08-08 10:06:25 +0800

    Delete models directory
    Signed-off-by: 12private12 <120792mon@gmail.com>

--- models/use-cases/index.ts
+++
@@ -1,7 +0,0 @@
-export enum UseCase {
-  autre = "Autre cas d'usage",
-  aidesPubliques = 'Aides publiques',
-  subventionsFonctionnementAssociation = 'Subventions de fonctionnement aux associations',
-  marches = 'Marchés publics',
-  fraude = 'Détection de la fraude',
-}
