fcef6c51adb5116fbe0228c873155c60b42741c2
Robin Monnier <monnier.robin@gmail.com>
2025-09-02 11:20:40 +0200

    fix: simplify banner

--- components/banner/temp-incident.tsx
+++ components/banner/temp-incident.tsx
@@ -1,56 +1,27 @@
-'use client';
-
 import { PrintNever } from '#components-ui/print-visibility';
-import ClientOnly from '#components/client-only';
 import constants from '#models/constants';
-import { useStorage } from 'hooks/use-storage';
 import styles from './styles.module.css';
 
-const TEMP_INCIDENT_BANNER_ENABLED = true;
-const TEMP_INCIDENT_BANNER_ID = 'temp-incident-banner-2025-09-02';
 const TEMP_INCIDENT_BANNER_MESSAGE = `
         🚨 Nous rencontrons actuellement des difficultés techniques, le service peut être temporairement indisponible.
         Nos équipes sont mobilisées pour résoudre ce problème, nous vous prions de nous excuser
         pour la gêne occasionnée.`;
 
 export default function TempIncidentBanner() {
-  const [shouldDisplayBanner, setShouldDisplayBanner] = useStorage(
-    'local',
-    TEMP_INCIDENT_BANNER_ID,
-    true
-  );
-
-  if (!TEMP_INCIDENT_BANNER_ENABLED) {
-    return null;
-  }
-
-  const close = () => {
-    setShouldDisplayBanner(false);
-  };
-
   return (
-    <ClientOnly>
-      {shouldDisplayBanner ? (
-        <PrintNever>
-          <div
-            id="temp-incident"
-            role="dialog"
-            aria-label="Incident en cours"
-            className={styles.npsModal}
-            style={{
-              backgroundColor: constants.colors.pastelBlue,
-              borderColor: constants.colors.blue,
-            }}
-          >
-            <div className="fr-container">
-              {TEMP_INCIDENT_BANNER_MESSAGE}
-              <button onClick={close}>
-                <strong>Ne plus afficher ce message ✕</strong>
-              </button>
-            </div>
-          </div>
-        </PrintNever>
-      ) : null}
-    </ClientOnly>
+    <PrintNever>
+      <div
+        id="temp-incident"
+        role="dialog"
+        aria-label="Incident en cours"
+        className={styles.npsModal}
+        style={{
+          backgroundColor: constants.colors.pastelBlue,
+          borderColor: constants.colors.frBlue,
+        }}
+      >
+        <div className="fr-container">{TEMP_INCIDENT_BANNER_MESSAGE}</div>
+      </div>
+    </PrintNever>
   );
 }
