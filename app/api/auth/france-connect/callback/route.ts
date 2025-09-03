d1d9d751e758b372662edde1a8a43488d0d4b78b
Robin Monnier <monnier.robin@gmail.com>
2025-09-03 10:45:02 +0200

    doc: add doc to test france connect locally

--- app/api/auth/france-connect/callback/route.ts
+++ app/api/auth/france-connect/callback/route.ts
@@ -6,6 +6,10 @@ import withSession from '#utils/session/with-session';
 import { NextResponse } from 'next/server';
 import { FranceConnectFailedException } from '../france-connect-types';
 
+// Local testing tip: to simulate the FranceConnect callback locally, point staging.annuaire-entreprises.data.gouv.fr to 127.0.0.1 in your /etc/hosts file:
+// Run `sudo vim /etc/hosts` and add the following line:
+// 127.0.0.1 staging.annuaire-entreprises.data.gouv.fr
+// Note: Due to HTTPS constraints, direct redirection may not work, but you can manually replace staging.annuaire-entreprises.data.gouv.fr with localhost:3000 in your browser to continue testing.
 export const GET = withSession(async function callbackRoute(req) {
   try {
     const userInfo = await franceConnectAuthenticate(req);
