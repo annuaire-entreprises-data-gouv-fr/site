d8e996b3f228321032fce97dc57e000cc437338b
Robin Monnier <monnier.robin@gmail.com>
2025-09-17 16:34:01 +0200

    fix: fc session conflict (#2005)
    * chore: rename variable for fc session
    
    * feat: disallow connexion at the same with proconnect and franceconnect

--- app/(header-default)/formulaire/supprimer-donnees-personnelles-entreprise/_components/connection-france-connect.tsx
+++ app/(header-default)/formulaire/supprimer-donnees-personnelles-entreprise/_components/connection-france-connect.tsx
@@ -11,7 +11,7 @@ export function ConnectionFranceConnect({
 }) {
   const missingDataInFranceConnect =
     session &&
-    session.hidePersonalDataRequestFC &&
+    session.franceConnectHidePersonalDataSession &&
     !getHidePersonalDataRequestFCSession(session);
 
   const franceConnectInfo =
