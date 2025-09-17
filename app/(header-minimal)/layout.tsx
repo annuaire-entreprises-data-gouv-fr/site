e6a7a4eea5b200db378bf34089275cff8d292b58
Robin Monnier <monnier.robin@gmail.com>
2025-09-17 09:02:01 +0200

    refactor: remove metadata imports and update indexing logic for query params

--- app/(header-minimal)/layout.tsx
+++ app/(header-minimal)/layout.tsx
@@ -1,9 +1,5 @@
 import { HeaderAppRouter } from '#components/header/header-app-router';
-import { meta } from '#components/meta/meta-server';
 import { Question } from '#components/question';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = meta({});
 
 export default function HomeLayout({
   children,
