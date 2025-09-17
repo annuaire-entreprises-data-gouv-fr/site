10e486fd0fe634e2b47d260699cd31fcdc25154f
Robin Monnier <monnier.robin@gmail.com>
2025-09-17 09:37:19 +0200

    Revert "feat: add tally (#1993)"
    This reverts commit 3e6d35423730051bca3479ad0f8ddafebab75b92.

--- components/banner/nps.tsx
+++ components/banner/nps.tsx
@@ -1,14 +1,17 @@
 'use client';
 
+import { PrintNever } from '#components-ui/print-visibility';
+import ClientOnly from '#components/client-only';
 import { Exception } from '#models/exceptions';
 import { logInfoInSentry } from '#utils/sentry';
 import { useStorage } from 'hooks';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import styles from './styles.module.css';
 
 const NPS_MODAL_ID = 'nps-modal-2';
 
 export const NPSBanner: React.FC<{}> = () => {
-  // const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   const [hasAlreadyBeenTriggered, setHasAlreadyBeenTriggered] = useStorage(
     'local',
@@ -43,59 +46,38 @@ export const NPSBanner: React.FC<{}> = () => {
 
   useEffect(() => {
     const t = pathCounter();
-
     if (t > 2) {
-      setHasAlreadyBeenTriggered(true);
-      showTally();
-      // setIsVisible(true);
+      setIsVisible(true);
     }
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const showTally = () => {
-    if (typeof window !== 'undefined') {
-      //@ts-ignore
-      window.Tally.openPopup('n04Jq0', {
-        emoji: {
-          text: '👋',
-          animation: 'wave',
-        },
-        open: {
-          trigger: 'time',
-          ms: 500,
-        },
-      });
-    }
+  const close = () => {
+    setHasAlreadyBeenTriggered(true);
+    setIsVisible(false);
   };
 
-  return null;
-
-  // const close = () => {
-  //   setHasAlreadyBeenTriggered(true);
-  //   setIsVisible(false);
-  // };
-
-  // return (
-  //   <ClientOnly>
-  //     {isVisible ? (
-  //       <PrintNever>
-  //         <div
-  //           id={NPS_MODAL_ID}
-  //           role="dialog"
-  //           aria-label="Donnez-nous votre avis"
-  //           className={styles.npsModal}
-  //         >
-  //           <div className="fr-container">
-  //             <a onClick={close} href="/formulaire/nps" target="_blank">
-  //               👍👎 Quel est votre avis sur l‘Annuaire des Entreprises ?
-  //             </a>
-  //             <button onClick={close}>
-  //               <strong>Ne plus afficher ce message ✕</strong>
-  //             </button>
-  //           </div>
-  //         </div>
-  //       </PrintNever>
-  //     ) : null}
-  //   </ClientOnly>
-  // );
+  return (
+    <ClientOnly>
+      {isVisible ? (
+        <PrintNever>
+          <div
+            id={NPS_MODAL_ID}
+            role="dialog"
+            aria-label="Donnez-nous votre avis"
+            className={styles.npsModal}
+          >
+            <div className="fr-container">
+              <a onClick={close} href="/formulaire/nps" target="_blank">
+                👍👎 Quel est votre avis sur l‘Annuaire des Entreprises ?
+              </a>
+              <button onClick={close}>
+                <strong>Ne plus afficher ce message ✕</strong>
+              </button>
+            </div>
+          </div>
+        </PrintNever>
+      ) : null}
+    </ClientOnly>
+  );
 };
