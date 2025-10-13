ff948b1e059897a10dfbdbfbee02f4ed043edd7e
Alan Chauchet <alan.chauchet@gmail.com>
2025-10-13 16:35:35 +0200

    chore(errors): removes TVAUserException

--- clients/api-proxy/tva/index.ts
+++ clients/api-proxy/tva/index.ts
@@ -8,17 +8,6 @@ type IVIESResponse = {
 };
 
 /**
- * Specific exception raised by VIES endpoint
- */
-export class TVAUserException extends Error {
-  constructor(public message: string) {
-    super(message);
-    this.message = message;
-    this.name = "TVAUserException";
-  }
-}
-
-/**
  * Call VIES to validate a French TVA number
  * @param tva
  * @returns TVA number if valid else null
@@ -26,17 +15,9 @@ export class TVAUserException extends Error {
 export const clientTVA = async (tva: TVANumber): Promise<string | null> => {
   const url = routes.proxy.tva(tva);
 
-  try {
-    const data = await clientAPIProxy<IVIESResponse>(url, {
-      timeout: constants.timeout.XXL,
-    });
+  const data = await clientAPIProxy<IVIESResponse>(url, {
+    timeout: constants.timeout.XXL,
+  });
 
-    return data.tva;
-  } catch (e: any) {
-    if (e instanceof Error) {
-      throw new TVAUserException(e.message);
-    }
-
-    throw e;
-  }
+  return data.tva;
 };
