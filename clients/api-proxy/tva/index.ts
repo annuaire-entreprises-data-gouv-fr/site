525edb400e63674b73fd657cacf82be8eb2c832b
Alan Chauchet <alan.chauchet@gmail.com>
2025-10-15 08:49:23 +0200

    Revert "feat(tva): uses new tva api-proxy method (#2074)"
    This reverts commit 3af9f7ef6e1850615750759ec9f82a81256ec52e.

--- clients/api-proxy/tva/index.ts
+++ clients/api-proxy/tva/index.ts
@@ -4,8 +4,38 @@ import constants from "#models/constants";
 import type { TVANumber } from "#utils/helpers";
 
 type IVIESResponse = {
-  tva: string | null;
+  isValid: boolean;
+  requestDate: string; //'2022-08-31T17:46:07.763Z';
+  userError: string; //'VALID';
+  name: string; //'SASU GANYMEDE';
+  address: string; //'OCP BUSINESS CENTER 4\n128 RUE LA BOETIE\n75008 PARIS';
+  requestIdentifier: string; //'';
+  vatNumber: string; //'09880878145';
+  viesApproximate: any;
+  // {
+  //   name: '---';
+  //   street: '---';
+  //   postalCode: '---';
+  //   city: '---';
+  //   companyType: '---';
+  //   matchName: 3;
+  //   matchStreet: 3;
+  //   matchPostalCode: 3;
+  //   matchCity: 3;
+  //   matchCompanyType: 3;
+  // };
 };
+
+/**
+ * Specific exception raised by VIES endpoint
+ */
+export class TVAUserException extends Error {
+  constructor(public message: string) {
+    super(message);
+    this.message = message;
+    this.name = "TVAUserException";
+  }
+}
 
 /**
  * Call VIES to validate a French TVA number
@@ -19,5 +49,9 @@ export const clientTVA = async (tva: TVANumber): Promise<string | null> => {
     timeout: constants.timeout.XXL,
   });
 
-  return data.tva;
+  if (data.userError && ["VALID", "INVALID"].indexOf(data.userError) === -1) {
+    throw new TVAUserException(data.userError);
+  }
+
+  return data.isValid ? data.vatNumber : null;
 };
--- cypress/mocks/handlers/tva.ts
+++ cypress/mocks/handlers/tva.ts
@@ -1,15 +1,17 @@
 import { HttpResponse, type HttpResponseResolver } from "msw";
 
 export const tvaHandler: HttpResponseResolver = ({ request }) => {
-  let tva: string | null = "12345678901";
+  let vatNumber: string | null = "12345678901";
+  let isValid: boolean | undefined = true;
 
   if (request.url.match("842019051")) {
-    tva = "43842019051";
+    vatNumber = "43842019051";
   } else if (request.url.match("217500016")) {
-    tva = "72217500016";
+    vatNumber = "72217500016";
   } else if (request.url.match("423208180") || request.url.match("383657467")) {
-    tva = null;
+    isValid = undefined;
+    vatNumber = null;
   }
 
-  return HttpResponse.json({ tva });
+  return HttpResponse.json({ isValid, vatNumber });
 };
