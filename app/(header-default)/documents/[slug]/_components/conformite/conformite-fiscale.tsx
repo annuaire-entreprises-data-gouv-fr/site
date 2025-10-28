98d603256b69d0eead14cc8c59d09f09241f1aff
Alan Chauchet <alan.chauchet@gmail.com>
2025-10-28 11:59:07 +0100

    feat(conformite): handles errors in a new component

--- app/(header-default)/documents/[slug]/_components/conformite/conformite-fiscale.tsx
+++ app/(header-default)/documents/[slug]/_components/conformite/conformite-fiscale.tsx
@@ -6,42 +6,7 @@ import {
   isAPINotResponding,
 } from "#models/api-not-responding";
 import type { IConformiteFiscale } from "#models/espace-agent/conformite";
-
-const APINotRespongingElement: React.FC<{
-  data: IAPINotRespondingError;
-  administration: string;
-}> = ({ data, administration }) => (
-  <>
-    {(data?.errorType === 408 && (
-      <>
-        <i>
-          <Icon color="orange" slug="alertFill">
-            La récupération du document auprès des services {administration} a
-            pris trop de temps.
-          </Icon>
-        </i>
-        <br />
-        <a href="">Rechargez la page</a> ou ré-essayez plus-tard.
-        <br />
-        <br />
-      </>
-    )) || (
-      <>
-        <i>
-          <Icon color="#df0a00" slug="errorFill">
-            La récupération du document auprès des services {administration} a
-            échoué.
-          </Icon>
-        </i>
-        <br />
-        Ré-essayez plus tard ou rapprochez-vous de l’entreprise pour lui
-        demander la pièce directement.
-        <br />
-        <br />
-      </>
-    )}
-  </>
-);
+import APINotRespongingElement from "./conformite-not-responding";
 
 const administration = "DGFiP";
 
