5b189af2b8e1ab24703eb6f4245ebc96fa8dad69
AlanChauchet <alan.chauchet@gmail.com>
2025-10-29 18:20:33 +0100

    feat(espace-agent): reapply adds new espace agent page (#2098) (#2116)
    related to #2092

--- components/espace-agent-components/agent-navigation.tsx
+++ components/espace-agent-components/agent-navigation.tsx
@@ -1,36 +1,78 @@
 import ButtonLink from "#components-ui/button";
+import FullWidthContainer from "#components-ui/container";
+import { Icon } from "#components-ui/icon/wrapper";
 import constants from "#models/constants";
+import getSession from "#utils/server-side-helper/get-session";
 import AgentNavigationLink from "./agent-navigation-link";
+import styles from "./styles.module.css";
 
 export default async function AgentNavigation() {
+  const session = await getSession();
+
   const navLinks = [
-    { label: "Mon espace", href: "/compte/accueil" },
-    { label: "Mes groupes", href: "/compte/mes-groupes" },
+    { id: "accueil", label: "Mon espace", href: "/compte/accueil" },
+    { id: "groupes", label: "Mes groupes", href: "/compte/mes-groupes" },
   ];
   return (
-    <nav
+    <FullWidthContainer
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        margin: "1rem 0",
+        background:
+          "radial-gradient(61.94% 118.71% at 36.66% 38.06%, #F9C5E1 0%, #D8E6FF 100%)",
       }}
     >
-      <div style={{ display: "flex", gap: "2rem" }}>
-        {navLinks.map(({ label, href }) => (
-          <AgentNavigationLink href={href} key={href} label={label} />
-        ))}
-      </div>
-      <div>
-        <ButtonLink
-          alt
-          ariaLabel="Documentation"
-          target="_blank"
-          to={constants.links.documentation.home}
-        >
-          Documentation
-        </ButtonLink>
-      </div>
-    </nav>
+      <nav className={styles.navContainer}>
+        <div className={`${styles.navContainerContent}`}>
+          {session?.user?.isSuperAgent && (
+            <ul
+              aria-label="Navigation espace agent"
+              className={`${styles.navTabsList} fr-tabs__list`}
+              // biome-ignore lint/a11y/noNoninteractiveElementToInteractiveRole: This ul is used as a tablist
+              role="tablist"
+            >
+              {navLinks.map(({ label, href, id }) => (
+                <li className={styles.navTabItem} key={id} role="presentation">
+                  <AgentNavigationLink href={href} id={id} label={label} />
+                </li>
+              ))}
+            </ul>
+          )}
+          <div className={styles.navLinks}>
+            <div className={styles.button}>
+              <ButtonLink
+                alt
+                ariaLabel="Nouveautés"
+                hideExternalIcon
+                target="_blank"
+                to="/historique-des-modifications"
+              >
+                <Icon slug="giftFill">Nouveautés</Icon>
+              </ButtonLink>
+            </div>
+            <div className={styles.button}>
+              <ButtonLink
+                alt
+                ariaLabel="Salon Tchap"
+                hideExternalIcon
+                target="_blank"
+                to={constants.links.tchap}
+              >
+                <Icon slug="chat3Fill">Salon Tchap</Icon>
+              </ButtonLink>
+            </div>
+            <div className={styles.button}>
+              <ButtonLink
+                alt
+                ariaLabel="Documentation"
+                hideExternalIcon
+                target="_blank"
+                to={constants.links.documentation.home}
+              >
+                <Icon slug="bookletFill">Documentation</Icon>
+              </ButtonLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </FullWidthContainer>
   );
 }
