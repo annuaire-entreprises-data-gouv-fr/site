e5b05f65c542760fc600ac3b6cd5f9655e6f141e
xavier jouppe <xajouppe@gmail.com>
2025-10-29 18:10:39 +0100

    chore: setup an incident banner

--- components/banner/temp-incident.tsx
+++ components/banner/temp-incident.tsx
@@ -2,15 +2,15 @@ import { PrintNever } from "#components-ui/print-visibility";
 import constants from "#models/constants";
 import styles from "./styles.module.css";
 
-const TEMP_INCIDENT_BANNER_MESSAGE = `
-        🚨 Nous rencontrons actuellement des difficultés techniques, le service peut être temporairement indisponible.
-        Nos équipes sont mobilisées pour résoudre ce problème, nous vous prions de nous excuser
-        pour la gêne occasionnée.`;
-
-const DISPLAY_INCIDENT_BANNER = false;
+const TEMP_INCIDENT_BANNER = {
+  message: `
+  ⚠️ Notre partenaire ProConnect est en cours de mise à jour. Durant cette période, il est possible que les agents publics rencontrent des difficultés à se connecter.
+  Nos équipes sont mobilisées pour résoudre ce problème, nous vous prions de nous excuser pour la gêne occasionnée.`,
+  shouldDisplay: true,
+};
 
 export default function TempIncidentBanner() {
-  if (!DISPLAY_INCIDENT_BANNER) {
+  if (!TEMP_INCIDENT_BANNER.shouldDisplay) {
     return null;
   }
   return (
@@ -25,7 +25,7 @@ export default function TempIncidentBanner() {
           borderColor: constants.colors.frBlue,
         }}
       >
-        <div className="fr-container">{TEMP_INCIDENT_BANNER_MESSAGE}</div>
+        <div className="fr-container">{TEMP_INCIDENT_BANNER.message}</div>
       </div>
     </PrintNever>
   );
