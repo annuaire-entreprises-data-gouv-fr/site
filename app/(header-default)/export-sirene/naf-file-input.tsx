91e25d8a6063b937e73970ed320a36162613f427
Alan Chauchet <alan.chauchet@gmail.com>
2025-11-03 11:56:19 +0100

    feat(export): adds error handling fornaf file

--- app/(header-default)/export-sirene/naf-file-input.tsx
+++ app/(header-default)/export-sirene/naf-file-input.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 import ButtonLink from "#components-ui/button";
 import FAQLink from "#components-ui/faq-link";
 import { codesNAFRev2 } from "#utils/helpers/formatting/metadata/codes-NAF-rev-2";
@@ -13,10 +13,12 @@ const regexNAFWithoutDot = /^\d{4}[A-Z]$/;
 export function NAFFileInput(props: NAFFileInputProps) {
   const { onChangeNAF } = props;
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [invalidNAF, setInvalidNAF] = useState<string[]>([]);
 
   const handleFileChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       const file = e.target.files?.[0];
+      setInvalidNAF([]);
       if (file) {
         const reader = new FileReader();
         reader.onload = (e) => {
@@ -26,17 +28,31 @@ export function NAFFileInput(props: NAFFileInputProps) {
             .map((line) => line.trim())
             .filter((line) => line.length > 0);
 
-          const naf = lines
-            .map((line) =>
-              regexNAFWithoutDot.test(line)
-                ? `${line.slice(0, 2)}.${line.slice(2)}`
-                : line
-            )
-            .filter((line) => line in codesNAFRev2);
+          const formattedLines = lines.map((line) =>
+            regexNAFWithoutDot.test(line)
+              ? `${line.slice(0, 2)}.${line.slice(2)}`
+              : line
+          );
 
-          onChangeNAF({
-            naf,
-          });
+          const { naf, invalidNAF } = formattedLines.reduce(
+            (acc, line) => {
+              if (line in codesNAFRev2) {
+                acc.naf.push(line);
+              } else {
+                acc.invalidNAF.push(line);
+              }
+              return acc;
+            },
+            { naf: [] as string[], invalidNAF: [] as string[] }
+          );
+
+          if (invalidNAF.length > 0) {
+            setInvalidNAF(invalidNAF);
+          } else {
+            onChangeNAF({
+              naf,
+            });
+          }
         };
 
         reader.readAsText(file);
@@ -77,6 +93,21 @@ export function NAFFileInput(props: NAFFileInputProps) {
       >
         Sélectionner un fichier .txt
       </ButtonLink>
+      {invalidNAF.length > 0 && (
+        <div className={styles.errorMessage}>
+          <FAQLink
+            tooltipLabel={`${invalidNAF.length} code(s) NAF/APE invalide(s)`}
+          >
+            <ul>
+              {invalidNAF.slice(0, 5).map((naf) => (
+                <li key={naf}>{naf}</li>
+              ))}
+              {invalidNAF.length > 5 && <li>...</li>}
+            </ul>
+          </FAQLink>{" "}
+          détecté(s).
+        </div>
+      )}
     </div>
   );
 }
