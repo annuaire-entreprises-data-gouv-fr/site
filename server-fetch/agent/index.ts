dbb2a6dde622a5ecac37fae707fb66895c56bfeb
Alan Chauchet <alan.chauchet@gmail.com>
2025-11-13 21:28:45 +0100

    chore(actions): simplifies fetcher typings

--- server-fetch/agent/index.ts
+++ server-fetch/agent/index.ts
@@ -1,142 +1,107 @@
-import type { IAPINotRespondingError } from "#models/api-not-responding";
 import { ApplicationRights } from "#models/authentication/user/rights";
 import type { ISession } from "#models/authentication/user/session";
-import {
-  getAssociationProtected,
-  type IAssociationProtected,
-} from "#models/espace-agent/association-protected";
-import {
-  getOpqibi,
-  type IOpqibi,
-} from "#models/espace-agent/certificats/opqibi";
-import {
-  getQualibat,
-  type IQualibat,
-} from "#models/espace-agent/certificats/qualibat";
-import {
-  getQualifelec,
-  type IQualifelec,
-} from "#models/espace-agent/certificats/qualifelec";
+import { getAssociationProtected } from "#models/espace-agent/association-protected";
+import { getOpqibi } from "#models/espace-agent/certificats/opqibi";
+import { getQualibat } from "#models/espace-agent/certificats/qualibat";
+import { getQualifelec } from "#models/espace-agent/certificats/qualifelec";
 import { getDirigeantsProtected } from "#models/espace-agent/dirigeants-protected";
-import {
-  getEffectifsAnnuelsProtected,
-  type IEffectifsAnnuelsProtected,
-} from "#models/espace-agent/effectifs/annuels";
+import { getEffectifsAnnuelsProtected } from "#models/espace-agent/effectifs/annuels";
 import { getDocumentsRNEProtected } from "#models/espace-agent/rne-protected/documents";
-import type {
-  IDirigeantsWithMetadataMergedIGInpi,
-  IDocumentsRNE,
-} from "#models/rne/types";
-import {
-  getSubventionsAssociationFromSlug,
-  type ISubventions,
-} from "#models/subventions/association";
+import { getSubventionsAssociationFromSlug } from "#models/subventions/association";
+import type { Siren, Siret } from "#utils/helpers";
 import { withAgentRateLimiter } from "#utils/server-side-helper/with-agent-rate-limiter";
 import { withApplicationRight } from "#utils/server-side-helper/with-application-right";
 import { withErrorHandler } from "#utils/server-side-helper/with-error-handler";
 
-export const getOpqibiFetcher = withErrorHandler<
-  IOpqibi | IAPINotRespondingError,
-  [string, ISession | null]
->((siren, session) =>
-  withApplicationRight(
-    withAgentRateLimiter(() => getOpqibi(siren), session?.user?.email ?? null),
-    ApplicationRights.protectedCertificats,
-    session
-  )(siren, session)
+export const getOpqibiFetcher = withErrorHandler(
+  (siren: Siren, session: ISession | null) =>
+    withApplicationRight(
+      withAgentRateLimiter(
+        () => getOpqibi(siren),
+        session?.user?.email ?? null
+      ),
+      ApplicationRights.protectedCertificats,
+      session
+    )(siren, session)
 );
 
-export const getQualibatFetcher = withErrorHandler<
-  IQualibat | IAPINotRespondingError,
-  [string, ISession | null]
->((siret, session) =>
-  withApplicationRight(
-    withAgentRateLimiter(
-      () => getQualibat(siret),
-      session?.user?.email ?? null
-    ),
-    ApplicationRights.protectedCertificats,
-    session
-  )(siret, session)
+export const getQualibatFetcher = withErrorHandler(
+  (siret: Siret, session: ISession | null) =>
+    withApplicationRight(
+      withAgentRateLimiter(
+        () => getQualibat(siret),
+        session?.user?.email ?? null
+      ),
+      ApplicationRights.protectedCertificats,
+      session
+    )(siret, session)
 );
 
-export const getQualifelecFetcher = withErrorHandler<
-  IQualifelec | IAPINotRespondingError,
-  [string, ISession | null]
->((siret, session) =>
-  withApplicationRight(
-    withAgentRateLimiter(
-      () => getQualifelec(siret),
-      session?.user?.email ?? null
-    ),
-    ApplicationRights.protectedCertificats,
-    session
-  )(siret, session)
+export const getQualifelecFetcher = withErrorHandler(
+  (siret: Siret, session: ISession | null) =>
+    withApplicationRight(
+      withAgentRateLimiter(
+        () => getQualifelec(siret),
+        session?.user?.email ?? null
+      ),
+      ApplicationRights.protectedCertificats,
+      session
+    )(siret, session)
 );
 
-export const getDirigeantsProtectedFetcher = withErrorHandler<
-  IDirigeantsWithMetadataMergedIGInpi | IAPINotRespondingError,
-  [string, boolean, ISession | null]
->((siren, isEI, session) =>
-  withApplicationRight(
-    withAgentRateLimiter(
-      () => getDirigeantsProtected(siren, isEI),
-      session?.user?.email ?? null
-    ),
-    ApplicationRights.mandatairesRCS,
-    session
-  )(siren, session)
+export const getDirigeantsProtectedFetcher = withErrorHandler(
+  (siren: Siren, isEI: boolean, session: ISession | null) =>
+    withApplicationRight(
+      withAgentRateLimiter(
+        () => getDirigeantsProtected(siren, isEI),
+        session?.user?.email ?? null
+      ),
+      ApplicationRights.mandatairesRCS,
+      session
+    )(siren, session)
 );
 
-export const getAgentRNEDocumentsFetcher = withErrorHandler<
-  IDocumentsRNE | IAPINotRespondingError,
-  [string, ISession | null]
->((siren, session) =>
-  withApplicationRight(
-    withAgentRateLimiter(
-      () => getDocumentsRNEProtected(siren),
-      session?.user?.email ?? null
-    ),
-    ApplicationRights.documentsRne,
-    session
-  )(siren, session)
+export const getAgentRNEDocumentsFetcher = withErrorHandler(
+  (siren: Siren, session: ISession | null) =>
+    withApplicationRight(
+      withAgentRateLimiter(
+        () => getDocumentsRNEProtected(siren),
+        session?.user?.email ?? null
+      ),
+      ApplicationRights.documentsRne,
+      session
+    )(siren, session)
 );
 
-export const getAgentAssociationProtectedFetcher = withErrorHandler<
-  IAssociationProtected | IAPINotRespondingError,
-  [string, ISession | null]
->((siren, session) =>
-  withApplicationRight(
-    () => getAssociationProtected(siren),
-    ApplicationRights.associationProtected,
-    session
-  )(siren, session)
+export const getAgentAssociationProtectedFetcher = withErrorHandler(
+  (siren: Siren, session: ISession | null) =>
+    withApplicationRight(
+      () => getAssociationProtected(siren),
+      ApplicationRights.associationProtected,
+      session
+    )(siren, session)
 );
 
-export const getAgentEffectifsAnnuelsProtectedFetcher = withErrorHandler<
-  IEffectifsAnnuelsProtected | IAPINotRespondingError,
-  [string, ISession | null]
->((siren, session) =>
-  withApplicationRight(
-    withAgentRateLimiter(
-      () => getEffectifsAnnuelsProtected(siren),
-      session?.user?.email ?? null
-    ),
-    ApplicationRights.effectifsAnnuels,
-    session
-  )(siren, session)
+export const getAgentEffectifsAnnuelsProtectedFetcher = withErrorHandler(
+  (siren, session) =>
+    withApplicationRight(
+      withAgentRateLimiter(
+        () => getEffectifsAnnuelsProtected(siren),
+        session?.user?.email ?? null
+      ),
+      ApplicationRights.effectifsAnnuels,
+      session
+    )(siren, session)
 );
 
-export const getAgentSubventionsAssociationFetcher = withErrorHandler<
-  ISubventions | IAPINotRespondingError,
-  [string, ISession | null]
->((siren, session) =>
-  withApplicationRight(
-    withAgentRateLimiter(
-      () => getSubventionsAssociationFromSlug(siren),
-      session?.user?.email ?? null
-    ),
-    ApplicationRights.subventionsAssociation,
-    session
-  )(siren, session)
+export const getAgentSubventionsAssociationFetcher = withErrorHandler(
+  (siren: Siren, session: ISession | null) =>
+    withApplicationRight(
+      withAgentRateLimiter(
+        () => getSubventionsAssociationFromSlug(siren),
+        session?.user?.email ?? null
+      ),
+      ApplicationRights.subventionsAssociation,
+      session
+    )(siren, session)
 );
