adb16ec5eff906d57e0425d7d8275c5ed80aaed9
Alan Chauchet <alan.chauchet@gmail.com>
2025-11-13 14:32:22 +0100

    chore(actions): converts verify-eori to server fetch

--- components/eori-cell/index.tsx
+++ components/eori-cell/index.tsx
@@ -1,87 +1,26 @@
-"use client";
-import { useMemo } from "react";
-import { getEoriValidationAction } from "server-actions/public/data-fetching";
-import { CopyPaste } from "#components/table/copy-paste";
-import { Icon } from "#components-ui/icon/wrapper";
-import InformationTooltip from "#components-ui/information-tooltip";
+import { Suspense } from "react";
 import { Loader } from "#components-ui/loader";
-import { useServerActionData } from "#hooks/fetch/use-server-action-data";
-import { isAPINotResponding } from "#models/api-not-responding";
-import type { ISession } from "#models/authentication/user/session";
-import {
-  hasFetchError,
-  isDataLoading,
-  isUnauthorized,
-} from "#models/data-fetching";
-import { formatSiret, type Siret } from "#utils/helpers";
+import { getEORIValidation } from "#models/eori-validation";
+import type { Siret } from "#utils/helpers";
+import { withErrorHandler } from "#utils/server-side-helper/with-error-handler";
+import { EORICellContent } from "./content";
 
 type IProps = {
   siret: Siret;
-  session: ISession | null;
 };
-export default function EORICell({ siret, session }: IProps) {
-  const input = useMemo(() => ({ siret }), [siret]);
-  const eoriValidation = useServerActionData(
-    getEoriValidationAction,
-    session,
-    input
-  );
+export default function EORICell({ siret }: IProps) {
+  const eoriValidation = withErrorHandler(() => getEORIValidation(siret));
 
-  if (isDataLoading(eoriValidation)) {
-    return (
-      <>
-        <Loader />
-        &nbsp;
-      </>
-    );
-  }
-
-  if (hasFetchError(eoriValidation) || isUnauthorized(eoriValidation)) {
-    return (
-      <InformationTooltip
-        horizontalOrientation="left"
-        label={
-          <>
-            Nous n’avons pas pu controler la validité du numéro EORI à cause
-            d’une erreur de connexion. Vous pouvez essayer de rafraichir la
-            page, ou revenir plus tard.
-          </>
-        }
-        left="5px"
-        tabIndex={0}
-      >
-        <Icon color="#df0a00" slug="errorFill">
-          <em>Erreur de connexion</em>
-        </Icon>
-      </InformationTooltip>
-    );
-  }
-
-  if (isAPINotResponding(eoriValidation)) {
-    return (
-      <InformationTooltip
-        horizontalOrientation="left"
-        label={
-          <>
-            Nous n’avons pas pu controler la validité du numéro EORI car le
-            téléservice ne fonctionne pas actuellement. Merci de ré-essayer plus
-            tard pour vérifier si cette structure possède un numéro EORI
-          </>
-        }
-        left="5px"
-        tabIndex={0}
-      >
-        <Icon color="#df0a00" slug="errorFill">
-          <em>Service indisponible</em>
-        </Icon>
-      </InformationTooltip>
-    );
-  }
-  return eoriValidation.isValid ? (
-    <CopyPaste label="eori" shouldRemoveSpace>
-      {formatSiret(eoriValidation.eori)}
-    </CopyPaste>
-  ) : (
-    <i>Pas de n° EORI valide</i>
+  return (
+    <Suspense
+      fallback={
+        <>
+          <Loader />
+          &nbsp;
+        </>
+      }
+    >
+      <EORICellContent eoriValidation={eoriValidation} />
+    </Suspense>
   );
 }
--- server-actions/public/data-fetching/schemas.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const getEoriValidationSchema = z.object({
-  siret: z.string().min(1, "Siret is required"),
-});
