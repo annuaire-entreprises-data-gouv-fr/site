63698f82927f505ba2d95893b444dc0299e7d6f2
Alan Chauchet <alan.chauchet@gmail.com>
2025-11-25 13:25:24 +0100

    fix(spectacles): adds error handler to spectacle vivant client

--- clients/api-data-gouv/entrepreneurs-spectacles/index.ts
+++ clients/api-data-gouv/entrepreneurs-spectacles/index.ts
@@ -1,31 +1,62 @@
 import { HttpNotFound } from "#clients/exceptions";
 import routes from "#clients/routes";
+import { EAdministration } from "#models/administrations/EAdministration";
+import {
+  APINotRespondingFactory,
+  type IAPINotRespondingError,
+} from "#models/api-not-responding";
 import constants from "#models/constants";
-import type { Siren } from "#utils/helpers";
+import type { IUniteLegale } from "#models/core/types";
+import { FetchRessourceException } from "#models/exceptions";
 import { httpGet } from "#utils/network";
+import logErrorInSentry from "#utils/sentry";
 import type {
   IEntrepreneursSpectacles,
   IEntrepreneursSpectaclesDatagouvResponse,
 } from "./interface";
 
 export const clientEntrepreneursSpectacles = async (
-  siren: Siren,
+  uniteLegale: Pick<IUniteLegale, "siren" | "complements">,
   page = 1
-): Promise<IEntrepreneursSpectacles> => {
-  const response = await httpGet<IEntrepreneursSpectaclesDatagouvResponse>(
-    `${routes.datagouv.entrepreneursSpectacles}?siren_siret__sort=asc&siren_siret__contains=${siren}&page=${page}&page_size=20`,
-    {
-      timeout: constants.timeout.S,
+): Promise<IEntrepreneursSpectacles | IAPINotRespondingError> => {
+  const siren = uniteLegale.siren;
+
+  try {
+    if (!uniteLegale.complements.estEntrepreneurSpectacle) {
+      throw new HttpNotFound("Not entrepreneur spectacle");
     }
-  );
 
-  if (response.data.length === 0) {
-    throw new HttpNotFound(
-      `No Spectacles Vivants record found for siren ${siren}`
+    const response = await httpGet<IEntrepreneursSpectaclesDatagouvResponse>(
+      `${routes.datagouv.entrepreneursSpectacles}?siren_siret__sort=asc&siren_siret__contains=${siren}&page=${page}&page_size=20`,
+      {
+        timeout: constants.timeout.S,
+      }
     );
-  }
 
-  return mapToDomainObject(response);
+    if (response.data.length === 0) {
+      throw new HttpNotFound(
+        `No Spectacles Vivants record found for siren ${siren}`
+      );
+    }
+
+    return mapToDomainObject(response);
+  } catch (e: any) {
+    if (e instanceof HttpNotFound) {
+      return APINotRespondingFactory(EAdministration.DINUM, 404);
+    }
+
+    logErrorInSentry(
+      new FetchRessourceException({
+        cause: e,
+        ressource: "EntrepreneurSpectacles",
+        context: {
+          siren: uniteLegale.siren,
+        },
+        administration: EAdministration.DINUM,
+      })
+    );
+    return APINotRespondingFactory(EAdministration.DINUM, 500);
+  }
 };
 
 const mapToDomainObject = (
