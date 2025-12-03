74c24198bd7d040085a1e4d5bcd5ca01e3a6bc3a
Alan Chauchet <alan.chauchet@gmail.com>
2025-12-03 16:15:26 +0100

    chore(next): uses standalone output for next builds

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,10 @@ if (
 ) {
   // Mock server for Cypress, must be put here according to :
   // https://github.com/mswjs/examples/pull/101/files
-  const { mockServer } = require("#cypress/mocks/server");
-  mockServer.listen({
-    onUnhandledRequest: "error",
+  import("#cypress/mocks/server").then(({ mockServer }) => {
+    mockServer.listen({
+      onUnhandledRequest: "error",
+    });
   });
 }
 
