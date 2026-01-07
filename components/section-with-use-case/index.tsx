43abf8ff750f76b71e6226b1387bcb27c0749a72
Alan Chauchet <alan.chauchet@gmail.com>
2026-01-07 09:31:31 +0100

    fix(section): fixes wording

--- components/section-with-use-case/index.tsx
+++ components/section-with-use-case/index.tsx
@@ -26,7 +26,7 @@ type WrappedSectionProps = {
 const DefaultIntroContent = () => (
   <p>
     Les agents publics peuvent demander à accéder à cette donnée uniquement dans
-    les cas d'usages justifiant d'un intérêt légitime. En déclarant le cadre
+    les cas d'usage justifiant d'un intérêt légitime. En déclarant le cadre
     juridique dans lequel vous accédez à ces données, vous vous engagez{" "}
     <Link href="/modalites-utilisation">
       à respecter nos modalités d'utilisation
