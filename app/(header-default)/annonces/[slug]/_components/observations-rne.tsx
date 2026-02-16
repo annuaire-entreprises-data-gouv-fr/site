190a286f703bf548fb2d6f7a1158372932b9999e
Alan Chauchet <alan.chauchet@gmail.com>
2026-02-16 16:25:38 +0100

    chore(api): reverts observations RNE

--- app/(header-default)/annonces/[slug]/_components/observations-rne.tsx
+++ app/(header-default)/annonces/[slug]/_components/observations-rne.tsx
@@ -1,23 +1,27 @@
-import { after } from "next/server";
-import { getRNEObservationsFetcher } from "server-fetch/public";
-import { AsyncDataSectionServer } from "#components/section/data-section/server";
+"use client";
+
+import { APIRoutesPaths } from "app/api/data-fetching/routes-paths";
+import { useAPIRouteData } from "hooks/fetch/use-API-route-data";
+import { AsyncDataSectionClient } from "#components/section/data-section/client";
+import { FullTable } from "#components/table/full";
+import InpiPartiallyDownWarning from "#components-ui/alerts-with-explanations/inpi-partially-down";
+import { Tag } from "#components-ui/tag";
 import { EAdministration } from "#models/administrations/EAdministration";
+import type { ISession } from "#models/authentication/user/session";
 import type { IUniteLegale } from "#models/core/types";
-import { ObservationsRNEContent } from "./content";
 
 export const ObservationsRNE: React.FC<{
   uniteLegale: IUniteLegale;
-}> = ({ uniteLegale }) => {
-  const controller = new AbortController();
-  const observations = getRNEObservationsFetcher(uniteLegale.siren, controller);
-
-  after(() => {
-    controller.abort();
-  });
+  session: ISession | null;
+}> = ({ uniteLegale, session }) => {
+  const observations = useAPIRouteData(
+    APIRoutesPaths.Observations,
+    uniteLegale.siren,
+    session
+  );
 
   return (
-    <AsyncDataSectionServer
-      ContentComponent={ObservationsRNEContent}
+    <AsyncDataSectionClient
       data={observations}
       id="observations-rne"
       notFoundInfo={
@@ -26,9 +30,34 @@ export const ObservationsRNE: React.FC<{
           <strong>Registre National des Entreprises (RNE)</strong>.
         </>
       }
-      otherContentProps={{}}
       sources={[EAdministration.INPI]}
       title="Observations au RNE"
-    />
+    >
+      {({ data, metadata }) =>
+        data.length > 0 ? (
+          <>
+            {metadata.isFallback && <InpiPartiallyDownWarning />}
+            <p>
+              Cette structure possède {data.length} observation(s) au{" "}
+              <strong>RNE</strong>
+              &nbsp;:
+            </p>
+            <FullTable
+              body={data.map((o) => [
+                o.dateAjout,
+                o.numObservation ? <Tag>{o.numObservation}</Tag> : "",
+                o.description,
+              ])}
+              head={["Date d’ajout", "Numéro d’observation", "Description"]}
+            />
+          </>
+        ) : (
+          <>
+            Cette structure ne possède pas d’observations au{" "}
+            <strong>Registre National des Entreprises (RNE)</strong>.
+          </>
+        )
+      }
+    </AsyncDataSectionClient>
   );
 };
--- server-fetch/public/index.ts
+++
@@ -1,5 +0,0 @@
-import { getRNEObservations } from "#models/rne/observations";
-import { createPublicFetcher } from "./middlewares";
-
-export const getRNEObservationsFetcher =
-  createPublicFetcher(getRNEObservations).build();
