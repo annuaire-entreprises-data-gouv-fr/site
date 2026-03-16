5bc4334a07b8aac56f5500be584037217909a981
Alan Chauchet <alan.chauchet@gmail.com>
2026-03-16 09:24:51 +0100

    fix(fc): fixes FranceConnect button

--- components-ui/button-france-connect/index.tsx
+++ components-ui/button-france-connect/index.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 const ButtonFranceConnect: React.FC<{}> = () => (
   <div className="fr-connect-group">
-    <button className="fr-connect" type="button">
+    <button className="fr-connect" type="submit">
       <span className="fr-connect__login">S’identifier avec</span>
       <span className="fr-connect__brand">FranceConnect</span>
     </button>
