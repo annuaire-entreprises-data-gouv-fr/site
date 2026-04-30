77902ec19196022c9f59a9c7f42202bf24474e1c
Alan Chauchet <alan.chauchet@gmail.com>
2026-04-30 14:06:05 +0200

    refactor

---
+++ components/welcome-modal-agent/storage.ts
@@ -0,0 +1,14 @@
+import { useStorage } from "hooks";
+import {
+  DATA_ACCESS_REMINDER_COOKIE_MAX_AGE_SECONDS,
+  DATA_ACCESS_REMINDER_MODAL_ID,
+  INITIAL_WELCOME_MODAL_ID,
+} from "./constants";
+
+export const useHasSeenInitialWelcomeModal = () =>
+  useStorage("local", INITIAL_WELCOME_MODAL_ID, false);
+
+export const useHasSeenDataAccessReminderModal = () =>
+  useStorage("cookie", DATA_ACCESS_REMINDER_MODAL_ID, false, {
+    maxAge: DATA_ACCESS_REMINDER_COOKIE_MAX_AGE_SECONDS,
+  });
