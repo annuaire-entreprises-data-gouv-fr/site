486e90369e6517d2846dd9af95e71422e39ab3fe
Alan Chauchet <alan.chauchet@gmail.com>
2026-05-13 18:09:44 +0200

    feat(start): setups divers page

--- src/routes/_header-default/divers.$slug.tsx
+++ src/routes/_header-default/divers.$slug.tsx
@@ -1,11 +1,81 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ConventionsCollectivesSection from "#/components/conventions-collectives-section";
+import Title from "#/components/title-section";
+import { FICHE } from "#/components/title-section/tabs";
+import { useAuth } from "#/contexts/auth.context";
+import { getAllIdccWithMetadata } from "#/models/conventions-collectives";
+import { getRechercheEntrepriseSourcesLastModified } from "#/models/recherche-entreprise-modified";
+import { meta } from "#/seo";
+import { getUniteLegaleFromSlugFn } from "#/server-functions/public/unite-legale";
+import {
+  uniteLegalePageDescription,
+  uniteLegalePageTitle,
+} from "#/utils/helpers";
 import { HeaderDefaultError } from "./-error";
 
 export const Route = createFileRoute("/_header-default/divers/$slug")({
+  loader: async ({ params }) => {
+    const [uniteLegale, sourcesLastModified] = await Promise.all([
+      getUniteLegaleFromSlugFn({
+        data: { slug: params.slug },
+      }),
+      getRechercheEntrepriseSourcesLastModified(),
+    ]);
+    const ccWithMetadata = await getAllIdccWithMetadata(uniteLegale.siren);
+
+    return { uniteLegale, ccWithMetadata, sourcesLastModified };
+  },
+  head: ({ loaderData }) => {
+    if (!loaderData) {
+      return {
+        meta: meta({
+          title: "Page non trouvée",
+          robots: {
+            follow: false,
+          },
+        }),
+      };
+    }
+
+    const { uniteLegale } = loaderData;
+    const canonical = `https://annuaire-entreprises.data.gouv.fr/divers/${uniteLegale.siren}`;
+    return {
+      meta: meta({
+        title: `Conventions collectives - ${uniteLegalePageTitle(uniteLegale)}`,
+        description: uniteLegalePageDescription(uniteLegale),
+        robots: {
+          follow: true,
+          index: false,
+        },
+        alternates: {
+          canonical,
+        },
+      }),
+      links: [
+        {
+          rel: "canonical",
+          href: canonical,
+        },
+      ],
+    };
+  },
   component: RouteComponent,
   errorComponent: HeaderDefaultError,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_header-default/divers/$slug"!</div>;
+  const { uniteLegale, ccWithMetadata, sourcesLastModified } =
+    Route.useLoaderData();
+
+  const { user } = useAuth();
+
+  return (
+    <div className="content-container">
+      <Title ficheType={FICHE.DIVERS} uniteLegale={uniteLegale} user={user} />
+      <ConventionsCollectivesSection
+        ccLastModified={sourcesLastModified.idcc}
+        ccWithMetadata={ccWithMetadata}
+      />
+    </div>
+  );
 }
