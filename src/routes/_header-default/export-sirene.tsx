b967c87efe73728e258730185e39e4e58d3ae826
Alan Chauchet <alan.chauchet@gmail.com>
2026-05-14 14:27:40 +0200

    feat(start): setups export-sirene page

--- src/routes/_header-default/export-sirene.tsx
+++ src/routes/_header-default/export-sirene.tsx
@@ -1,11 +1,31 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ExportCsv from "#/components/screens/export-sirene/export-csv";
+import { meta } from "#/seo";
 import { HeaderDefaultError } from "./-error";
 
 export const Route = createFileRoute("/_header-default/export-sirene")({
+  head: () => {
+    const canonical = "https://annuaire-entreprises.data.gouv.fr/export-sirene";
+    return {
+      meta: meta({
+        title:
+          "Générer une liste CSV d‘entreprises | L’Annuaire des Entreprises",
+        alternates: {
+          canonical,
+        },
+      }),
+      links: [
+        {
+          rel: "canonical",
+          href: canonical,
+        },
+      ],
+    };
+  },
   component: RouteComponent,
   errorComponent: HeaderDefaultError,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_header-default/export-sirene"!</div>;
+  return <ExportCsv />;
 }
