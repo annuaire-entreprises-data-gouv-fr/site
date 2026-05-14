f81212f0125c5cbeacd817ab403eb31cd6bae044
Alan Chauchet <alan.chauchet@gmail.com>
2026-05-14 15:05:42 +0200

    feat(start): setups formulaire page

--- src/routes/_header-default/formulaire.supprimer-donnees-personnelles-entreprise.tsx
+++ src/routes/_header-default/formulaire.supprimer-donnees-personnelles-entreprise.tsx
@@ -1,18 +1,60 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import HidePersonalDataPageClient from "#/components/screens/formulaire.supprimer-donnees-personnelles-entreprise";
+import TextWrapper from "#/components-ui/text-wrapper";
+import { meta } from "#/seo";
+import {
+  getCurrentSession,
+  getHidePersonalDataRequestFCSession,
+} from "#/utils/session";
 import { HeaderDefaultError } from "./-error";
+
+const fetchHidePersonalDataRequestFn = createServerFn().handler(async () => {
+  const session = await getCurrentSession();
+
+  const franceConnectInfo = getHidePersonalDataRequestFCSession(session.data);
+
+  return franceConnectInfo
+    ? {
+        franceConnectInfo: {
+          firstName: franceConnectInfo.firstName,
+          familyName: franceConnectInfo.familyName,
+          birthdate: franceConnectInfo.birthdate,
+        },
+        hasInvalidFCSession: false,
+      }
+    : {
+        franceConnectInfo: null,
+        hasInvalidFCSession:
+          !!session.data.franceConnectHidePersonalDataSession,
+      };
+});
 
 export const Route = createFileRoute(
   "/_header-default/formulaire/supprimer-donnees-personnelles-entreprise"
 )({
+  loader: async () => await fetchHidePersonalDataRequestFn(),
+  head: () => ({
+    meta: meta({
+      title: "Demande de suppression de données personnelles",
+      description:
+        "Demande de suppression de données personnelles de dirigeant d’entreprise sur l’Annuaire des Entreprises",
+    }),
+  }),
   component: RouteComponent,
   errorComponent: HeaderDefaultError,
 });
 
 function RouteComponent() {
+  const { franceConnectInfo, hasInvalidFCSession } = Route.useLoaderData();
+
   return (
-    <div>
-      Hello
-      "/_header-default/formulaire/supprimer-donnees-personnelles-entreprise"!
-    </div>
+    <TextWrapper>
+      <h1>Demande de suppression de données personnelles</h1>
+      <HidePersonalDataPageClient
+        franceConnectInfo={franceConnectInfo}
+        hasInvalidFCSession={hasInvalidFCSession}
+      />
+    </TextWrapper>
   );
 }
