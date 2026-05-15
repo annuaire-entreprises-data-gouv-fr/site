d36b91b024be585fa6b57bbafed2a3054935aa37
Alan Chauchet <alan.chauchet@gmail.com>
2026-05-15 15:49:49 +0200

    feat(start): fixes immatriculation.cy

--- cypress/e2e/fonctionnalites/immatriculation.cy.js
+++ cypress/e2e/fonctionnalites/immatriculation.cy.js
@@ -2,24 +2,23 @@ describe("Immatriculation RNE", () => {
   it("Should display immatriculation", () => {
     cy.visit("/entreprise/552032534");
     cy.contains("Immatriculée au RNE");
-    cy.contains("01/03/2054");
+    cy.contains("01/03/1955");
   });
 
   it("Should display immatriculation even for non diffusible", () => {
     cy.visit("/entreprise/300025764");
     cy.contains("Immatriculée au RNE");
-    cy.contains("Libérale non réglementée, Gestion de biens");
+    cy.contains("Gestion de biens, Libérale non réglementée");
   });
 
   it("Should display immatriculation even for closed structure", () => {
     cy.visit("/entreprise/880878145");
     cy.contains("Radiée au RNE");
-    cy.contains("23/01/2119");
+    cy.contains("14/11/2022");
   });
 
-  // could also use 929081255, 784410607
   it("Should display warning when not found in RNE", () => {
-    cy.visit("/entreprise/356000000");
+    cy.visit("/entreprise/784410607");
     cy.contains("Non trouvée dans le RNE");
   });
 });
