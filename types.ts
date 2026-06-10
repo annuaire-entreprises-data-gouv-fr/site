84068aefc007327512439fa69b5c21fa03de00a8
Alan Chauchet <alan.chauchet@gmail.com>
2026-06-10 15:09:01 +0200

    feat(matomo): adds ab test for filtres avances

--- types.ts
+++ types.ts
@@ -2,21 +2,23 @@ export type Entries<T> = {
   [K in keyof T]: [K, T[K]];
 }[keyof T][];
 
+export interface MatomoAbTestVariations {
+  FiltresAvances: "original" | "VariationA" | "VariationB";
+}
+
 declare global {
   interface Window {
     _paq: any[];
     matomoAbTesting: {
-      getVariation(
-        name: keyof Window["matomoAbTesting"]["variations"]
-      ): "original" | "Variation1";
-      setVariation(
-        name: keyof Window["matomoAbTesting"]["variations"],
-        variation: "original" | "Variation1"
+      getVariation<T extends keyof MatomoAbTestVariations>(
+        name: T
+      ): MatomoAbTestVariations[T];
+      setVariation<T extends keyof MatomoAbTestVariations>(
+        name: T,
+        variation: MatomoAbTestVariations[T]
       ): void;
       subscribe(listener: () => void): () => void;
-      variations: {
-        AdvancedSearchFilters: "original" | "Variation1";
-      };
+      variations: MatomoAbTestVariations;
     };
   }
 }
