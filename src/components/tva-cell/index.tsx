fe1254b9f417bd79964d14d6bb3067b42784169c
Alan Chauchet <alan.chauchet@gmail.com>
2026-06-15 10:38:16 +0200

    refactor(tva): simplify NoTVA component logic for better readability

--- src/components/tva-cell/index.tsx
+++ src/components/tva-cell/index.tsx
@@ -3,19 +3,21 @@ import { estActif } from "#/models/core/etat-administratif";
 import type { IUniteLegale } from "#/models/core/types";
 import TVAList from "./tva-list";
 
-const NoTVA = ({ isActive }: { isActive: boolean }) => (
-  <i>
-    Pas de{" "}
-    <FAQLink
-      to="/definitions/tva-intracommunautaire"
-      tooltipLabel="n° TVA valide"
-    >
-      {isActive
-        ? ""
-        : "Cette structure n’est plus en activité, par conséquent elle ne peut pas avoir de numéro de TVA valide."}
-    </FAQLink>
-  </i>
-);
+const NoTVA = ({ isActive }: { isActive: boolean }) =>
+  isActive ? (
+    <i>Pas de n° TVA valide</i>
+  ) : (
+    <i>
+      Pas de{" "}
+      <FAQLink
+        to="/definitions/tva-intracommunautaire"
+        tooltipLabel="n° TVA valide"
+      >
+        Cette structure n’est plus en activité, par conséquent elle ne peut pas
+        avoir de numéro de TVA valide.
+      </FAQLink>
+    </i>
+  );
 
 const TVACell: React.FC<{
   uniteLegale: IUniteLegale;
