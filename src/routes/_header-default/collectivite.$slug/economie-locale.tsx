ae154ea522d19551c8a4fc1f550283ab839aeb32
Alan Chauchet <alan.chauchet@gmail.com>
2026-07-01 11:13:43 +0200

    adds effectifs par grand secteur

--- src/routes/_header-default/collectivite.$slug/economie-locale.tsx
+++ src/routes/_header-default/collectivite.$slug/economie-locale.tsx
@@ -1,19 +1,42 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import {
+  type CollectiviteEconomieLocaleEffectifsResponse,
+  CollectiviteEconomieLocaleSection,
+} from "#/components/collectivite/economie-locale";
 import { CollectiviteMap } from "#/components/collectivite/map";
+import { httpGet } from "#/utils/network";
 import { Route as CollectiviteRoute } from "./route";
+
+const loadRouteData = createServerFn().handler(async () => {
+  const effectifs = await httpGet<CollectiviteEconomieLocaleEffectifsResponse>(
+    "https://ade.s3.sbg.io.cloud.ovh.net/ae/dev/adc/13101.json"
+  );
+
+  return {
+    effectifs,
+  };
+});
 
 export const Route = createFileRoute(
   "/_header-default/collectivite/$slug/economie-locale"
 )({
   component: RouteComponent,
+  loader: async () => {
+    const result = await loadRouteData();
+
+    return result;
+  },
 });
 
 function RouteComponent() {
   const { uniteLegale, geoCommune } = CollectiviteRoute.useLoaderData();
+  const { effectifs } = Route.useLoaderData();
 
   return (
     <>
       <CollectiviteMap geoCommune={geoCommune} uniteLegale={uniteLegale} />
+      <CollectiviteEconomieLocaleSection effectifs={effectifs} />
     </>
   );
 }
