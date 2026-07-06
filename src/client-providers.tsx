52f6910c10308dc869901d8eee218c95c8c7c22b
Alan Chauchet <alan.chauchet@gmail.com>
2026-07-06 16:14:02 +0200

    feat(client): implement hash scroll restoration in ClientProviders

--- src/client-providers.tsx
+++ src/client-providers.tsx
@@ -1,7 +1,11 @@
+import { useLocation } from "@tanstack/react-router";
+import { useEffect } from "react";
 import { BrowserIsOutdatedBanner } from "./components/banner/browser-is-outdated";
 import { AuthProvider } from "./contexts/auth.context";
 import { useMatomoInit } from "./matomo-init";
 import type { ISession } from "./models/authentication/user/session";
+
+const HASH_SCROLL_RETRY_DELAYS_MS = [0, 50, 150, 350];
 
 export function ClientProviders({
   children,
@@ -14,7 +18,39 @@ export function ClientProviders({
 
   return (
     <BrowserIsOutdatedBanner>
-      <AuthProvider user={user}>{children}</AuthProvider>
+      <AuthProvider user={user}>
+        <HashScrollRestoration />
+        {children}
+      </AuthProvider>
     </BrowserIsOutdatedBanner>
   );
+}
+
+function HashScrollRestoration() {
+  const hash = useLocation({
+    select: (location) => location.hash,
+  });
+
+  useEffect(() => {
+    if (!hash) {
+      return;
+    }
+
+    const timeoutIds = HASH_SCROLL_RETRY_DELAYS_MS.map((delay) =>
+      window.setTimeout(() => {
+        document.getElementById(hash)?.scrollIntoView({
+          block: "start",
+          inline: "nearest",
+        });
+      }, delay)
+    );
+
+    return () => {
+      for (const timeoutId of timeoutIds) {
+        window.clearTimeout(timeoutId);
+      }
+    };
+  }, [hash]);
+
+  return null;
 }
