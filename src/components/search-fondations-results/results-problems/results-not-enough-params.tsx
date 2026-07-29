162886820eaee839c718ed2b9cbf119209f56b2d
AlanChauchet <alan.chauchet@gmail.com>
2026-07-29 16:23:02 +0200

    feat(fondations): adds fondations lp (#2653)

---
+++ src/components/search-fondations-results/results-problems/results-not-enough-params.tsx
@@ -0,0 +1,9 @@
+export const NotEnoughParams = () => (
+  <h3>
+    🔍 Votre requête ne contient pas assez de paramètres de recherche pour nous
+    permettre
+    <br />
+    de vous proposer un résultat, veuillez utiliser un terme de recherche plus
+    long (au moins 3 lettres).
+  </h3>
+);
--- src/routes/_header-home/lp/$slug.tsx
+++ src/routes/_header-home/lp/$slug.tsx
@@ -57,14 +57,16 @@ function RouteComponent() {
     title,
     description,
     filter,
+    searchPath,
     reassurance = [],
     datasources = [],
     body,
+    searchPlaceholder,
   } = landingPage;
   return (
     <>
       <form
-        action={"/rechercher"}
+        action={searchPath || "/rechercher"}
         className={`${styles["centered-search"]} layout-center`}
         id="search-bar-form"
         method="get"
@@ -77,17 +79,19 @@ function RouteComponent() {
           {title}
         </h1>
         <h2 className={styles["sub-title"]}>{description}</h2>
-        <input
-          name={filter.name}
-          readOnly
-          style={{ display: "none" }}
-          value={filter.value}
-        />
+        {filter && (
+          <input
+            name={filter.name}
+            readOnly
+            style={{ display: "none" }}
+            value={filter.value}
+          />
+        )}
         <div className={styles["search-bar-wrapper"]}>
           <SearchBar
             autoFocus={true}
             defaultValue=""
-            placeholder="Nom, adresse, n° SIRET/SIREN..."
+            placeholder={searchPlaceholder || "Nom, adresse, n° SIRET/SIREN..."}
           />
         </div>
       </form>
