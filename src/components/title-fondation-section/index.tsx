f795f4d7a535a969516751036ef41f9b0f5d82e5
AlanChauchet <alan.chauchet@gmail.com>
2026-07-31 08:53:34 +0200

    feat(fondations): adds fondation resume page (#2664)

---
+++ src/components/title-fondation-section/index.tsx
@@ -0,0 +1,65 @@
+import { Link } from "#/components/link";
+import { FondationBadge } from "#/components-ui/badge/frequent";
+import SocialMedia from "#/components-ui/social-media";
+import type { IAgentInfo } from "#/models/authentication/agent";
+import type { IFondation } from "#/models/core/fondations.types";
+import type { IUniteLegale } from "#/models/core/types";
+import { CopyPaste } from "../table/copy-paste";
+import styles from "../title-section/styles.module.css";
+import { FICHE, Tabs } from "../title-section/tabs";
+import { UniteLegaleEtablissementCountDescription } from "../unite-legale-description/etablissement-count-description";
+
+interface IProps {
+  fondation: IFondation;
+  uniteLegale: IUniteLegale | null;
+  user: IAgentInfo | null;
+}
+
+export function TitleFondation(props: IProps) {
+  const { fondation, uniteLegale, user } = props;
+  return (
+    <div className={styles.headerSection}>
+      <div>
+        <h1>
+          <Link params={{ slug: fondation.id }} to="/fondation/$slug">
+            {fondation.title}
+          </Link>
+        </h1>
+        <div className={styles.subTitle}>
+          <FondationBadge />
+          <span className={styles.sirenTitle}>
+            &nbsp;‣&nbsp;
+            <span style={{ display: "inline-flex" }}>
+              <CopyPaste
+                disableCopyIcon={true}
+                label="ID RNF"
+                shouldRemoveSpace={true}
+              >
+                {fondation.id}
+              </CopyPaste>
+            </span>
+          </span>
+        </div>
+        {uniteLegale?.etablissements.all && (
+          <div className={styles.subSubTitle}>
+            <UniteLegaleEtablissementCountDescription
+              uniteLegale={uniteLegale}
+            />
+          </div>
+        )}
+      </div>
+      <SocialMedia
+        id={fondation.id}
+        label={fondation.title}
+        path={`https://annuaire-entreprises.data.gouv.fr/fondation/${fondation.id}`}
+      />
+      {uniteLegale && (
+        <Tabs
+          currentFicheType={FICHE.INFORMATION}
+          uniteLegale={uniteLegale}
+          user={user}
+        />
+      )}
+    </div>
+  );
+}
