571ab1e4ed826e6b7e9332f7532183ea23c7fdc8
Alan Chauchet <alan.chauchet@gmail.com>
2026-08-17 14:20:07 +0200

    feat(fondations): adds search fondations buttons to title section

--- src/components/title-fondation-section/index.tsx
+++ src/components/title-fondation-section/index.tsx
@@ -1,5 +1,7 @@
+import clsx from "clsx";
 import { Link } from "#/components/link";
 import { FondationBadge } from "#/components-ui/badge/frequent";
+import { Icon } from "#/components-ui/icon/wrapper";
 import SocialMedia from "#/components-ui/social-media";
 import type { IAgentInfo } from "#/models/authentication/agent";
 import type { IFondation } from "#/models/core/fondations.types";
@@ -19,34 +21,51 @@ export function TitleFondation(props: IProps) {
   const { fondation, uniteLegale, user } = props;
   return (
     <div className={styles.headerSection}>
-      <div>
-        <h1>
-          <Link params={{ slug: fondation.id }} to="/fondation/$slug">
-            {fondation.title}
-          </Link>
-        </h1>
-        <div className={styles.subTitle}>
-          <FondationBadge />
-          <span className={styles.sirenTitle}>
-            &nbsp;‣&nbsp;
-            <span style={{ display: "inline-flex" }}>
-              <CopyPaste
-                disableCopyIcon={true}
-                label="ID RNF"
-                shouldRemoveSpace={true}
-              >
-                {fondation.id}
-              </CopyPaste>
+      <div className="fr-grid-row fr-grid-row--gutters">
+        <div className={clsx(styles.titleContainer, "fr-col-12 fr-col-md-8")}>
+          <h1>
+            <Link params={{ slug: fondation.id }} to="/fondation/$slug">
+              {fondation.title}
+            </Link>
+          </h1>
+          <div className={styles.subTitle}>
+            <FondationBadge />
+            <span className={styles.sirenTitle}>
+              &nbsp;‣&nbsp;
+              <span style={{ display: "inline-flex" }}>
+                <CopyPaste
+                  disableCopyIcon={true}
+                  label="ID RNF"
+                  shouldRemoveSpace={true}
+                >
+                  {fondation.id}
+                </CopyPaste>
+              </span>
             </span>
-          </span>
-        </div>
-        {uniteLegale?.etablissements.all && (
-          <div className={styles.subSubTitle}>
-            <UniteLegaleEtablissementCountDescription
-              uniteLegale={uniteLegale}
-            />
           </div>
-        )}
+          {uniteLegale?.etablissements.all && (
+            <div className={styles.subSubTitle}>
+              <UniteLegaleEtablissementCountDescription
+                uniteLegale={uniteLegale}
+              />
+            </div>
+          )}
+        </div>
+        <div
+          className={clsx(
+            styles.searchFondationContainer,
+            "fr-col-12 fr-col-md-4"
+          )}
+        >
+          <Link
+            className="fr-btn fr-btn--icon-right fr-btn--secondary"
+            params={{ slug: "fondations" }}
+            to="/lp/$slug"
+          >
+            Rechercher une fondation
+            <Icon className="fr-ml-1w" slug="searchLine" />
+          </Link>
+        </div>
       </div>
       <SocialMedia
         id={fondation.id}
