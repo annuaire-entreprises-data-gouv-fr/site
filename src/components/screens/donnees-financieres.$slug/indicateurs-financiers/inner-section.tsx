901ff741e156c57b8552c03becc50c3ef6d08e35
Alan Chauchet <alan.chauchet@gmail.com>
2026-09-16 16:56:51 +0700

    fix(finances): hides number of bilan when none are accessible

--- src/components/screens/donnees-financieres.$slug/indicateurs-financiers/inner-section.tsx
+++ src/components/screens/donnees-financieres.$slug/indicateurs-financiers/inner-section.tsx
@@ -1,5 +1,9 @@
 import FAQLink from "#/components-ui/faq-link";
 import type { IAgentInfo } from "#/models/authentication/agent";
+import {
+  ApplicationRights,
+  hasRights,
+} from "#/models/authentication/user/rights";
 import type { IIndicateursFinanciersSociete } from "#/models/finances-societe/types";
 import { pluralize } from "#/utils/helpers";
 import { FinancesSocieteChartAndTable } from "./chart-and-table";
@@ -10,59 +14,66 @@ export const FinancesSocieteInnerSection = ({
 }: {
   financesSociete: IIndicateursFinanciersSociete;
   user: IAgentInfo | null;
-}) => (
-  <>
-    <p>
-      Cette entreprise possède des indicateurs financiers pour{" "}
-      {financesSociete.indicateurs.length} bilan
-      {pluralize(financesSociete.indicateurs)} déposé
-      {pluralize(financesSociete.indicateurs)} au RNE :
-    </p>
-    {financesSociete.hasBilanConsolide && (
-      <>
-        <h3>
-          Bilans{" "}
-          <FAQLink tooltipLabel="Consolidés">
-            Une entreprise peut déposer différents types de bilans :
-            <ul>
-              <li>simplifié : un bilan allégé</li>
-              <li>complet : le bilan classique</li>
-              <li>
-                consolidé : un bilan qui intègre les données des filiales d’un
-                groupe
-              </li>
-            </ul>
-          </FAQLink>
-        </h3>
-        <FinancesSocieteChartAndTable
-          estBilanConsolide={true}
-          hasCADGFiP={false}
-          indicateurs={financesSociete.indicateurs.filter(
-            (b) => b.estConsolide
-          )}
-          user={user}
-        />
-      </>
-    )}
-    <h3>
-      Bilans{" "}
-      <FAQLink tooltipLabel="Complets ou Simplifiés">
-        Une entreprise peut déposer différents types de bilans :
-        <ul>
-          <li>simplifié : un bilan allégé</li>
-          <li>complet : le bilan classique</li>
-          <li>
-            consolidé : un bilan qui intègre les données des filiales d’un
-            groupe
-          </li>
-        </ul>
-      </FAQLink>
-    </h3>
-    <FinancesSocieteChartAndTable
-      estBilanConsolide={false}
-      hasCADGFiP={financesSociete.hasCADGFiP}
-      indicateurs={financesSociete.indicateurs.filter((b) => !b.estConsolide)}
-      user={user}
-    />
-  </>
-);
+}) => {
+  const hasVisibleBilans =
+    hasRights({ user }, ApplicationRights.bilansRne) ||
+    financesSociete.indicateurs.some((b) => b.confidentiality === "Public");
+  return (
+    <>
+      {hasVisibleBilans && (
+        <p>
+          Cette entreprise possède des indicateurs financiers pour{" "}
+          {financesSociete.indicateurs.length} bilan
+          {pluralize(financesSociete.indicateurs)} déposé
+          {pluralize(financesSociete.indicateurs)} au RNE :
+        </p>
+      )}
+      {financesSociete.hasBilanConsolide && (
+        <>
+          <h3>
+            Bilans{" "}
+            <FAQLink tooltipLabel="Consolidés">
+              Une entreprise peut déposer différents types de bilans :
+              <ul>
+                <li>simplifié : un bilan allégé</li>
+                <li>complet : le bilan classique</li>
+                <li>
+                  consolidé : un bilan qui intègre les données des filiales d’un
+                  groupe
+                </li>
+              </ul>
+            </FAQLink>
+          </h3>
+          <FinancesSocieteChartAndTable
+            estBilanConsolide={true}
+            hasCADGFiP={false}
+            indicateurs={financesSociete.indicateurs.filter(
+              (b) => b.estConsolide
+            )}
+            user={user}
+          />
+        </>
+      )}
+      <h3>
+        Bilans{" "}
+        <FAQLink tooltipLabel="Complets ou Simplifiés">
+          Une entreprise peut déposer différents types de bilans :
+          <ul>
+            <li>simplifié : un bilan allégé</li>
+            <li>complet : le bilan classique</li>
+            <li>
+              consolidé : un bilan qui intègre les données des filiales d’un
+              groupe
+            </li>
+          </ul>
+        </FAQLink>
+      </h3>
+      <FinancesSocieteChartAndTable
+        estBilanConsolide={false}
+        hasCADGFiP={financesSociete.hasCADGFiP}
+        indicateurs={financesSociete.indicateurs.filter((b) => !b.estConsolide)}
+        user={user}
+      />
+    </>
+  );
+};
